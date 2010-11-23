4bb8d5b08a2122259d1558749d15663e34992ae6
Stephan Gerhard <git@unidesign.ch>
2010-11-23 12:41:25 +0100

    Adding treenodes on the overlay working

--- httpdocs/widgets/overlay.js
+++ httpdocs/widgets/overlay.js
@@ -1,14 +1,33 @@
-console.log("Overlay loading...");
+// active treenode
+var atn = null;
 
+function activateNode( node ) {
+    // activate new node, i.e. first deactivate old one
+    if ( atn != null ) {
+      atn.getC().attr({
+            fill: "rgb(0, 0, 255)" });
+    };
+    // if node == null, just deactivate
+    if( node == null ) {
+      atn = null;
+      return;
+    }
+    atn = node;
+    atn.getC().attr({
+          fill: "rgb(0, 255, 0)" });
+};
 
-Node = function( paper, parent, x, y, r )
-{
+Node = function( id, paper, parent, x, y, r )
+{ 
+  self = this;
+  self.id = id;
+  
 	var ox = 0, oy = 0;
 	
 	var c = paper.circle( x, y, r ).attr({
-					fill: "hsb(.8, 1, 1)",
+					fill: "rgb(0, 0, 255)",
 					stroke: "none",
-					opacity: .5
+					opacity: 1
 					});
 	this.getC = function(){ return c; }
 	
@@ -17,6 +36,9 @@ Node = function( paper, parent, x, y, r )
 					stroke: "none",
 					opacity: 0
 					});
+	// add a reference to the parent container node
+  // in order to get active treenode working
+	mc.parentnode = self;
 	
 	var children = new Array();
 	this.getChildren = function(){ return children; }
@@ -31,28 +53,45 @@ Node = function( paper, parent, x, y, r )
 		l.attr( {path: [ [ "M", c.attrs.cx, c.attrs.cy ], [ "L", parent.getC().attrs.cx, parent.getC().attrs.cy ] ] } );
 		l.toBack();
 	}
-	
 	this.drawLine = drawLine;
 	
+	var draw = function() {
+
+	  // draws/updates path to parent and children
+    for ( var i = 0; i < children.length; ++i )
+      children[ i ].drawLine();
+    if ( parent != null )
+      drawLine();
+	}
+	this.draw = draw;
+	
+	mc.click(function (e) {
+	  // activate this node
+	  activateNode( this.parentnode );
+	  // stop propagation of the event
+	  e.stopPropagation();
+  });
+
 	mc.move = function( dx, dy )
 	{
 		var x = ox + dx;
     	var y = oy + dy;
 		c.attr({cx: x,cy: y});
 		mc.attr({cx: x,cy: y});
-		for ( var i = 0; i < children.length; ++i )
-			children[ i ].drawLine();
-		drawLine();
+    draw();
 	}
 	mc.up = function()
 	{
-		c.attr({opacity:.5});
+		c.attr({opacity:1});
 	}
 	mc.start = function()
 	{
+	  // as soon you do something with the node, activate it
+	  activateNode( this.parentnode );
+		
 		ox = mc.attr("cx");
 		oy = mc.attr("cy");
-		c.attr({opacity:1});
+		c.attr({opacity:0.7});
 	}
 	mc.drag( mc.move, mc.start, mc.up );
 }
@@ -66,17 +105,16 @@ SVGOverlay = function(
   this.createdata = function() 
   {
     // storing original coordinates
-    
     var ln = null;
     
     var x = r.width / 2;
     var y = r.height / 2;
     
-    for (var i = 0; i < 100; ++i)
+    for (var i = 0; i < 10; ++i)
     {
       x = Math.min( r.width, Math.max( 0, x + ( .5 - Math.random() ) * 100 ) );
-      y = Math.min( r.height, Math.max( 0, y + ( .5 - Math.random() ) * 100 ) );
-      ln = new Node( r, ln, x, y, 8 );
+      y = Math.min( r.height, Math.max( 0, y + ( .5 - Math.random() ) * 100 ) );      
+      ln = new Node( i, r, ln, x, y, 4 );
     }
     
     while ( ln.parent != null )
@@ -100,12 +138,22 @@ SVGOverlay = function(
   }
   
   var onclick = function( e )
-  {
+  {    
     var m = ui.getMouse( e );
-    console.log("clicked", m);
-    return false;
+    // if ctrl is pressed and clicked, deselect atn
+    if( e.ctrlKey ) {
+      activateNode( null );
+    } else {
+      var nn = new Node( 100, r, atn, m.offsetX, m.offsetY, 4 ); 
+      nn.draw(); 
+      activateNode( nn );
+      // if the parent (i.e. active node is not null) we need to
+      // add the newly created treenode as a child
+      if(atn != null)
+        nn.parent.getChildren().push( nn );
+    }           
+    //return true;
   }
-  
   
 	self = this;
 	self.resolution = resolution;
@@ -114,9 +162,9 @@ SVGOverlay = function(
   var view = document.createElement( "div" );
   view.className = "sliceSVGOverlay";
   view.onclick = onclick;
-  view.style.zIndex = 4;
+  view.style.zIndex = 7;
   
 	var r = Raphael(view, 1000, 400);
-
+  self.r = r;
 
 };

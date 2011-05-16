24e51b00cf8687526cef2073faaa9a543d807b75
Stephan Gerhard <git@github.com>
2011-05-16 20:13:08 +0200

    Pie chart working

--- httpdocs/widgets/stats.js
+++ httpdocs/widgets/stats.js
@@ -14,10 +14,33 @@ function update_stats_fields(data)
   $("#proj_tags").text(data.proj_tags);
 }
 
+function update_piechart( data )
+{
+  var rpie = Raphael("piechart_treenode_holder");
+  var pie = rpie.g.piechart(80, 100, 80, data.values, { legend: data.users, legendpos: "east"});
+  pie.hover(function () {
+      this.sector.stop();
+      this.sector.scale(1.1, 1.1, this.cx, this.cy);
+      if (this.label) {
+          this.label[0].stop();
+          this.label[0].scale(1.5);
+          this.label[1].attr({"font-weight": 800});
+      }
+  }, function () {
+      this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
+      if (this.label) {
+          this.label[0].animate({scale: 1}, 500, "bounce");
+          this.label[1].attr({"font-weight": 400});
+      }
+  });
+  console.log("test", pie);
+}
+
 function refresh_project_statistics()
 {
-  requestQueue.replace("model/stats.list.php", "POST", {
-    "pid": project.id,
+  
+  requestQueue.register("model/stats.list.php", "POST", {
+    "pid": project.id
   }, function (status, text, xml)
     {
       if (status == 200)
@@ -32,6 +55,28 @@ function refresh_project_statistics()
           else
           {
             update_stats_fields(jso);
+          }
+        }
+      }
+      return true;
+    });
+
+  requestQueue.register("model/stats.treenodes.list.php", "POST", {
+    "pid": project.id
+  }, function (status, text, xml)
+    {
+      if (status == 200)
+      {
+        if (text && text != " ")
+        {
+          var jso = $.parseJSON(text);
+          if (jso.error)
+          {
+            alert(jso.error);
+          }
+          else
+          {
+            update_piechart(jso);
           }
         }
       }

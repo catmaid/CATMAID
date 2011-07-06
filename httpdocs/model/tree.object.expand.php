8032b4750e15331e92b41a532ab21b28acea3529
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-06 17:12:57 -0400

    tree.object.expand queries run in a transaction.

--- httpdocs/model/tree.object.expand.php
+++ httpdocs/model/tree.object.expand.php
@@ -5,6 +5,7 @@ include_once( 'db.pg.class.php' );
 include_once( 'session.class.php' );
 include_once( 'tools.inc.php' );
 include_once( 'json.inc.php' );
+include_once( 'utils.php' );
 
 $db =& getDB();
 $ses =& getSession();
@@ -36,6 +37,10 @@ if ( ! $uid ) {
 	return;
 }
 
+# 4. The user must have permissions to edit this tree
+# TODO -- currently all users can edit everything
+
+
 # Preconditions passed!
 
 # Treenode is element_of class_instance (skeleton), which is model_of (neuron) which is part_of class_instance (?), recursively, until reaching class_instance ('root').
@@ -59,45 +64,56 @@ $skeletonClassID = $db->getClassId( $pid, "skeleton" );
 if (!$skeletonClassID) { echo makeJSON( array( 'error' => 'Cannot find "skeleton" class for this project' ) ); return; }
 
 
-# 1. The skeleton id:
-$skeleton = $db->getClassInstanceForTreenode( $pid, $treenodeID, $element_of );
-if ( !empty($skeleton) ) {
-	// DECLARE skeletonID
-  $skeletonID = $skeleton[0]['class_instance_id'];
-} else {
-	echo makeJSON( array( 'error' => 'Cannot find skeleton for treenode with id: '.$treenodeID ) );
+// Start transaction
+if (! $db->begin() ) {
+	echo makeJSON( array( 'error' => 'Could not start transaction.' ) );
 	return;
 }
 
-# 2. Retrieve neuron id of the skeleton
-# getCIFromCI means "getClassInstanceFromClassInstance"
-$neuron = $db->getCIFromCI( $pid, $skeletonID, $model_of );
-if (!empty($neuron)) {
-	// DECLARE neuronID for the first time
+try {
+
+	# 1. The skeleton id:
+	$skeleton = $db->getClassInstanceForTreenode( $pid, $treenodeID, $element_of );
+	if ( false === $skeleton || empty($skeleton) ) {
+		emitErrorAndExit( $db, 'Cannot find skeleton for treenode with id: '.$treenodeID );
+	}
+
+	$skeletonID = $skeleton[0]['class_instance_id'];
+
+	# 2. Retrieve neuron id of the skeleton
+	# (getCIFromCI means "getClassInstanceFromClassInstance")
+	$neuron = $db->getCIFromCI( $pid, $skeletonID, $model_of );
+	if ( false === $neuron || empty($neuron) ) {
+		emitErrorAndExit( $db, 'Cannot find neuron for the skeleton with id: '.$skeletonID );
+	}
+
 	$neuronID = $neuron[0]['id'];
-} else {
-	echo makeJSON( array( 'error' => 'Cannot find neuron for the skeleton with id: '.$skeletonID ) );
-	return;
-}
 
-# 3. Retrieve, recursively, all the nodes of which the neuron is a part of.
-$path = array( $skeletonID, $neuronID );
-while(true) {
-	$q = $db->getCIFromCIWithClassNameAndId( $pid, end($path), $part_of );
-	if (!empty($q)) {
+	# 3. Retrieve, recursively, all the nodes of which the neuron is a part of.
+	$path = array( $skeletonID, $neuronID );
+	while(true) {
+		$q = $db->getCIFromCIWithClassNameAndId( $pid, end($path), $part_of );
+		if (false === $q || empty($q)) {
+			emitErrorAndExit( $db, 'Cannot find parent instance for instance with id: '.end($path) );
+		}
 		// Append the child at the last position in the path array
 		$path[count($path)] = $q[0]['parent_id'];
 		// If we reached the root, stop
 		if (0 == strcmp('root', $q[0]['class_name'])) {
 			break;
 		}
-  } else {
-	  echo makeJSON( array( 'error' => 'Cannot find parent instance for instance with id: '.end($path) ) );
-	  return;
 	}
-}
 
-# 4. Return the list of ids that represent the path from the root id to the skeleton id in the object hierarchy:
-echo json_encode( array_reverse($path) );
+	// There is nothing to commit but the transaction has to be closed:
+	if (! $db->commit() ) {
+		emitErrorAndExit( $db, 'Failed to commit for expand!' );
+	}
+
+	# 4. Return the list of ids that represent the path from the root id to the skeleton id in the object hierarchy:
+	echo json_encode( array_reverse($path) );
+
+} catch (Exception $e) {
+	emitErrorAndExit( $db, 'ERROR: '.$e );
+}
 
 ?>

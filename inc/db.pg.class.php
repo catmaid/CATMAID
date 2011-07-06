4be229083ee05388e67133a1a0903929db4bb4ee
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-06 10:49:33 -0400

    insertInto must return the result of submitting the query.

--- inc/db.pg.class.php
+++ inc/db.pg.class.php
@@ -187,8 +187,7 @@ class DB
 		//echo $queryStr, "<br />\n";
 		if( $this->debug )
 			error_log("In insertInto: ".preg_replace('/\s+/', ' ', $queryStr));
-		pg_query( $this->handle, $queryStr );
-		return;
+		return pg_query( $this->handle, $queryStr );
 	}
 	
 	/**

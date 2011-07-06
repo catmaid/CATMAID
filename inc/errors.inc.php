116f7394d0175997a025b8067632ea398186d382
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-06 16:20:33 -0400

    Fixed error handling to rethrow the error as an ErrorException.

--- inc/errors.inc.php
+++ inc/errors.inc.php
@@ -21,38 +21,28 @@ set_exception_handler('handleException');
 /* Also set an error handler with set_error_handler, following the
  * example here:
  *   http://php.net/manual/en/function.set-error-handler.php
+ *	
+ *	Test with:
+ *	trigger_error('Blabla', E_USER_ERROR);
  */
 
 function handleError($errno, $errstr, $errfile, $errline)
 {
-    header("HTTP/1.1 500 Internal Server Error");
 
     if (!(error_reporting() & $errno)) {
         // This error code is not included in error_reporting
         return;
     }
 
-    switch ($errno) {
-    case E_USER_ERROR:
-        echo "E_USER_ERROR [$errno] $errstr\n";
-        echo "  Fatal error on line $errline in file $errfile";
-        break;
-
-    case E_USER_WARNING:
-        echo "E_USER_WARNING [$errno] $errstr\n";
-        break;
-
-    case E_USER_NOTICE:
-        echo "E_USER_NOTICE [$errno] $errstr\n";
-        break;
-
-    default:
-        echo "Unknown error type: [$errno] $errstr\n";
-        break;
-    }
+		// Sent to the try/catch if any,
+		// or else goes to the default exception handler (see above)
+		// which according to the manual dies after execution.
+ 		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 
     /* Still execute the PHP internal error handler */
-    return false;
+
+		// NEVER die here or transactions cannot be rolled back.
+		// NEVER return anything or execution doesn't stop.
 }
 
 set_error_handler('handleError');

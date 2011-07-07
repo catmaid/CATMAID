0210232776f406a5f22fa0d666235071da8947fd
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-07 11:38:29 -0400

    Refactored treenode.info.php. Now uses json_encode.

--- httpdocs/model/treenode.info.php
+++ httpdocs/model/treenode.info.php
@@ -14,11 +14,30 @@ $uid = $ses->isSessionValid() ? $ses->getId() : 0;
 
 $tnid = isset( $_REQUEST[ 'tnid' ] ) ? intval( $_REQUEST[ 'tnid' ] ) : -1;
 
-if ( $pid )
-{
-	if ( $uid )
-	{
-		$query = "SELECT ci.id as skeleton_id, ci.name as skeleton_name, ".
+
+# Check preconditions:
+
+# 1. There must be a treenode id
+if ( ! $tnid ) {
+	echo json_encode( array( 'error' => 'A treenode id has not been provided!' ) );
+	return;
+}
+
+# 2. There must be a project id
+if ( ! $pid ) {
+  echo json_encode( array( 'error' => 'Project closed. Cannot apply operation.' ) );
+	return;
+}
+
+# 3. There must be a user id
+if ( ! $uid ) {
+    echo json_encode( array( 'error' => 'You are not logged in.' ) );
+	return;
+}
+
+// Single query: no need for an explicit transaction
+
+$query = "SELECT ci.id as skeleton_id, ci.name as skeleton_name, ".
 			"            ci2.id as neuron_id, ci2.name as neuron_name ".
 			"FROM treenode_class_instance tci, relation r, relation r2, ".
 			"     class_instance ci, class_instance ci2, class_instance_class_instance cici ".
@@ -28,19 +47,16 @@ if ( $pid )
 			"    ci.id = cici.class_instance_a AND ci2.id = cici.class_instance_b AND ".
 			"    cici.relation_id = r2.id AND r2.relation_name = 'model_of'";
 
-		$result = $db->getResult($query);
-		if ($result) {
-			if (count($result) > 1)
-				echo makeJSON( array( 'error' => "Found more than one skeleton and neuron for treenode $tnid" ) );
-			else
-				echo makeJSON( $result[0] );
-		} else
-			echo makeJSON( array( 'error' => "No skeleton and neuron found for treenode $tnid" ) );
+$result = $db->getResult($query);
+
+if ($result) {
+	if (count($result) > 1) {
+		echo json_encode( array( 'error' => "Found more than one skeleton and neuron for treenode $tnid" ) );
+	} else {
+		echo json_encode( $result[0] );
 	}
-	else
-		echo makeJSON( array( 'error' => 'You are not logged in currently.  Please log in to get skeleton and neuron details' ) );
+} else {
+	echo json_encode( array( 'error' => "No skeleton and neuron found for treenode $tnid" ) );
 }
-else
-    echo makeJSON( array( 'error' => 'Project closed. Can not apply operation.' ) );
   
 ?>

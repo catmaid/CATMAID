catmaid/CATMAID

commit b59bf938a10f71320787371cebbf81ccf2cfdb3a

Add a working mousemove handler for tracing mode
This has the following positive effects:

 * The current world coordinates are displayed in the
   status bar.

 * The project.lastX and project.lastY values are updated,
   so zooming in on the current point under the mouse
   pointer works again.  See issue #59

 * Now project.last[XY] are accurate we can go ahead an
   implement the 'g' key press for selecting the node
   currently under the mouse pointer.

httpdocs/stack.js
@@ -1050,11 +1050,7 @@ trakem2_project //!< boolean that states if a TrakEM2 project is available for t
 
       // for the surrounding mouse event catcher
       mouseCatcher.onmousedown = onmousedown.move;
-      mouseCatcher.onmousemove = onmousemove.pos;
-      // but also for the svgoverlay, stops dragging node mdoe
-      svgOverlay.view.onmousedown = onmousedown.trace;
-      // XXX: coordinates are adjusted, either position or dragging but not both :(
-      // svgOverlay.view.onmousemove = onmousemove.trace;
+      mouseCatcher.onmousemove = onmousemove.trace;
       try {
         svgOverlay.view.addEventListener("DOMMouseScroll", onmousewheel.zoom, false); /* Webkit takes the event but does not understand it ... */
         svgOverlay.view.addEventListener("mousewheel", onmousewheel.zoom, false);

commit 912d6ad8a1731a5cbc793f17369c365ebd32a52f

Fix new node creation
Commit b59bf938a10f71320787 accidentally broke the creation
of new nodes - this commit fixes that.

httpdocs/stack.js
@@ -1051,6 +1051,7 @@ trakem2_project //!< boolean that states if a TrakEM2 project is available for t
       // for the surrounding mouse event catcher
       mouseCatcher.onmousedown = onmousedown.move;
       mouseCatcher.onmousemove = onmousemove.trace;
+      svgOverlay.view.onmousedown = onmousedown.trace;
       try {
         svgOverlay.view.addEventListener("DOMMouseScroll", onmousewheel.zoom, false); /* Webkit takes the event but does not understand it ... */
         svgOverlay.view.addEventListener("mousewheel", onmousewheel.zoom, false);
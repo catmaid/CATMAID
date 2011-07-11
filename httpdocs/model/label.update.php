f60d20cc811968d453bc29f30ccf5fdae395dc9c
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-11 17:59:21 -0400

    label.update.php runs in a transaction

--- httpdocs/model/label.update.php
+++ httpdocs/model/label.update.php
@@ -6,6 +6,7 @@ include_once( 'db.pg.class.php' );
 include_once( 'session.class.php' );
 include_once( 'tools.inc.php' );
 include_once( 'json.inc.php' );
+include_once( 'utils.php' );
 
 $db =& getDB();
 $ses =& getSession();
@@ -20,89 +21,136 @@ $tags = isset( $_REQUEST[ 'tags' ] ) ? $_REQUEST[ 'tags' ] : '[]';
 $ntype = isset( $_REQUEST[ 'ntype' ] ) ? $_REQUEST[ 'ntype' ] : 'treenode';
 
 
-if ( $pid )
-{
-  if ( $uid )
-  {
-    $label_id = $db->getClassId( $pid, 'label' );
-    if(!$label_id) { echo makeJSON( array( 'error' => 'Can not find "label" class for this project' ) ); return; }
+# 1. There must be a project id
+if ( ! $pid ) {
+  echo json_encode( array( 'error' => 'Project closed. Cannot apply operation.' ) );
+	return;
+}
+
+# 2. There must be a user id
+if ( ! $uid ) {
+    echo json_encode( array( 'error' => 'You are not logged in.' ) );
+	return;
+}
+
+# 3. Permissions?
+canEditOrExit($db, $uid, $pid);
+
+
+// Start transaction
+if (! $db->begin() ) {
+	echo json_encode( array( 'error' => 'Could not start transaction.' ) );
+	return;
+}
+
+try {
+
+  $label_id = $db->getClassId( $pid, 'label' );
+  if(!$label_id) { echo makeJSON( array( 'error' => 'Can not find "label" class for this project' ) ); return; }
+  
+  $labeled_as_id = $db->getRelationId( $pid, 'labeled_as' );
+  if(!$labeled_as_id) { echo makeJSON( array( 'error' => 'Can not find "labeled_as" relation for this project' ) ); return; }
+  
+  // delete all treenode_class_instance to labels
+  
+  if ($ntype == "treenode") {
     
-    $labeled_as_id = $db->getRelationId( $pid, 'labeled_as' );
-    if(!$labeled_as_id) { echo makeJSON( array( 'error' => 'Can not find "labeled_as" relation for this project' ) ); return; }
+    $ids = $db->deleteFrom("treenode_class_instance", ' "treenode_class_instance"."treenode_id" = '.$nid.' AND
+    "treenode_class_instance"."relation_id" = '.$labeled_as_id);
+  
+  } else if ($ntype == "location") {
+
+    $ids = $db->deleteFrom("connector_class_instance", ' "connector_class_instance"."connector_id" = '.$nid.' AND
+    "connector_class_instance"."relation_id" = '.$labeled_as_id);
+
+  }
+  
+  if (false === $ids) {
+    emitErrorAndExit($db, 'Failed to delete relations between treenodes or connectors to labels.');
+  }
+  
+  // see which labels have to be created anew and which ones are retrieved
+  $ta = explode(',', $tags);
+  // remove empty strings
+  foreach($ta as $val) {
+    // exclusion criteria
+    // if value empty, skip
+    if(empty($val))
+      continue;
     
-    // delete all treenode_class_instance to labels
+    // see if label exists
+    $res = $db->getResult('SELECT "ci"."name" as "name", "ci"."id" as "id" FROM "class_instance" as "ci" WHERE
+    "ci"."class_id" = '.$label_id.' AND 
+    "ci"."name" = \''.pg_escape_string($val).'\' AND
+    "ci"."project_id" = '.$pid);
+
+    if (false === $res) {
+      emitErrorAndExit($db, 'Failed to select label.');
+    }
+    
+    if (empty($res)) {
+      // create class_instance of label
+      $data = array(
+        'user_id' => $uid,
+        'project_id' => $pid,
+        'class_id' => $label_id,
+        'name' => $val
+        );
+      $lab_ci_id = $db->insertIntoId('class_instance', $data );
+      
+      if (false === $lab_ci_id) {
+        emitErrorAndExit($db, 'Failed to insert new label instance.');
+      }
+      
+    } else {
+      // retrieve class_instance id
+      $lab_ci_id = $res[0]['id'];
+    }
+    // add relation
     
     if($ntype == "treenode") {
+
+      $data = array(
+        'user_id' => $uid,
+        'project_id' => $pid,
+        'relation_id' => $labeled_as_id,
+        'treenode_id' => $nid,
+        'class_instance_id' => $lab_ci_id
+        );
+      $lab_ci_id = $db->insertIntoId('treenode_class_instance', $data );
       
-      $ids = $db->deleteFrom("treenode_class_instance", ' "treenode_class_instance"."treenode_id" = '.$nid.' AND
-      "treenode_class_instance"."relation_id" = '.$labeled_as_id);
-    
+      if (false === $lab_ci_id) {
+        emitErrorAndExit($db, 'Failed to insert relation of label with treenode.');
+      }
+      
     } else if ($ntype == "location") {
 
-      $ids = $db->deleteFrom("connector_class_instance", ' "connector_class_instance"."connector_id" = '.$nid.' AND
-      "connector_class_instance"."relation_id" = '.$labeled_as_id);
-
-    }
-    
-    // see which labels have to be created a new and which ones are retrieved
-    $ta = explode(',', $tags);
-    // remove empty strings
-    foreach($ta as $val) {
-      // exclusion criteria
-      // if value empty, skip
-      if(empty($val))
-        continue;
+      $data = array(
+        'user_id' => $uid,
+        'project_id' => $pid,
+        'relation_id' => $labeled_as_id,
+        'connector_id' => $nid,
+        'class_instance_id' => $lab_ci_id
+        );
+      $lab_ci_id = $db->insertIntoId('connector_class_instance', $data );
       
-      // see if label exists
-      $res = $db->getResult('SELECT "ci"."name" as "name", "ci"."id" as "id" FROM "class_instance" as "ci" WHERE
-      "ci"."class_id" = '.$label_id.' AND 
-      "ci"."name" = \''.pg_escape_string($val).'\' AND
-      "ci"."project_id" = '.$pid);
-      if(empty($res)) {
-        // create class_instance of label
-        $data = array(
-          'user_id' => $uid,
-          'project_id' => $pid,
-          'class_id' => $label_id,
-          'name' => $val
-          );
-        $lab_ci_id = $db->insertIntoId('class_instance', $data );
-      } else {
-        // retrieve class_instance id
-        $lab_ci_id = $res[0]['id'];
+      if (false === $lab_ci_id) {
+        emitErrorAndExit($db, 'Failed to insert relation of label with location.');
       }
-      // add relation
       
-      if($ntype == "treenode") {
-
-        $data = array(
-          'user_id' => $uid,
-          'project_id' => $pid,
-          'relation_id' => $labeled_as_id,
-          'treenode_id' => $nid,
-          'class_instance_id' => $lab_ci_id
-          );
-        $lab_ci_id = $db->insertIntoId('treenode_class_instance', $data );
-        
-      } else if ($ntype == "location") {
-
-        $data = array(
-          'user_id' => $uid,
-          'project_id' => $pid,
-          'relation_id' => $labeled_as_id,
-          'connector_id' => $nid,
-          'class_instance_id' => $lab_ci_id
-          );
-        $lab_ci_id = $db->insertIntoId('connector_class_instance', $data );
-        
-      }
-
     }
-      echo json_encode( array('message' => 'success') );  
+
   }
-  else
-    echo makeJSON( array( 'error' => 'You are not logged in currently.  Please log in to be able to retrieve labels.' ) );
-}
-else
-  echo makeJSON( array( 'error' => 'Project closed. Can not apply operation.' ) );
   
+  if (! $db->commit() ) {
+    emitErrorAndExit( $db, 'Failed to commit!' );
+  }
+  
+  
+  echo json_encode( array('message' => 'success') );  
+
+} catch (Exception $e) {
+	emitErrorAndExit( $db, 'ERROR: '.$e );
+}
+  
+?>

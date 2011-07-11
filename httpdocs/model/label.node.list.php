7656c62aa8519962a31413a9b734840ffb8f02dd
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-11 17:59:32 -0400

    label.node.list.php runs in a transaction

--- httpdocs/model/label.node.list.php
+++ httpdocs/model/label.node.list.php
@@ -17,52 +17,77 @@ $cid = isset( $_REQUEST[ 'cid' ] ) ? intval( $_REQUEST[ 'cid' ] ) : 0;
 $nid = isset( $_REQUEST[ 'nid' ] ) ? intval( $_REQUEST[ 'nid' ] ) : 0;
 $ntype = isset( $_REQUEST[ 'ntype' ] ) ? $_REQUEST[ 'ntype' ] : 'treenode';
 
-if ( $pid )
-{
-  if ( $uid )
-  {
-    $label_id = $db->getClassId( $pid, 'label' );
-    if(!$label_id) { echo makeJSON( array( 'error' => 'Can not find "label" class for this project' ) ); return; }
-    
-    $labeled_as_id = $db->getRelationId( $pid, 'labeled_as' );
-    if(!$labeled_as_id) { echo makeJSON( array( 'error' => 'Can not find "labeled_as" relation for this project' ) ); return; }
-    
-    if($ntype == "treenode") {
-      $res = $db->getResult('SELECT "ci"."name" as "name" FROM "class_instance" as "ci", "treenode_class_instance" as "tci" WHERE
-      "tci"."treenode_id" = '.$nid.' AND
-      "tci"."class_instance_id" = "ci"."id" AND
-      "tci"."relation_id" = '.$labeled_as_id.' AND
-      "ci"."class_id" = '.$label_id.' AND 
-      "ci"."project_id" = '.$pid);
-      
-    } else if ($ntype == "location") {
-      $res = $db->getResult('SELECT "ci"."name" as "name" FROM "class_instance" as "ci", "connector_class_instance" as "cci" WHERE
-      "cci"."connector_id" = '.$nid.' AND
-      "cci"."class_instance_id" = "ci"."id" AND
-      "cci"."relation_id" = '.$labeled_as_id.' AND
-      "ci"."class_id" = '.$label_id.' AND 
-      "ci"."project_id" = '.$pid);
-      
-    }
+# 1. There must be a project id
+if ( ! $pid ) {
+  echo json_encode( array( 'error' => 'Project closed. Cannot apply operation.' ) );
+	return;
+}
 
-    $list = array();
-    foreach($res as $key => $val) {
-      if(empty($list[$nid]))
-      {
-        $list[$nid] = array();
-        $list[$nid][] = $val['name'];
-      } else {
-        $list[$nid][] = $val['name'];
-      }
-      
-    }
+# 2. There must be a user id
+if ( ! $uid ) {
+    echo json_encode( array( 'error' => 'You are not logged in.' ) );
+	return;
+}
+
+// Start transaction
+if (! $db->begin() ) {
+	echo json_encode( array( 'error' => 'Could not start transaction.' ) );
+	return;
+}
+
+try {
+
+  $label_id = $db->getClassId( $pid, 'label' );
+  if(!$label_id) { echo makeJSON( array( 'error' => 'Can not find "label" class for this project' ) ); return; }
+  
+  $labeled_as_id = $db->getRelationId( $pid, 'labeled_as' );
+  if(!$labeled_as_id) { echo makeJSON( array( 'error' => 'Can not find "labeled_as" relation for this project' ) ); return; }
+  
+  if ($ntype == "treenode") {
+    $res = $db->getResult('SELECT "ci"."name" as "name" FROM "class_instance" as "ci", "treenode_class_instance" as "tci" WHERE
+    "tci"."treenode_id" = '.$nid.' AND
+    "tci"."class_instance_id" = "ci"."id" AND
+    "tci"."relation_id" = '.$labeled_as_id.' AND
+    "ci"."class_id" = '.$label_id.' AND 
+    "ci"."project_id" = '.$pid);
     
-    echo json_encode( $list );
+  } else if ($ntype == "location") {
+    $res = $db->getResult('SELECT "ci"."name" as "name" FROM "class_instance" as "ci", "connector_class_instance" as "cci" WHERE
+    "cci"."connector_id" = '.$nid.' AND
+    "cci"."class_instance_id" = "ci"."id" AND
+    "cci"."relation_id" = '.$labeled_as_id.' AND
+    "ci"."class_id" = '.$label_id.' AND 
+    "ci"."project_id" = '.$pid);
     
   }
-  else
-    echo makeJSON( array( 'error' => 'You are not logged in currently.  Please log in to be able to retrieve labels.' ) );
+  
+  if (false === $res) {
+    emitErrorAndExit($db, 'Failed to select data for labels.');
+  }
+
+  $list = array();
+  foreach($res as $key => $val) {
+    if(empty($list[$nid]))
+    {
+      $list[$nid] = array();
+      $list[$nid][] = $val['name'];
+    } else {
+      $list[$nid][] = $val['name'];
+    }
+    
+  }
+  
+  if (! $db->commit() ) {
+    // No need to commit, just be nice to postgres
+    emitErrorAndExit( $db, 'Failed to commit!' );
+  }
+  
+  echo json_encode( $list );
+    
+} catch (Exception $e) {
+	emitErrorAndExit( $db, 'ERROR: '.$e );
 }
-else
-  echo makeJSON( array( 'error' => 'Project closed. Can not apply operation.' ) );
+
+?>
+
   

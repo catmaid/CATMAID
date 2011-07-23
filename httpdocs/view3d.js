8dbadea7828c6b3635984fc5a7bb7019ad7ecaa6
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-07-23 20:10:05 +0200

    3D window works. It is odd that the overlay disappears when the window with the images loses focus. To be fixed soon.

--- httpdocs/view3d.js
+++ httpdocs/view3d.js
@@ -1,67 +1,90 @@
 /** Creates and returns a new 3d window. */
 function create3dWindow()
 {
-	var win = new CMWWindow( "3D View" );
-	var content = win.getFrame();
-	content.style.backgroundColor = "#ffffff";
-	var context = document.createElement( "div" );
-	context.setAttribute( "id", "view_3d_context" );
-	context.style.position = "absolute";
-	context.style.bottom = "0px";
-	context.style.width = "100%";
-	context.style.overflow = "auto";
-	content.appendChild( context );
+  var win = new CMWWindow( "3D View" );
+  var content = win.getFrame();
+  content.style.backgroundColor = "#ffffff";
 
-	console.log("id is: " + context.getAttribute("id"));
-	createViewerFromCATMAID( context.getAttribute( "id" ) );
-/*
-	var keyboardShortcutsList = document.createElement( "p" );
-    keyboardShortcutsList.id="keyShortcutsText";
-    var keysHTML = '';
-    for (i in stringToKeyAction) {
-      keysHTML += '<button style="width:3em; margin-right:1em">' + i + '</button>' + stringToKeyAction[i].helpText + "<br />";
-    }
-    keyboardShortcutsList.innerHTML = keysHTML;
-    keyboardShortcutsContext.appendChild( keyboardShortcutsList );
-    keyboardShortcutsContent.appendChild( keyboardShortcutsContext );
-*/
+  var container = document.createElement( "div" );
+  container.setAttribute( "id", "view_in_3d_widget" );
+  container.setAttribute( "class", "sliceView");
+  container.style.position = "absolute";
+  container.style.bottom = "0px";
+  container.style.width = "100%";
+  container.style.overflow = "auto";
+  container.style.backgroundColor = "#ffffff";
+  content.appendChild( container );
 
-	win.addListener(
-		function( callingWindow, signal )
-		{
-			switch ( signal )
-			{
-			case CMWWindow.CLOSE:
-				if ( typeof project == undefined || project == null )
-				{
-					rootWindow.close();
-					document.getElementById( "content" ).style.display = "none";
-				}
-				else {
-					win.close();
+  var add = document.createElement('input');
+  add.setAttribute("type", "button");
+  add.setAttribute("id", "add_current_to_3d_view");
+  add.setAttribute("value", "Add current skeleton to 3D view");
+  add.onclick = addTo3DView; // function declared in overlay.js
+  container.appendChild( add );
+
+  var introduction = document.createElement('p')
+  introduction.setAttribute("id", "view3DIntroduction");
+  container.appendChild(introduction);
+
+  var list = document.createElement('ul');
+  list.setAttribute("id", "view-3d-object-list")
+  container.appendChild(list);
+
+  var canvas = document.createElement('div');
+  canvas.setAttribute("id", "viewer-3d-canvas");
+  canvas.style.width = "500px";
+  canvas.style.height = "500px";
+  container.appendChild(canvas);
+
+  var buttons = document.createElement('div');
+  ['xy', 'xz', 'zy'].map(function (s) {
+    var b = document.createElement('input');
+    b.setAttribute("id", s + "-button");
+    b.setAttribute("type", "button");
+    b.setAttribute("value", s.toUpperCase());
+    buttons.appendChild(b);
+  });
+  container.appendChild(buttons);
+
+  win.addListener(
+    function( callingWindow, signal )
+    {
+      switch ( signal )
+      {
+      case CMWWindow.CLOSE:
+        if ( typeof project == undefined || project == null )
+        {
+          rootWindow.close();
+          document.getElementById( "content" ).style.display = "none";
         }
-				win = null;
-				break;
-			case CMWWindow.RESIZE:
-				context.style.height = win.getContentHeight() + "px";
-				break;
-			}
-			return true;
-		});
+        else {
+          win.close();
+        }
+        win = null;
+        break;
+      case CMWWindow.RESIZE:
+        container.style.height = win.getContentHeight() + "px";
+        break;
+      }
+      return true;
+    });
 
-	document.getElementById( "content" ).style.display = "none";
+  document.getElementById( "content" ).style.display = "none";
 
-	/* be the first window */
-	if ( rootWindow.getFrame().parentNode != document.body )
-	{
-		document.body.appendChild( rootWindow.getFrame() );
-		document.getElementById( "content" ).style.display = "none";
-	}
+  /* be the first window */
+  if ( rootWindow.getFrame().parentNode != document.body )
+  {
+    document.body.appendChild( rootWindow.getFrame() );
+    document.getElementById( "content" ).style.display = "none";
+  }
 
-	if ( rootWindow.getChild() == null )
-		rootWindow.replaceChild( win );
-	else
-		rootWindow.replaceChild( new CMWHSplitNode( rootWindow.getChild(), win ) );
+  if ( rootWindow.getChild() == null )
+    rootWindow.replaceChild( win );
+  else
+    rootWindow.replaceChild( new CMWHSplitNode( rootWindow.getChild(), win ) );
 
-	return win;
+  // Fill in with a Raphael canvas, now that the window exists in the DOM:
+  createViewerFromCATMAID( canvas.getAttribute("id") );
+
+  return win;
 };

3e06ceda4e6343521d3e33651130cd0fdefd62d9
Stephan Gerhard <git@unidesign.ch>
2011-08-24 22:34:12 +0200

    WIP: Improved CATMAID to Python dump script

--- scripts/export_from_catmaid.py
+++ scripts/export_from_catmaid.py
@@ -4,20 +4,25 @@ import sys
 import psycopg2
 import os
 import numpy as np
+import yaml
 
-if len(sys.argv) != 2:
-    print >> sys.stderr, "Usage: export_from_catmaid.py <PROJECT-ID>"
+pid = 3 # int(sys.argv[1])
+
+try:
+
+    conf = yaml.load(open(os.path.join(os.environ['HOME'], '.catmaid-db')))
+except:
+    print >> sys.stderr, '''Your ~/.catmaid-db file should look like:
+
+host: localhost
+database: catmaid
+username: catmaid_user
+password: password_of_your_catmaid_user'''
     sys.exit(1)
 
-pid = int(sys.argv[1])
-
-db_login_filename = os.path.join(os.environ['HOME'],'.catmaid-db.test')
-fp = open(db_login_filename)
-for i, line in enumerate(fp):
-	if i == 0:
-		catmaid_db_user = line.strip()
-	elif i == 1:
-		catmaid_db_password = line.strip()
+#if len(sys.argv) != 2:
+#    print >> sys.stderr, "Usage: export_from_catmaid.py <PROJECT-ID>"
+#    sys.exit(1)
 
 def treenodes_to_numpy(tnrows):
 	""" Converts retrieved list of treenode tuples to an array 
@@ -65,7 +70,10 @@ def treenodes_to_numpy(tnrows):
 	return locarr, loctop, idx
 	
 
-conn = psycopg2.connect(database="catmaid",user=catmaid_db_user,password=catmaid_db_password)
+conn = psycopg2.connect(host=conf['host'],
+                        database=conf['database'],
+                        user=conf['username'],
+                        password=conf['password'])
 c = conn.cursor()
 
 # Find project information
@@ -110,51 +118,96 @@ rows = c.fetchall()
 if len(rows) == 0:
     print >> sys.stderr, "No skeletons found in project {0}".format(ptitle)
     sys.exit(1)
-    
-# Loop over all skeletons, retrieve treenodes (element_of)
-allskeletons = []
-skelidlist = []
-for row in rows:
-	skid, = row
-	skelidlist.append( skid )
-	print "exporting skeleton {0}".format(skid)
-	query = 'SELECT t.id, (t.location).x, (t.location).y, (t.location).z , t.parent_id '
-	query += 'FROM treenode t, treenode_class_instance tci '
-	query += 'WHERE t.project_id = %s AND tci.class_instance_id = %s AND '
-	query += 't.id = tci.treenode_id AND tci.relation_id = {relid} '.format(relid = eleofid)
-	query += 'ORDER BY t.parent_id DESC'
-	c.execute(query,(pid,skid,))
-	tnrows = c.fetchall()
-	# Transform retrieved list of treenodes to a numpy array
-	allskeletons.append( treenodes_to_numpy(tnrows) )
 
-# Make big table
-# Find absolute number of treenodes
-N = 0
-for sk in allskeletons:
-	N += len( sk[0] )
-
-all_id =  np.zeros( (N,), dtype = np.int32 )
-all_loc = np.zeros( (N,3), dtype = np.float32 )
-all_lab = np.zeros( (N,), dtype = np.int32 )
-all_top = np.zeros( (N,), dtype = np.int32 )
-
+# fetch skeleton nodes
+query = """
+select tn.id, (tn.location).x, (tn.location).y, (tn.location).z, tn.parent_id, tci.class_instance_id as skeleton_id from treenode as tn, treenode_class_instance as tci where tn.project_id = 3 and tci.treenode_id = tn.id and tci.relation_id = 11 order by tci.class_instance_id asc
+"""
+c.execute(query,(pid,scid))
+tn_nr = c.rowcount
+tn_xyz = np.zeros( (tn_nr, 3), dtype = np.float32 )
+tn_connectivity = np.zeros( (tn_nr, 2), dtype = np.uint32 )
+tn_skeletonid =  np.zeros( (tn_nr, 1), dtype = np.uint32 )
+tn_id =  np.zeros( (tn_nr, 1), dtype = np.uint32 )
+tn_mapid2idx = {}
+tn_mapid2skelid = {}
 cnt = 0
-for i, arr in enumerate(allskeletons):
-	locarr, toparr, idx = arr
-	Nskel = len( locarr )
-	
-	all_loc[cnt:cnt+Nskel, :] = locarr
-	all_top[cnt:cnt+Nskel] = toparr
-	all_lab[cnt:cnt+Nskel] = [ skelidlist[i] ] * Nskel
-	all_id[cnt:cnt+Nskel] = idx
-	cnt += Nskel
-	
-# Exporting treenodes finished.
+concnt = 0
+for i,row in enumerate(c):
+    tn_mapid2idx[row[0]] = i
+    tn_mapid2skelid[row[0]] = row[5]
+    tn_id[i] = row[0]
+    tn_xyz[i,0] = row[1]
+    tn_xyz[i,1] = row[2]
+    tn_xyz[i,2] = row[3]
+    if row[4] is None:
+        # a root node
+        cnt += 1
+    else:
+        tn_connectivity[concnt,0] = i
+        tn_connectivity[concnt,1] = row[4]
+        concnt += 1
+    tn_skeletonid[i,0] = row[5]
 
-###
-# Export connectors
-###
-# Need to be clear how we index topology into treenode arrays
-# Probably need to recover the globally unique treenode id for global topology,
-# or remapped topology of all_X arrays
+
+# discard unused rows
+tn_connectivity = tn_connectivity[:-(cnt),:]
+# map connectivity to index
+for row in tn_connectivity:
+    print row
+    row[1] = tn_mapid2idx[row[1]]
+# type, only skeleton now
+tn_type =  np.ones( (len(tn_connectivity), 1), dtype = np.uint32 )
+
+# fetch connector nodes
+query = """
+select cn.id, (cn.location).x, (cn.location).y, (cn.location).z from connector as cn where cn.project_id = 3
+"""
+c.execute(query,(pid,scid))
+cn_nr = c.rowcount
+cn_id = np.zeros( (cn_nr, 1), dtype = np.uint32 )
+cn_xyz = np.zeros( (cn_nr, 3), dtype = np.float32 )
+cn_mapid2idx = {}
+for i,row in enumerate(c):
+    cn_mapid2idx[row[0]] = i
+    cn_id[i,0] = row[0]
+    cn_xyz[i,0] = row[1]
+    cn_xyz[i,1] = row[2]
+    cn_xyz[i,2] = row[3]
+
+# fetch treenode - connector connectivity, transform to index connectivity
+query = """
+select tc.relation_id, tc.treenode_id, tc.connector_id from treenode_connector as tc where tc.project_id = 3
+"""
+c.execute(query,(pid,scid))
+tc_nr = c.rowcount
+tc_connectivity = np.zeros( (tc_nr, 2), dtype = np.uint32 )
+tc_type = np.zeros( (tc_nr, 1), dtype = np.uint32 )
+tc_id = np.zeros( (tc_nr, 1), dtype = np.uint32 )
+for i,row in enumerate(c):
+    tc_type[i,0] = row[0]
+    tc_id[i,0] = tn_mapid2skelid[row[1]]
+    tc_connectivity[i,0] = tn_mapid2idx[row[1]]
+    tc_connectivity[i,1] = cn_mapid2idx[row[2]] + len(tn_xyz)
+
+print tc_connectivity
+
+vertices = np.concatenate( (tn_xyz, cn_xyz) )
+connectivity = np.concatenate( (tn_connectivity, tc_connectivity) )
+connectivity_type = np.concatenate( (tn_type, tc_type) )
+connectivity_skeletonid = np.concatenate( (tn_skeletonid, tc_id) )
+
+print "vertices", vertices
+print "connectivity", connectivity
+print "type", connectivity_type
+print "skeletonid", connectivity_skeletonid
+
+print "cnids", cn_id
+# output into neurohdf irregular dataset for stack region
+
+np.save('/home/stephan/vert.npy', vertices)
+np.save('/home/stephan/conn.npy', connectivity)
+np.save('/home/stephan/conn_id.npy', connectivity_skeletonid)
+
+# act.deselect()
+

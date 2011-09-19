8690530dea42f843f56b2e17ade065809bd8f8db
Mark Longair <longair@ini.phys.ethz.ch>
2011-09-19 18:45:44 +0200

    Add some initial tests, a couple failing for TDD reasons

--- mysite/vncbrowser/tests.py
+++ mysite/vncbrowser/tests.py
@@ -1,12 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.db import connection
+import os
 
+from vncbrowser.models import Project, Stack
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +10,47 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+def ensure_schema_exists():
+    """
+    This function will create the CATMAID schema is it doesn't seem to
+    exist yet (based on the presence or not of the 'project' table.
+    """
+    cursor = connection.cursor()
+    # See if the project table has been created:
+    cursor.execute("SELECT count(*) FROM pg_tables WHERE tablename = 'project'")
+    row = cursor.fetchone()
+    if row[0] == 1:
+        return
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(current_directory, "tables.sql")) as fp:
+        cursor.execute(fp.read())
+
+class InsertionTest(TestCase):
+    def setUp(self):
+        ensure_schema_exists()
+
+    def test_project_insertion(self):
+        """
+        Tests that a project can be inserted, and that the
+        id is retrievable afterwards.  (This is something that
+        the custom psycopg2 driver is needed for.)
+        """
+        p = Project()
+        p.title = "Example Project"
+        p.comment = "This is an example project for the Django tests"
+        p.save()
+        self.assertEqual(p.id, 1)
+
+    def test_stack_insertion(self):
+        s = Stack()
+        s.title = "Example Stack"
+        s.image_base = "http://incf.ini.uzh.ch/image-stack-fib/"
+        s.trakem2_project = False
+        s.save()
+        self.assertEqual(s.id, 1)
+        # Now try to associate this stack with the project:
+        p = Project.objects.get(pk=1)
+        self.assertTrue(p)
+        p.stacks.add(s)
+        self.assertEqual(len(self.stacks))

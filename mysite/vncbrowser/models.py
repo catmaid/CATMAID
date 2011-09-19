3bce312c1fd1662b7b6152533b01fb9cf5a1dc2e
Mark Longair <longair@ini.phys.ethz.ch>
2011-09-19 18:05:32 +0200

    The field name may not end in an underscore

--- mysite/vncbrowser/models.py
+++ mysite/vncbrowser/models.py
@@ -112,7 +112,7 @@ class ClassInstance(models.Model):
     edition_time = models.DateTimeField(default=now)
     project = models.ForeignKey(Project)
     # Default=Now new columns:
-    class_ = models.ForeignKey(Class, db_column="class") # underscore since class is a keyword
+    class_column = models.ForeignKey(Class, db_column="class") # underscore since class is a keyword
     name = models.CharField(max_length=255)
 
 

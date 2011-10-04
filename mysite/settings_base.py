a33ee9bc27e6c128e7b295aa906c82145fdf8cf6
Mark Longair <mhl@pobox.com>
2011-10-04 22:12:05 +0200

    Put back settings system from the old version

---
+++ mysite/settings_base.py
@@ -0,0 +1,56 @@
+# Django settings for mysite project.
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'mysite.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'mysite.vncbrowser',
+    'django.contrib.admin',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = ('mysite.context_processors.staticfiles',
+                               'django.contrib.auth.context_processors.auth')
+
+URL_PREFIX = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = URL_PREFIX + 'accounts/login'

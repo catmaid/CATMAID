16769c6eec791358e217bb311934930269abc212
Mark Longair <longair@ini.phys.ethz.ch>
2011-11-22 15:35:44 +0100

    Simplify the queries in httpdocs/model/node.list.php
    PostgreSQL's query planner sometimes chooses a very poor query
    plan for the first of these queries.  This commit simplifies
    the query to aid in debugging, although it doesn't actually
    fix the bad query plan issue.

--- httpdocs/model/node.list.php
+++ httpdocs/model/node.list.php
@@ -116,31 +116,34 @@ $zbound = 1.0;
 // limit number of retrieved treenodes
 $limit = 400;
 
+$classes = $db->getMap( $pid, 'class' );
+if (!$classes) {
+    echo makeJSON( array( 'error' => "Could not find classes for project $pid" ) );
+    return;
+}
+error_log("got classes map ".print_r($classes, TRUE));
 
+$relations = $db->getMap( $pid, 'relation' );
+if (!$relations) {
+    echo makeJSON( array( 'error' => "Could not find relations for project $pid" ) );
+    return;
+}
 
-// need relation: model_of, presynaptic_to, postsynaptic_to
-// need class: synapse, presynaptic terminal, postsynaptic terminal
-// retrieve class ids
-$syn = $db->getClassId( $pid, "synapse" );
-if(!$syn) { echo makeJSON( array( 'error' => 'Can not find "synapse" class for this project' ) ); return; }
-
-$presyn = $db->getClassId( $pid, "presynaptic terminal" );
-if(!$syn) { echo makeJSON( array( 'error' => 'Can not find "presynaptic terminal" class for this project' ) ); return; }
-
-$postsyn = $db->getClassId( $pid, "postsynaptic terminal" );
-if(!$syn) { echo makeJSON( array( 'error' => 'Can not find "postsynaptic terminal" class for this project' ) ); return; }
-
-
-// relation ids
-$model_of = $db->getRelationId( $pid, "model_of" );
-if(!$model_of) { echo makeJSON( array( 'error' => 'Can not find "model_of" relation for this project' ) ); return; }
-
-$presyn_to = $db->getRelationId( $pid, "presynaptic_to" );
-if(!$presyn_to) { echo makeJSON( array( 'error' => 'Can not find "presynaptic_to" relation for this project' ) ); return; }
-
-$postsyn_to = $db->getRelationId( $pid, "postsynaptic_to" );
-if(!$postsyn_to) { echo makeJSON( array( 'error' => 'Can not find "postsynaptic_to" relation for this project' ) ); return; }
-
+// Check that this returned all the required IDs:
+$required_classes = array('synapse', 'presynaptic terminal', 'postsynaptic terminal', 'skeleton');
+$required_relations = array('model_of', 'presynaptic_to', 'postsynaptic_to', 'element_of');
+foreach ($required_classes as $class) {
+    if (!array_key_exists($class, $classes)) {
+        echo makeJSON( array( 'error' => "Failed to find the required class '$class'" ) );
+        return;
+    }
+}
+foreach ($required_relations as $relation) {
+    if (!array_key_exists($relation, $relations)) {
+        echo makeJSON( array( 'error' => "Failed to find the required relation '$relation'" ) );
+        return;
+    }
+}
 
 // Start transaction
 if (! $db->begin() ) {
@@ -151,7 +154,7 @@ if (! $db->begin() ) {
 try {
 
   $treenodes = $db->getResult(
-    'SELECT treenode.id AS id,
+    "SELECT treenode.id AS id,
          treenode.parent_id AS parentid,
          (treenode.location).x AS x,
          (treenode.location).y AS y,
@@ -159,22 +162,22 @@ try {
          treenode.confidence AS confidence,
          treenode.user_id AS user_id,
          treenode.radius AS radius,
-         ((treenode.location).z - '.$z.') AS z_diff,
+         ((treenode.location).z - $z) AS z_diff,
          treenode_class_instance.class_instance_id AS skeleton_id
-     FROM (treenode INNER JOIN relation ON (relation.relation_name = \'element_of\' AND relation.project_id = treenode.project_id))
+     FROM treenode
          LEFT OUTER JOIN (treenode_class_instance
-                          INNER JOIN (class_instance INNER JOIN class ON class_instance.class_id = class.id AND class.class_name = \'skeleton\')
-                          ON treenode_class_instance.class_instance_id = class_instance.id)
-         ON (treenode_class_instance.treenode_id = treenode.id AND treenode_class_instance.relation_id = relation.id)
-     WHERE treenode.project_id = '.$pid.'
-      AND (treenode.location).x >= '.$left.'
-      AND (treenode.location).x <= '.( $left + $width ).'
-      AND (treenode.location).y >= '.$top.'
-      AND (treenode.location).y <= '.( $top + $height ).'
-      AND (treenode.location).z >= '.$z.' - '.$zbound.' * '.$zres.'
-      AND (treenode.location).z <= '.$z.' + '.$zbound.' * '.$zres.'
+                          INNER JOIN class_instance
+                          ON treenode_class_instance.class_instance_id = class_instance.id AND class_instance.class_id = {$classes['skeleton']})
+         ON (treenode_class_instance.treenode_id = treenode.id AND treenode_class_instance.relation_id = {$relations['element_of']})
+     WHERE treenode.project_id = $pid
+      AND (treenode.location).x >= $left
+      AND (treenode.location).x <= ($left + $width)
+      AND (treenode.location).y >= $top
+      AND (treenode.location).y <= ($top + $height)
+      AND (treenode.location).z >= ($z - $zbound * $zres)
+      AND (treenode.location).z <= ($z + $zbound * $zres)
       ORDER BY parentid DESC, id, z_diff
-      LIMIT '.$limit
+      LIMIT $limit"
   );
 
   if (false === $treenodes) {
@@ -191,29 +194,29 @@ try {
   // the treenode_connector table, so that we get entries even if the
   // connector is not connected to any treenodes
   $connectors = $db->getResult(
-    'SELECT connector.id AS id,
+    "SELECT connector.id AS id,
         (connector.location).x AS x,
         (connector.location).y AS y,
         (connector.location).z AS z,
         connector.user_id AS user_id,
-        ( (connector.location).z - '.$z.' ) AS z_diff,
+        ((connector.location).z - $z) AS z_diff,
         treenode_connector.relation_id AS treenode_relation_id,
         treenode_connector.treenode_id AS tnid
      FROM connector_class_instance AS lci, class_instance AS ci, connector
          LEFT OUTER JOIN treenode_connector ON treenode_connector.connector_id = connector.id
-        WHERE connector.project_id = '.$pid.' AND
-            (connector.location).x >= '.$left.' AND
-            (connector.location).x <= '.( $left + $width ).' AND
-            (connector.location).y >= '.$top.' AND
-            (connector.location).y <= '.( $top + $height ).' AND
-            (connector.location).z >= '.$z.' - '.$zbound.' * '.$zres.' AND
-            (connector.location).z <= '.$z.' + '.$zbound.' * '.$zres.' AND
+        WHERE connector.project_id = $pid AND
+            (connector.location).x >= $left AND
+            (connector.location).x <= ($left + $width) AND
+            (connector.location).y >= $top AND
+            (connector.location).y <= ($top + $height) AND
+            (connector.location).z >= ($z - $zbound * $zres) AND
+            (connector.location).z <= ($z + $zbound * $zres) AND
             connector.id = lci.connector_id AND
             ci.id = lci.class_instance_id AND
-            lci.relation_id = '.$model_of.' AND
-            ci.class_id = '.$syn.'
+            lci.relation_id = {$relations['model_of']} AND
+            ci.class_id = {$classes['synapse']}
 
-        ORDER BY id, z_diff LIMIT '.$limit
+        ORDER BY id, z_diff LIMIT $limit"
   );
 
   if (false === $connectors) {
@@ -229,7 +232,7 @@ try {
 
       if (isset($val['tnid'])) {
           $tnid = $val['tnid'];
-          $relationship = ($val['treenode_relation_id'] === $presyn_to) ? "pre" : "post";
+          $relationship = ($val['treenode_relation_id'] === $relations['presynaptic_to']) ? "pre" : "post";
       } else {
           // The connector wasn't connected to any treenodes
           $tnid = NULL;

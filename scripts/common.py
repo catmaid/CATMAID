efa6f4b6f5074b152cf0f573b6ff026842ce5e8c
Mark Longair <longair@ini.phys.ethz.ch>
2011-12-01 15:44:35 +0100

    The CATMAID_CONFIGURATION env. variable can override ~/.catmaid-db for the Python scripts

--- scripts/common.py
+++ scripts/common.py
@@ -3,15 +3,20 @@ import psycopg2
 import os
 import yaml
 
+if 'CATMAID_CONFIGURATION' in os.environ:
+    path = os.environ['CATMAID_CONFIGURATION']
+else:
+    path = os.path.join(os.environ['HOME'], '.catmaid-db')
+
 try:
-    conf = yaml.load(open(os.path.join(os.environ['HOME'], '.catmaid-db')))
+    conf = yaml.load(open(path))
 except:
-    print >> sys.stderr, '''Your ~/.catmaid-db file should look like:
+    print >> sys.stderr, '''Your %s file should look like:
 
 host: localhost
 database: catmaid
 username: catmaid_user
-password: password_of_your_catmaid_user'''
+password: password_of_your_catmaid_user''' % (path,)
     sys.exit(1)
 
 db_connection = psycopg2.connect(host=conf['host'],

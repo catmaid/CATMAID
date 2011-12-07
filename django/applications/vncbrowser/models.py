catmaid/CATMAID

commit 2dadf996f2aa7d96c13a6f77fb8f665dfb1a99ef

The related name class_instances_[ab] was confusing; change to cici_via_[ab]

django/applications/vncbrowser/models.py
@@ -178,22 +178,22 @@ class ClassInstance(models.Model):
         synapses = ClassInstance.objects.filter(
             class_column__class_name='synapse',
             project__id=project_id,
-            class_instances_b__relation__relation_name=this_to_syn+'synaptic_to',
-            class_instances_b__class_instance_a__class_column__class_name=this_to_syn+'synaptic terminal',
-            class_instances_b__class_instance_a__class_instances_a__relation__relation_name='part_of',
-            class_instances_b__class_instance_a__class_instances_a__class_instance_b__class_column__class_name='skeleton',
-            class_instances_b__class_instance_a__class_instances_a__class_instance_b__class_instances_a__relation__relation_name='model_of',
-            class_instances_b__class_instance_a__class_instances_a__class_instance_b__class_instances_a__class_instance_b=self.id)
+            cici_via_b__relation__relation_name=this_to_syn+'synaptic_to',
+            cici_via_b__class_instance_a__class_column__class_name=this_to_syn+'synaptic terminal',
+            cici_via_b__class_instance_a__cici_via_a__relation__relation_name='part_of',
+            cici_via_b__class_instance_a__cici_via_a__class_instance_b__class_column__class_name='skeleton',
+            cici_via_b__class_instance_a__cici_via_a__class_instance_b__cici_via_a__relation__relation_name='model_of',
+            cici_via_b__class_instance_a__cici_via_a__class_instance_b__cici_via_a__class_instance_b=self.id)
 
         connected_neurons = ClassInstance.objects.filter(
             class_column__class_name='neuron',
             project__id=project_id,
-            class_instances_b__relation__relation_name='model_of',
-            class_instances_b__class_instance_a__class_column__class_name='skeleton',
-            class_instances_b__class_instance_a__class_instances_b__relation__relation_name='part_of',
-            class_instances_b__class_instance_a__class_instances_b__class_instance_a__class_column__class_name=syn_to_con+'synaptic terminal',
-            class_instances_b__class_instance_a__class_instances_b__class_instance_a__class_instances_a__relation__relation_name=syn_to_con+'synaptic_to',
-            class_instances_b__class_instance_a__class_instances_b__class_instance_a__class_instances_a__class_instance_b__id__in=[s.id for s in list(synapses)])
+            cici_via_b__relation__relation_name='model_of',
+            cici_via_b__class_instance_a__class_column__class_name='skeleton',
+            cici_via_b__class_instance_a__cici_via_b__relation__relation_name='part_of',
+            cici_via_b__class_instance_a__cici_via_b__class_instance_a__class_column__class_name=syn_to_con+'synaptic terminal',
+            cici_via_b__class_instance_a__cici_via_b__class_instance_a__cici_via_a__relation__relation_name=syn_to_con+'synaptic_to',
+            cici_via_b__class_instance_a__cici_via_b__class_instance_a__cici_via_a__class_instance_b__id__in=[s.id for s in list(synapses)])
 
         return connected_neurons.values('id','name').annotate(models.Count('id')).order_by('-id__count')
 
@@ -210,8 +210,8 @@ class ClassInstance(models.Model):
     def cell_body_location(self):
         qs = list(ClassInstance.objects.filter(
                 class_column__class_name='cell_body_location',
-                class_instances_b__relation__relation_name='has_cell_body',
-                class_instances_b__class_instance_a=self))
+                cici_via_b__relation__relation_name='has_cell_body',
+                cici_via_b__class_instance_a=self))
         if len(qs) == 0:
             return 'Unknown'
         elif len(qs) == 1:
@@ -225,8 +225,8 @@ class ClassInstance(models.Model):
             raise Exception, "Incorrect cell body location '%s'" % (new_location,)
         # Just delete the ClassInstance - ON DELETE CASCADE should deal with the rest:
         ClassInstance.objects.filter(
-            class_instances_b__relation__relation_name='has_cell_body',
-            class_instances_b__class_instance_a=self).delete()
+            cici_via_b__relation__relation_name='has_cell_body',
+            cici_via_b__class_instance_a=self).delete()
         if new_location != 'Unknown':
             location = ClassInstance()
             location.name=new_location
@@ -293,10 +293,10 @@ class ClassInstanceClassInstance(models.Model):
     relation = models.ForeignKey(Relation)
     # Now new columns:
     class_instance_a = models.ForeignKey(ClassInstance,
-                                         related_name='class_instances_a',
+                                         related_name='cici_via_a',
                                          db_column='class_instance_a')
     class_instance_b = models.ForeignKey(ClassInstance,
-                                         related_name='class_instances_b',
+                                         related_name='cici_via_b',
                                          db_column='class_instance_b')
 
 class BrokenSlice(models.Model):

django/applications/vncbrowser/tests.py
@@ -464,8 +464,8 @@ class TreenodeTests(TestCase):
                                            class_column__class_name='neuron')
         skeleton = ClassInstance.objects.get(
             class_column__class_name='skeleton',
-            class_instances_a__relation__relation_name='model_of',
-            class_instances_a__class_instance_b=neuron)
+            cici_via_a__relation__relation_name='model_of',
+            cici_via_a__class_instance_b=neuron)
 
         tns = Treenode.objects.filter(
             treenodeclassinstance__class_instance=skeleton).order_by('id')

django/applications/vncbrowser/views/common.py
@@ -79,8 +79,8 @@ def get_form_and_neurons(request, project_id, kwargs):
         location = cell_body_choices_dict[cell_body_location]
         all_neurons = all_neurons.filter(
             project__id=project_id,
-            class_instances_a__relation__relation_name='has_cell_body',
-            class_instances_a__class_instance_b__name=location)
+            cici_via_a__relation__relation_name='has_cell_body',
+            cici_via_a__class_instance_b__name=location)
 
     cici_qs = ClassInstanceClassInstance.objects.filter(
         project__id=project_id,

django/applications/vncbrowser/views/views.py
@@ -67,8 +67,8 @@ def view(request, project_id=None, neuron_id=None, neuron_name=None, logged_in_u
 
     lines = ClassInstance.objects.filter(
         project=p,
-        class_instances_a__class_instance_b=n,
-        class_instances_a__relation__relation_name='expresses_in').all()
+        cici_via_a__class_instance_b=n,
+        cici_via_a__relation__relation_name='expresses_in').all()
 
     outgoing = n.all_neurons_downstream(project_id)
     incoming = n.all_neurons_upstream(project_id)
@@ -78,9 +78,9 @@ def view(request, project_id=None, neuron_id=None, neuron_name=None, logged_in_u
 
     skeletons = ClassInstance.objects.filter(
         project=p,
-        class_instances_a__relation__relation_name='model_of',
+        cici_via_a__relation__relation_name='model_of',
         class_column__class_name='skeleton',
-        class_instances_a__class_instance_b=n)
+        cici_via_a__class_instance_b=n)
 
     return my_render_to_response(request,
                                  'vncbrowser/view.html',
@@ -113,8 +113,8 @@ def line(request, project_id=None, line_id=None, logged_in_user=None):
     p = get_object_or_404(Project, pk=project_id)
     l = get_object_or_404(ClassInstance, pk=line_id, project=p, class_column__class_name='driver_line')
     sorted_neurons = ClassInstance.objects.filter(
-        class_instances_b__relation__relation_name='expresses_in',
-        class_instances_b__class_instance_a=l).order_by('name')
+        cici_via_b__relation__relation_name='expresses_in',
+        cici_via_b__class_instance_a=l).order_by('name')
     return my_render_to_response(request,
                                  'vncbrowser/line.html',
                                  {'line': l,
@@ -209,6 +209,6 @@ def neuron_to_skeletons(request, project_id=None, neuron_id=None, logged_in_user
                                project=p)
     qs = ClassInstance.objects.filter(
         project=p,
-        class_instances_a__relation__relation_name='model_of',
-        class_instances_a__class_instance_b=neuron)
+        cici_via_a__relation__relation_name='model_of',
+        cici_via_a__class_instance_b=neuron)
     return HttpResponse(json.dumps([x.id for x in qs]), mimetype="text/json")

commit eddabdd78f2cd72f17de29bc916279c85c9cfaf7

Reduce the number of joins required to find upstream / downstream neurons
This fix is required because of a performance problem introduced
by 2dadf996f2aa7d96c13a6f77fb8f665dfb1a99ef.  That commit only
changed the related_name of two fields, but seems to be causing
a different query to be generated.  For example, the bad query is:

    [sql] SELECT ...
      FROM "class_instance"
      INNER JOIN "class_instance_class_instance" ON ("class_instance"."id" = "class_instance_class_instance"."class_instance_b")
      INNER JOIN "class_instance" T4 ON ("class_instance_class_instance"."class_instance_a" = T4."id")
      INNER JOIN "class_instance_class_instance" T5 ON (T4."id" = T5."class_instance_a")
      INNER JOIN "class_instance" T6 ON (T5."class_instance_b" = T6."id")
      INNER JOIN "class_instance_class_instance" T7 ON (T6."id" = T7."class_instance_a")
      INNER JOIN "relation" ON (T7."relation_id" = "relation"."id")
      INNER JOIN "relation" T9 ON ("class_instance_class_instance"."relation_id" = T9."id")
      INNER JOIN "relation" T10 ON (T5."relation_id" = T10."id")
      INNER JOIN "class" ON (T4."class_id" = "class"."id")
      INNER JOIN "class" T12 ON ("class_instance"."class_id" = T12."id")
      INNER JOIN "class_instance" T13 ON (T5."class_instance_b" = T13."id")
      INNER JOIN "class" T14 ON (T13."class_id" = T14."id")
      INNER JOIN "class_instance" T15 ON (T5."class_instance_b" = T15."id")
      INNER JOIN "class_instance_class_instance" T16 ON (T15."id" = T16."class_instance_a")
      WHERE ("class_instance"."project_id" = 4
             AND "relation"."relation_name" = model_of
             AND T9."relation_name" = presynaptic_to
             AND T10."relation_name" = part_of
             AND "class"."class_name" = presynaptic terminal
             AND T12."class_name" = synapse
             AND T14."class_name" = skeleton
             AND T16."class_instance_b" = 14638336)

... while the good query is:

    [sql] SELECT ...

django/applications/vncbrowser/models.py
@@ -175,24 +175,27 @@ class ClassInstance(models.Model):
         else:
             raise Exception, "Unknown connectivity direction: "+str(direction)
 
+        relations = dict((r.relation_name, r.id) for r in Relation.objects.filter(project=project_id))
+        classes = dict((c.class_name, c.id) for c in Class.objects.filter(project=project_id))
+
         synapses = ClassInstance.objects.filter(
             class_column__class_name='synapse',
             project__id=project_id,
-            cici_via_b__relation__relation_name=this_to_syn+'synaptic_to',
-            cici_via_b__class_instance_a__class_column__class_name=this_to_syn+'synaptic terminal',
-            cici_via_b__class_instance_a__cici_via_a__relation__relation_name='part_of',
-            cici_via_b__class_instance_a__cici_via_a__class_instance_b__class_column__class_name='skeleton',
-            cici_via_b__class_instance_a__cici_via_a__class_instance_b__cici_via_a__relation__relation_name='model_of',
+            cici_via_b__relation=relations[this_to_syn+'synaptic_to'],
+            cici_via_b__class_instance_a__class_column=classes[this_to_syn+'synaptic terminal'],
+            cici_via_b__class_instance_a__cici_via_a__relation=relations['part_of'],
+            cici_via_b__class_instance_a__cici_via_a__class_instance_b__class_column=classes['skeleton'],
+            cici_via_b__class_instance_a__cici_via_a__class_instance_b__cici_via_a__relation=relations['model_of'],
             cici_via_b__class_instance_a__cici_via_a__class_instance_b__cici_via_a__class_instance_b=self.id)
 
         connected_neurons = ClassInstance.objects.filter(
             class_column__class_name='neuron',
             project__id=project_id,
-            cici_via_b__relation__relation_name='model_of',
-            cici_via_b__class_instance_a__class_column__class_name='skeleton',
-            cici_via_b__class_instance_a__cici_via_b__relation__relation_name='part_of',
-            cici_via_b__class_instance_a__cici_via_b__class_instance_a__class_column__class_name=syn_to_con+'synaptic terminal',
-            cici_via_b__class_instance_a__cici_via_b__class_instance_a__cici_via_a__relation__relation_name=syn_to_con+'synaptic_to',
+            cici_via_b__relation=relations['model_of'],
+            cici_via_b__class_instance_a__class_column=classes['skeleton'],
+            cici_via_b__class_instance_a__cici_via_b__relation=relations['part_of'],
+            cici_via_b__class_instance_a__cici_via_b__class_instance_a__class_column=classes[syn_to_con+'synaptic terminal'],
+            cici_via_b__class_instance_a__cici_via_b__class_instance_a__cici_via_a__relation=relations[syn_to_con+'synaptic_to'],
             cici_via_b__class_instance_a__cici_via_b__class_instance_a__cici_via_a__class_instance_b__id__in=[s.id for s in list(synapses)])
 
         return connected_neurons.values('id','name').annotate(models.Count('id')).order_by('-id__count')
cbdba65d4517a68438d06aa35d97732e6214198c
Mark Longair <mhl@pobox.com>
2011-12-09 13:49:17 +0100

    Exclude the native code based functions from the database dump

--- scripts/dump-database-schema.sh
+++ scripts/dump-database-schema.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-pg_dump --no-privileges --schema-only --no-owner --no-tablespaces catmaid -U catmaid_user | egrep -v '^--' | egrep -v '^ *$'
-
+pg_dump --no-privileges --schema-only --no-owner \
+   --no-tablespaces catmaid -U catmaid_user | \
+   egrep -v '^--' | \
+   egrep -v '^ *$' | \
+   sed -e '/CREATE FUNCTION connect/,+2d'

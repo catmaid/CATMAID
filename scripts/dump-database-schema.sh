9df9ce8c125884b1f0c8d6af2480fe0d683d0b7c
Stephan Gerhard <git@unidesign.ch>
2011-12-20 16:30:25 +0100

    Usage information for database dump scripts

--- scripts/dump-database-schema.sh
+++ scripts/dump-database-schema.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <DATABASE-NAME>"
+  exit 1
+fi  
+
 pg_dump --no-privileges --schema-only --no-owner \
-   --no-tablespaces catmaid -U catmaid_user | \
+   --no-tablespaces $1 -U catmaid_user | \
    egrep -v '^--' | \
    egrep -v '^ *$' | \
    sed -e '/CREATE FUNCTION connect/,+2d'

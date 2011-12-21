catmaid/CATMAID

commit 675d047ff8bd6fb79a0ad3c29f25d0d15c20b090

Add natural sorting for projects and sort the stacks, too

httpdocs/model/project.list.php
@@ -31,7 +31,7 @@ if ( $ses->isSessionValid() )
 			WHERE	"project_user"."user_id" = '.$ses->getId().' OR
 					"project"."public"
 					
-			ORDER BY "ptitle"'
+			ORDER BY "ptitle", "stitle"'
 	);
 }
 else
@@ -51,7 +51,7 @@ else
 			
 			WHERE	"project"."public"
 					
-			ORDER BY "ptitle"'
+			ORDER BY "ptitle", "stitle"'
 	);
 }
 /*
@@ -103,6 +103,19 @@ foreach ( $pprojects as $p )
 			'action' => 'javascript:openProjectStack( '.$p[ 'pid' ].', '.$p[ 'sid' ].' )',
 			'note' => '' );
 }
+
+// a sorting function to sort by title
+function sortByTitle($a, $b) {
+	$title1 = (string)($a['title']);
+	$title2 = (string)($b['title']);
+    return strnatcasecmp($title1, $title2);
+}
+
+if ( $projects ) {
+	// sort the projects
+	usort($projects, 'sortByTitle');
+}
+
 /*
 foreach ( $private_projects as $p )
 {

commit 10e06d4dbe028cebd66bdec2e2257c8f94966c0d

A quick fix for the "URL to this view" links not working
The commit 675d047ff8bd6fb79a0ad3c29f25d0d15c20b090 broke
the following of URLs to a particular view.  This was because
the use of PHP's usort to sort the array of projects loses
the old array keys (which where the project IDs) and
renumbered each project array to 0, 1, 2, etc.  The logic
in handle_updateProjects still assumed that they keys are
project IDs and then failed to set keep_project_alive to
true.

The simplest fix (introduced in this commit) is just to add
a pid member to each project array, and change
handle_updateProjects to use that field for comparison.

Fixes #208.

httpdocs/init.js
@@ -226,7 +226,7 @@ function handle_updateProjects(status, text, xml) {
       alert(e.error);
     } else {
       for (var i in e) {
-        if (project && project.id == i) {
+        if (project && project.id == e[i].pid) {
           keep_project_alive = true;
           keep_project_editable = e[i].editable;
         }

httpdocs/model/project.list.php
@@ -90,6 +90,7 @@ foreach ( $pprojects as $p )
 	if ( !isset( $projects[ $p[ 'pid' ] ] ) )
 	{
 		$projects[ $p[ 'pid' ] ] = array(
+				'pid'		=> $p[ 'pid' ],
 				'title'		=> $p[ 'ptitle' ],
 				'public_project'	=> $p[ 'public' ] == 't',
 				'action'	=> array(),
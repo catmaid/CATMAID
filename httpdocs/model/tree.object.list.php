c01445b60f7d77744e485cec3b6ac9db16eaf0e1
Mark Longair <longair@ini.phys.ethz.ch>
2011-12-21 17:53:43 +0100

    Work-in-progress on a special case for the "Isolated synaptic terminals" group

--- httpdocs/model/tree.object.list.php
+++ httpdocs/model/tree.object.list.php
@@ -14,6 +14,15 @@ $pid = isset( $_REQUEST[ 'pid' ] ) ? intval( $_REQUEST[ 'pid' ] ) : 0;
 $uid = $ses->isSessionValid() ? $ses->getId() : 0;
 
 $parentid = isset( $_REQUEST[ 'parentid' ] ) ? intval($_REQUEST[ 'parentid' ]) : 0;
+$parentname = isset( $_REQUEST[ 'parentname' ] ) ? $_REQUEST[ 'parentname' ] : "";
+
+if (isset($_REQUEST['expandtarget'])) {
+    $expandRequest = array_map("intval",
+                               explode(',', $_REQUEST['expandtarget']));
+} else {
+    $expandRequest = array();
+}
+
 // extend it by giving a set of relationship types
 // limit number of nodes retrievable
 $maxnodes = 1000;
@@ -31,23 +40,33 @@ if ( ! $uid ) {
 	return;
 }
 
+$classes = $db->getMap( $pid, 'class' );
+if (!$classes) {
+    echo makeJSON( array( 'error' => "Could not find classes for project $pid" ) );
+    return;
+}
 
-// instances to display
-$nid = $db->getClassId( $pid, "neuron" );
-if(!$nid) { echo makeJSON( array( 'error' => 'Can not find "neuron" class for this project' ) ); return; }
-$skid = $db->getClassId( $pid, "skeleton" );
-if(!$skid) { echo makeJSON( array( 'error' => 'Can not find "skeleton" class for this project' ) ); return; }
-$gid = $db->getClassId( $pid, "group" );
-if(!$gid) { echo makeJSON( array( 'error' => 'Can not find "group" class for this project' ) ); return; }
-$rid = $db->getClassId( $pid, "root" );
-if(!$rid) { echo makeJSON( array( 'error' => 'Can not find "root" class for this project' ) ); return; }
+$relations = $db->getMap( $pid, 'relation' );
+if (!$relations) {
+    echo makeJSON( array( 'error' => "Could not find relations for project $pid" ) );
+    return;
+}
 
-// relations
-$modid = $db->getRelationId( $pid, "model_of" );
-if(!$modid) { echo makeJSON( array( 'error' => 'Can not find "model_of" relation for this project' ) ); return; }
-$partof_id = $db->getRelationId( $pid, "part_of" );
-if(!$partof_id) { echo makeJSON( array( 'error' => 'Can not find "part_of" relation for this project' ) ); return; }
-
+// Check that this returned all the required IDs:
+$required_classes = array('neuron', 'skeleton', 'group', 'root');
+$required_relations = array('model_of', 'part_of');
+foreach ($required_classes as $class) {
+    if (!array_key_exists($class, $classes)) {
+        echo makeJSON( array( 'error' => "Failed to find the required class '$class'" ) );
+        return;
+    }
+}
+foreach ($required_relations as $relation) {
+    if (!array_key_exists($relation, $relations)) {
+        echo makeJSON( array( 'error' => "Failed to find the required relation '$relation'" ) );
+        return;
+    }
+}
 
 # Just one select query, no transaction needed:
 if ( !$parentid ) {
@@ -55,7 +74,7 @@ if ( !$parentid ) {
   $res = $db->getResult('SELECT "ci"."id", "ci"."name"
                          FROM "class_instance" AS "ci" 
                          WHERE "ci"."project_id" = '.$pid.'
-                         AND "ci"."class_id" = '.$rid);
+                         AND "ci"."class_id" = '.$classes['root']);
 
   if (false === $res) {
     emitErrorAndExit($db, 'Could not select the id of the root node.');
@@ -79,25 +98,122 @@ if ( !$parentid ) {
   echo $sOutput;
   return;
 }
-  
-  
-# Just one select query, no transaction needed:
-$res = $db->getResult(
-  'SELECT "ci"."id", "ci"."name", "ci"."class_id", "user"."name" AS "username",
-          "cici"."relation_id", "cici"."class_instance_b" AS "parent", "cl"."class_name"
-  FROM "class_instance" AS "ci"
-  INNER JOIN "class_instance_class_instance" AS "cici" 
-    ON "ci"."id" = "cici"."class_instance_a" 
-    INNER JOIN "class" AS "cl" 
-      ON "ci"."class_id" = "cl"."id"
-      INNER JOIN "user"
-      ON "ci"."user_id" = "user"."id"
-  WHERE "ci"."project_id" = '.$pid.' AND
-     "cici"."class_instance_b" = '.$parentid.' AND
-     ("cici"."relation_id" = '.$modid.'
-    OR "cici"."relation_id" = '.$partof_id.')
-  ORDER BY "ci"."creation_time"
-  LIMIT '.$maxnodes);
+
+if (strpos($parentname, "Isolated synaptic terminals")) {
+
+    $res = $db->getResult(
+        "SELECT count(tci.id) as treenodes,
+                ci.id,
+                ci.name,
+                ci.class_id, cici.relation_id,
+                cici.class_instance_b AS parent,
+                sk.id AS skeleton_id,
+                u.name AS username,
+                cl.class_name
+         FROM class_instance ci,
+              class cl,
+              class_instance_class_instance cici,
+              class_instance_class_instance modof,
+              class_instance sk,
+              treenode_class_instance tci,
+              \"user\" u
+         WHERE cici.class_instance_b = $parentid AND
+               cici.class_instance_a = ci.id AND
+               cl.id = ci.class_id AND
+               modof.class_instance_b = cici.class_instance_a AND
+               modof.relation_id = {$relations['model_of']} AND
+               sk.id = modof.class_instance_a AND
+               tci.class_instance_id = sk.id AND
+               tci.relation_id = {$relations['element_of']} AND
+               u.id = ci.user_id AND
+               ci.project_id = $pid
+         GROUP BY ci.id,
+                  ci.name,
+                  ci.class_id,
+                  cici.relation_id,
+                  cici.class_instance_b,
+                  skeleton_id,
+                  u.name,
+                  cl.class_name
+         HAVING count(tci.id) > 1");
+
+    if ($res === FALSE) {
+        echo makeJSON( array( 'error' => "Failed to find children of the Isolated synaptic terminals" ) );
+        return;
+    }
+
+    // If this list is part of an expansion caused by selecting a
+    // particular skeleton that is part of a neuron that is in the
+    // 'Isolated synaptic terminals', add that to the results.
+    $isolated_group_index = array_search($parentid, $expandRequest);
+
+    error_log("got isolated_group_index $isolated_group_index");
+    error_log("got count(expandRequest)".count($expandRequest));
+
+    if (($isolated_group_index !== FALSE) &&
+        ($isolated_group_index < count($expandRequest))) {
+
+        $neuron_id = $expandRequest[$isolated_group_index + 1];
+
+        $extra_res = $db->getResult(
+            "SELECT ci.id,
+                    ci.name,
+                    ci.class_id,
+                    u.name AS username,
+                    cici.relation_id,
+                    cici.class_instance_b AS parent,
+                    cl.class_name
+             FROM class_instance AS ci
+               INNER JOIN class_instance_class_instance AS cici
+                 ON ci.id = cici.class_instance_a
+               INNER JOIN class AS cl
+                 ON ci.class_id = cl.id
+               INNER JOIN \"user\" AS u
+                 ON ci.user_id = u.id
+             WHERE ci.id = $neuron_id AND
+                   ci.project_id = $pid AND
+                   cici.class_instance_b = $parentid AND
+                   (cici.relation_id = ${relations['model_of']}
+                    OR cici.relation_id = ${relations['part_of']})
+             ORDER BY ci.creation_time
+             LIMIT $maxnodes");
+
+        error_log("got extra_res ".print_r($extra_res, TRUE));
+
+        if ($extra_res === FALSE) {
+            echo makeJSON( array( 'error' => "Failed to find the requested neuron" ) );
+            return;
+        }
+
+        $res = array_merge($res, $extra_res);
+    }
+
+} else {
+
+  # Just one select query, no transaction needed:
+  $res = $db->getResult(
+      "SELECT ci.id,
+              ci.name,
+              ci.class_id,
+              \"user\".name AS username,
+              cici.relation_id,
+              cici.class_instance_b AS parent,
+              cl.class_name
+       FROM class_instance AS ci
+         INNER JOIN class_instance_class_instance AS cici
+           ON ci.id = cici.class_instance_a
+         INNER JOIN class AS cl
+           ON ci.class_id = cl.id
+         INNER JOIN \"user\"
+          ON ci.user_id = \"user\".id
+       WHERE ci.project_id = $pid AND
+             cici.class_instance_b = $parentid AND
+             (cici.relation_id = {$relations['model_of']}
+              OR cici.relation_id = {$relations['part_of']})
+       ORDER BY ci.creation_time
+       LIMIT $maxnodes");
+
+}
 
 # Loop through the array and generate children to return
 $sOutput = '[';

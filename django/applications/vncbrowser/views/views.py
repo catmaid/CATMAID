56ce4f8fb8b50770fb1a5b5b004894b35f8605f8
Stephan Gerhard <git@unidesign.ch>
2012-01-10 11:49:22 +0100

    Remove merge shred

--- django/applications/vncbrowser/views/views.py
+++ django/applications/vncbrowser/views/views.py
@@ -453,8 +453,6 @@ def neuron_to_skeletons(request, project_id=None, neuron_id=None, logged_in_user
         cici_via_a__class_instance_b=neuron)
     return HttpResponse(json.dumps([x.id for x in qs]), mimetype="text/json")
 
-<<<<<<< HEAD
-=======
 @catmaid_login_required
 def multiple_presynaptic_terminals(request, project_id=None, logged_in_user=None):
     p = get_object_or_404(Project, pk=project_id)

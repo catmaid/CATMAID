5d6ac30c00dc2cb88ffdf22f9d75f54660c68ef2
Mark Longair <longair@ini.phys.ethz.ch>
2012-01-19 19:29:11 +0100

    Add two more relevant checks to check-database.py

--- scripts/check-database.py
+++ scripts/check-database.py
@@ -25,7 +25,51 @@ if len(sys.argv) != 1:
 
 c = db_connection.cursor()
 
-# First, go through each skeleton - check that it has only one node
+# ------------------------------------------------------------------------
+# Find all empty skeletons: first, find all the skeletons in the project:
+
+c.execute("""
+SELECT ci.id
+   FROM (class_instance ci INNER JOIN class c ON c.id = ci.class_id AND c.class_name = 'skeleton')
+   WHERE c.class_name = 'skeleton'""")
+
+all_skeletons = set(x[0] for x in c.fetchall())
+
+# Now find all skeletons that have at least one skeleton:
+
+c.execute("""
+SELECT DISTINCT tci.class_instance_id
+   FROM (class_instance ci INNER JOIN class c ON c.id = ci.class_id AND c.class_name = 'skeleton')
+     INNER JOIN treenode_class_instance tci ON tci.class_instance_id = ci.id
+     INNER JOIN relation r ON tci.relation_id = r.id AND r.relation_name = 'element_of'""")
+
+skeletons_with_at_least_one_treenode = set(x[0] for x in c.fetchall())
+
+for skeleton_id in sorted((all_skeletons - skeletons_with_at_least_one_treenode)):
+    print "[skeleton %d] is empty (has no treenodes)" % (skeleton_id,)
+
+# ------------------------------------------------------------------------
+
+# Try to find any cases where the skeleton_id column of
+# treenode_connector table is inconsistent with the skeleton that the
+# treenode is an element_of.
+
+c.execute("""
+SELECT tc.treenode_id, tci.class_instance_id, tc.id, tc.connector_id, tc.skeleton_id
+  FROM treenode_connector tc, treenode_class_instance tci, relation er
+  WHERE tc.treenode_id = tci.treenode_id AND
+        tci.relation_id = er.id AND relation_name = 'element_of'
+        AND tc.skeleton_id != tci.class_instance_id""")
+
+for tid, skid, tcid, cid, tc_skid in c.fetchall():
+    context = "[treenode %d <-> connector %d]" % (tid, cid)
+    print context, "The treenode is an element_of skeleton %d, but the skeleton_id column of treenode_connector is set to %d" % (skid, tc_skid)
+
+sys.exit(0)
+
+# ------------------------------------------------------------------------
+
+# Go through each skeleton - check that it has only one node
 # for which the parent is NULL and that every other node has a parent
 # in the same skeleton.
 

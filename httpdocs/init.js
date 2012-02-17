70ad89e561792c6110759f094ced09f787e32bad
Stephan Gerhard <git@unidesign.ch>
2012-02-17 17:58:26 +0100

    TileServer type 2 do not have to be able to consume labels (but they should :)

--- httpdocs/init.js
+++ httpdocs/init.js
@@ -386,7 +386,8 @@ function handle_openProjectStack( status, text, xml )
 			project.setEditable( e.editable );
 
 			var labelupload = '';
-			if( e.tile_sourcetype === 2 ) {
+
+			if( e.hasOwnProperty('labelupload_url') && e.tile_source_type === 2 ) {
 			    labelupload = e.labelupload_url;
 			}
 

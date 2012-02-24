d38aca1514795ec15900714754cabddca1b7f027
Stephan Gerhard <git@unidesign.ch>
2012-02-24 15:34:24 +0100

    WebGL: Make incoming link cyan for better contrast

--- httpdocs/webglapp.js
+++ httpdocs/webglapp.js
@@ -166,7 +166,7 @@ function WebGLViewer(divID) {
 
     this.line_material[connectivity_types[0]] = new THREE.LineBasicMaterial( { color: 0xffff00, opacity: 1.0, linewidth: 3 } );
     this.line_material[connectivity_types[1]] = new THREE.LineBasicMaterial( { color: 0xff0000, opacity: 1.0, linewidth: 6 } )
-    this.line_material[connectivity_types[2]] = new THREE.LineBasicMaterial( { color: 0x0000ff, opacity: 1.0, linewidth: 6 } )
+    this.line_material[connectivity_types[2]] = new THREE.LineBasicMaterial( { color: 0x00f6ff, opacity: 1.0, linewidth: 6 } )
 
     this.original_vertices = skeleton_data.vertices;
     this.original_connectivity = skeleton_data.connectivity;

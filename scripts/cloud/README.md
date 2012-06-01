e5f4ac5ac0c1343ef14d187b9f3fe46022f6954d
Stephan Gerhard <git@unidesign.ch>
2012-06-01 12:20:20 +0200

    Update Amazon AWS launcher fabfile

--- scripts/cloud/README.md
+++ scripts/cloud/README.md
@@ -1,30 +1,33 @@
-# Catmaid AWS launcher
-
-byproduct of the INCF Hackathon at MIT (September 8, 2011)
-
-Rich Stoner
+CATMAID AWS Launcher
+====================
 
 Script Requirements: fabric, boto
 
-Based on install steps found at https://github.com/acardona/CATMAID
+    sudo pip install fabric
+    sudo pip install boto
 
-## Usage
+Based on install steps for CATMAID at https://github.com/acardona/CATMAID
 
-* Edit fabfile.py, 
-* Enter your AWS credentials, keypair, and desired 64-bit instance size (t1.micro, m1.large, etc)
-* run 'fab buildApp' from the command line
+# Create keypair
 
-The instance will boot, install, and configure a standalone catmaid install
+Usage
+-----
+
+* Edit configuration.py, 
+
+* run 'fab installCatmaid' from the command line
+
+The instance will (boot), install, and configure a standalone catmaid install
 
 * Once complete, the script will present the catmaid url: http://$ec2hostname/catmaid/
 
 Before loading example data, first visit http://$ec2hostname/catmaid/ -> an error saying 'no projects available', but prepopulates fields
 
-* After first visit, run 'fab installExampleData $ec2hostname'
+* After first visit, run 'fab installExampleProject $ec2hostname'
 * Log in using any username, demo username & password is 'gerhard'
 
 *Note: this configuration is highly insecure. Do not use in production!*
 
-## License 
-
-BSD
+Original Author: Rich Stoner, Byproduct of the INCF Hackathon at MIT (September 8, 2011)
+Modifications: Stephan Gerhard, June 2012
+BSD License
--- scripts/cloud/fabfile.py
+++ scripts/cloud/fabfile.py
@@ -12,142 +12,140 @@ from boto import ec2
 import sys, pprint, time
 from datetime import datetime
 
-## Amazon web configuration
-### update with your settings
-### AWS access key and secret key can be located here: http://bit.ly/KiZ2VP
+import tempfile
+import os.path as op
+import re
 
-aws_access_key_id = ''		# access key for account with launch privileges 
-aws_secret_access_key = ''	# secret key, set EC2 keypair below!
-aws_size = 'm1.large' 		# or t1.micro
-aws_keypair = 'ec2-keypair'	# your ec2 keypair
-aws_AMI = 'ami-63be790a'	# ubuntu 10.04 LTS on US-East
-env.user = 'ubuntu' 		# default user
-localkeypath = ''               # path to your .pem file (or equivalent)
-env.key_filename = localkeypath
-
+execfile('configuration.py')
 
 # Creates a new instance with desired settings on aws
 def buildApp():
-	'''Creates a new instance on ec2 and returns the ip address and summary information'''
-	with settings(warn_only = True):
+    '''Creates a new instance on ec2 and returns the ip address and summary information'''
+    with settings(warn_only = True):
 
-		conn = ec2.EC2Connection(aws_access_key_id, aws_secret_access_key)
-		reservation = conn.run_instances(aws_AMI, instance_type=aws_size, key_name=aws_keypair)
-		instance = reservation.instances[0]
-		
-		print 'Starting instance %s' %(instance)
-		while not instance.update() == 'running':
-			time.sleep(1)
-			sys.stdout.write('.')
-			sys.stdout.flush()
-			
-		instance.add_tag('Name', 'incf_catmaid')					
-		print 'Instance started: %s' % instance.__dict__['id']
-		print 'Public DNS: %s\n' % instance.__dict__['public_dns_name']
-		
-		print '************** Waiting 30 seconds for boot to finish **************\n'
-		time.sleep(30)
+        conn = ec2.EC2Connection(aws_access_key_id, aws_secret_access_key)
+        reservation = conn.run_instances(aws_AMI, instance_type=aws_size, key_name=aws_keypair)
+        instance = reservation.instances[0]
+        
+        print 'Starting instance %s' %(instance)
+        while not instance.update() == 'running':
+            time.sleep(1)
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            
+        instance.add_tag('Name', 'incf_catmaid')                    
+        print 'Instance started: %s' % instance.__dict__['id']
+        print 'Public DNS: %s\n' % instance.__dict__['public_dns_name']
+        
+        print '************** Waiting 30 seconds for boot to finish **************\n'
+        time.sleep(30)
 
-		env.user = 'ubuntu'
-		env.host_string = instance.__dict__['public_dns_name']
+        env.user = 'ubuntu'
+        env.host_string = instance.__dict__['public_dns_name']
 
-		installBasePackages()
-		installImageComponents()
-		installCatmaid()
-		installFIJI()
-		
-		print 'Instance has been launched successfully'
-		print 'To access, open a browser to http://%s/catmaid/' % (instance.__dict__['public_dns_name'])
+        installBasePackages()
+        installImageComponents()
+        installCatmaid()
+        installFIJI()
+        
+        print 'Instance has been launched successfully'
+        print 'To access, open a browser to http://%s/catmaid/' % (instance.__dict__['public_dns_name'])
 
 # Basic packages for building, version control
 def installBasePackages():
-	'''Basic packages for building, version control'''
-	with settings(warn_only=True):
-		# Update image and install needed base components
-		run("sudo apt-get -y --force-yes update", pty = True)
-		run("sudo apt-get -y --force-yes upgrade", pty = True)		
-		packagelist = ['git-core', 'mercurial', 'subversion', 'unzip', 'build-essential', 'g++']
-		for each_package in packagelist: 
-			print each_package
-			run('sudo apt-get -y --force-yes install %s' % each_package, pty = True)
+    '''Basic packages for building, version control'''
+    with settings(warn_only=True):
+        # Update image and install needed base components
+        run("sudo apt-get -y --force-yes update", pty = True)
+        run("sudo apt-get -y --force-yes upgrade", pty = True)        
+        packagelist = ['git', 'apache2', 'build-essential', 'g++', 'libapache2-mod-php5', 'php5-pgsql', 'imagemagick', \
+                       'python-psycopg2', 'python-yaml', 'python-tz', 'postgresql', 'pgadmin3','phppgadmin','postgresql-contrib']
+        for each_package in packagelist: 
+            print each_package
+            run('sudo apt-get -y --force-yes install %s' % each_package, pty = True)
 
+def generateConfigFiles():
+    with settings(warn_only=True):
+
+        in_configfile = op.join('../../inc/setup.inc.php.template')
+        out_configfile = op.join( tempfile.gettempdir(), 'setup.inc.php')
+
+        o = open( out_configfile ,'w')
+        data = open( in_configfile, 'r' ).read()
+        data = re.sub('catmaid_user', catmaid_database_username, data)
+        data = re.sub('diamtac', catmaid_database_password, data)
+        data = re.sub("'catmaid'", "'%s'" % catmaid_database_name, data)
+        o.write( data )
+        o.close()
+
+        in_configfile = op.join('pg_hba.conf')
+        out_configfile = op.join( tempfile.gettempdir(), 'pg_hba.conf')
+
+        o = open( out_configfile ,'w')
+        data = open( in_configfile, 'r' ).read()
+        data = re.sub('CATMAID_USERNAME', catmaid_database_username, data)
+        data = re.sub('CATMAID_DATABASE', catmaid_database_name, data)
+        o.write( data )
+        o.close()
+
+        in_configfile = op.join('catmaid-db')
+        out_configfile = op.join( tempfile.gettempdir(), '.catmaid-db')
+
+        o = open( out_configfile ,'w')
+        data = open( in_configfile, 'r' ).read()
+        data = re.sub('CATMAID_USERNAME', catmaid_database_username, data)
+        data = re.sub('CATMAID_DATABASE', catmaid_database_name, data)
+        data = re.sub('CATMAID_PASSWORD', catmaid_database_password, data)
+        o.write( data )
+        o.close()
+
+def installDjangoBackend():
+    pass
 
 # Basic packages for catmaid
 def installCatmaid():
-	'''Basic packages for building, version control'''
-	with settings(warn_only=True):
-		# Update image and install needed base components
-		packagelist = ['libapache2-mod-php5', 'php5-pgsql', 'imagemagick', 'python-psycopg2', 'python-yaml', 'postgresql','pgadmin3','phppgadmin','postgresql-contrib']
-		for each_package in packagelist: 
-			print each_package
-			run('sudo apt-get -y --force-yes install %s' % each_package, pty = True)
-
-		run('git clone https://github.com/acardona/CATMAID.git')
-		sudo('rm -rvf /var/www/CATMAID')
-		sudo('ln -s /home/ubuntu/CATMAID /var/www/CATMAID')
-		sudo('mkdir -p /var/log/apache2/catmaid/')
-
-		put('apache.conf')
-		sudo('chown root:root apache.conf')
-		sudo('mv -vf /home/ubuntu/apache.conf /etc/phppgadmin/apache.conf')
-
-		put('catmaid')
-		sudo('chown root:root catmaid')
-		sudo('mv -vf /home/ubuntu/catmaid /etc/apache2/sites-available/catmaid')
-		sudo('a2dissite default')
-		sudo('a2ensite catmaid')
-		sudo('/etc/init.d/apache2 reload')
-
-		put('setup.inc.php')
-		sudo('chown root:root setup.inc.php')
-		sudo('mv -vf /home/ubuntu/setup.inc.php /home/ubuntu/CATMAID/inc/setup.inc.php')
-
-		put('pg_hba.conf')
-		sudo('chown root:root pg_hba.conf')
-		sudo('chmod 600 pg_hba.conf')
-		sudo('mv -vf /home/ubuntu/pg_hba.conf /etc/postgresql/8.4/main/pg_hba.conf')
-
-		with cd('CATMAID'):
-			run('sudo -u postgres psql < docs/createuser.sql')
-			
-
-def installFIJI():
-	with settings(warn_only=True):
-		sudo('apt-get install -y --force-yes libxtst-dev')
-		run('wget http://fiji.sc/downloads/Madison/fiji-linux64-20110307.tar.bz2')
-		run('tar xvjf fiji-linux64-20110307.tar.bz2')
-
-
-# Install Jpeg2000 (kakadu) utils
-def installImageComponents():
+    '''Basic packages for building, version control'''
     with settings(warn_only=True):
-            
-            packagelist = ['libjpeg62-dev', 'libtiff-dev']
-            for each_package in packagelist: 
-                    print each_package
-                    run('sudo apt-get -y --force-yes install %s' % each_package, pty = True)
-    
-            # install kakadu libraries here
-            kakadu_tools = 'http://s3.amazonaws.com/wholeslide/installs/Kakadu_v6_3_1-00781N_Linux-64-bit-Compiled.tar.gz' 
-            
-            run("mkdir kakadu")	
-            with cd('kakadu'):
-                    run('wget %s' % kakadu_tools)
-                    run('tar -xvzf Kakadu_v6_3_1-00781N_Linux-64-bit-Compiled.tar.gz')			
-                    
-                    run('sudo mv ./bin/* /usr/local/bin/')
-                    run('sudo mv ./lib/* /usr/local/lib/')
-                    run('sudo /sbin/ldconfig')
-                    
-            run('rm -rvf kakadu')
 
+        installBasePackages()
+        generateConfigFiles()
+
+        run('git clone -b cmw-integration git://github.com/acardona/CATMAID.git')
+        #sudo('rm -rvf /var/www/CATMAID')
+        #sudo('ln -s /home/ubuntu/CATMAID /var/www/CATMAID')
+        #sudo('mkdir -p /var/log/apache2/catmaid/')
+
+        put('apache.conf')
+        sudo('chown root:root apache.conf')
+        sudo('mv -vf /home/ubuntu/apache.conf /etc/phppgadmin/apache.conf')
+
+        put('000-default')
+        sudo('chown root:root 000-default')
+        sudo('mv -vf /home/ubuntu/000-default /etc/apache2/sites-enabled/000-default')
+        #sudo('a2dissite default')
+        #sudo('a2ensite catmaid')
+        sudo('/etc/init.d/apache2 reload')
+
+        put(op.join( tempfile.gettempdir(), 'setup.inc.php'))
+        sudo('chown root:root setup.inc.php')
+        sudo('mv -vf /home/ubuntu/setup.inc.php /home/ubuntu/CATMAID/inc/setup.inc.php')
+
+        put(op.join( tempfile.gettempdir(), 'pg_hba.conf'))
+        sudo('chown root:root pg_hba.conf')
+        sudo('chmod 600 pg_hba.conf')
+        sudo('mv -vf /home/ubuntu/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf')
+
+        put(op.join( tempfile.gettempdir(), '.catmaid-db'))
+
+        with cd('CATMAID'):
+            run('scripts/createuser.sh {0} {1} {2} | sudo -u postgres psql'.format( catmaid_database_name, \
+            catmaid_database_username, catmaid_database_password))
 
 # this has to be run AFTER visiting http://domain/catmaid/
-def installExampleData():
-	with settings(warn_only=True):
-		with cd('CATMAID'):
-			run('sudo -u postgres psql < docs/createuser.sql')
-			run('sudo -u postgres psql catmaid < docs/example-projects.sql')
+def installExampleProject():
+    with settings(warn_only=True):
+        with cd('CATMAID'):
+            run('scripts/database/insert-example-projects.py')
 
 
 

575fa701b6a649ee0ae64a764771f5afc521d08d
Stephan Gerhard <git@unidesign.ch>
2012-06-05 14:54:03 +0200

    Working configuration to launch AWS instances

--- scripts/cloud/fabfile.py
+++ scripts/cloud/fabfile.py
@@ -23,32 +23,27 @@ def buildApp():
     with settings(warn_only = True):
 
         conn = ec2.EC2Connection(aws_access_key_id, aws_secret_access_key)
-        reservation = conn.run_instances(aws_AMI, instance_type=aws_size, key_name=aws_keypair)
+
+        # check if it exists already
+        if not 'catmaidgroup' in [group.name for group in conn.get_all_security_groups()]:
+            print 'Create CATMAID security group'
+            web = conn.create_security_group('catmaidgroup', 'CATMAID security group')
+            web.authorize('tcp', 80, 80, '0.0.0.0/0')
+            web.authorize('tcp', 22, 2, '0.0.0.0/0')
+        reservation = conn.run_instances(aws_AMI, instance_type=aws_size, key_name=aws_keypair_name,
+            security_groups=['catmaidgroup'])
         instance = reservation.instances[0]
-        
-        print 'Starting instance %s' %(instance)
+
+        print 'Starting instance %s' % (instance)
         while not instance.update() == 'running':
             time.sleep(1)
             sys.stdout.write('.')
             sys.stdout.flush()
             
-        instance.add_tag('Name', 'incf_catmaid')                    
+        # instance.add_tag('Name', 'incf_catmaid')
         print 'Instance started: %s' % instance.__dict__['id']
         print 'Public DNS: %s\n' % instance.__dict__['public_dns_name']
-        
-        print '************** Waiting 30 seconds for boot to finish **************\n'
-        time.sleep(30)
-
-        env.user = 'ubuntu'
-        env.host_string = instance.__dict__['public_dns_name']
-
-        installBasePackages()
-        installImageComponents()
-        installCatmaid()
-        installFIJI()
-        
-        print 'Instance has been launched successfully'
-        print 'To access, open a browser to http://%s/catmaid/' % (instance.__dict__['public_dns_name'])
+        print '-> Write this public DNS to your configuration.py file (env.host_string)'
 
 # Basic packages for building, version control
 def installBasePackages():
@@ -72,7 +67,7 @@ def generateConfigFiles():
         o = open( out_configfile ,'w')
         data = open( in_configfile, 'r' ).read()
         data = re.sub('catmaid_user', catmaid_database_username, data)
-        data = re.sub('diamtac', catmaid_database_password, data)
+        data = re.sub('password', catmaid_database_password, data)
         data = re.sub("'catmaid'", "'%s'" % catmaid_database_name, data)
         o.write( data )
         o.close()
@@ -98,8 +93,79 @@ def generateConfigFiles():
         o.write( data )
         o.close()
 
+        in_configfile = op.join('../../django/projects/mysite/settings_apache.py.example')
+        out_configfile = op.join( tempfile.gettempdir(), 'settings_apache.py')
+
+        o = open( out_configfile ,'w')
+        data = open( in_configfile, 'r' ).read()
+        data = re.sub('catmaid_database_name', catmaid_database_name, data)
+        data = re.sub('catmaid_user', catmaid_database_username, data)
+        data = re.sub('catmaid_user_password', catmaid_database_password, data)
+        data = re.sub('USERNAME', env.user, data)
+        data = re.sub('CATMAID_PATH', 'CATMAID', data)
+        data = re.sub('CATMAID_WEBURL', env.host_string, data)
+        data = re.sub('PYTHON_VERSION', 'python2.7', data)
+
+        o.write( data )
+        o.close()
+
+        in_configfile = op.join('../../django/projects/mysite/django.wsgi.example')
+        out_configfile = op.join( tempfile.gettempdir(), 'django.wsgi')
+
+        o = open( out_configfile ,'w')
+        data = open( in_configfile, 'r' ).read()
+        data = re.sub('USERNAME', env.user, data)
+        data = re.sub('CATMAID_PATH', 'CATMAID', data)
+        data = re.sub('PYTHON_VERSION', 'python2.7', data)
+
+        o.write( data )
+        o.close()
+
 def installDjangoBackend():
-    pass
+    with settings(warn_only=True):
+
+        packagelist = ['python-virtualenv', 'libpq-dev python-dev', 'libxml2-dev','libxslt1-dev','libjpeg-dev',\
+                       'libtiff-dev','libgraphicsmagick++3','libgraphicsmagick++1-dev','libboost-python1.42.0','h5py','libboost-python1.42-dev']
+        for each_package in packagelist:
+            print each_package
+            run('sudo apt-get -y --force-yes install %s' % each_package, pty = True)
+
+        packagelist = ['python-numpy', 'python-h5py' ,'graphicsmagick', 'libimage-exiftool-perl']
+        for each_package in packagelist:
+            print each_package
+            run('sudo apt-get -y --force-yes build-dep %s' % each_package, pty = True)
+
+        run('sudo apt-get install libapache2-mod-wsgi')
+
+        with cd('CATMAID/django'):
+            run('virtualenv --no-site-packages env')
+            with prefix('source /home/ubuntu/CATMAID/django/env/bin/activate'):
+                run('pip install Django==1.3.1')
+                run('pip install distribute==0.6.25')
+                run('pip install django-devserver==0.3.1')
+                run('pip install numpy==1.6.1')
+                run('pip install h5py==2.0.1')
+                run('pip install psycopg2==2.4.1')
+                run('pip install sqlparse==0.1.3')
+                run('pip install wsgiref==0.1.2')
+                run('pip install networkx==1.6')
+                run('pip install pgmagick==0.5.1')
+                run('pip install celery==2.4.6')
+                run('pip install django-celery==2.4.2')
+                run('pip install kombu==2.0.0')
+                run('pip install django-kombu==0.9.4')
+
+        # settings_apache.py
+        put(op.join( tempfile.gettempdir(), 'settings_apache.py'))
+        sudo('mv -vf /home/ubuntu/settings_apache.py /home/ubuntu/CATMAID/django/projects/mysite/settings_apache.py')
+
+        # django.wsgi
+        put(op.join( tempfile.gettempdir(), 'django.wsgi'))
+        sudo('mv -vf /home/ubuntu/django.wsgi /home/ubuntu/CATMAID/django/projects/mysite/django.wsgi')
+
+        sudo('/etc/init.d/apache2 reload')
+        sudo('sudo /etc/init.d/postgresql restart')
+
 
 # Basic packages for catmaid
 def installCatmaid():
@@ -107,12 +173,10 @@ def installCatmaid():
     with settings(warn_only=True):
 
         installBasePackages()
-        generateConfigFiles()
 
         run('git clone -b cmw-integration git://github.com/acardona/CATMAID.git')
-        #sudo('rm -rvf /var/www/CATMAID')
-        #sudo('ln -s /home/ubuntu/CATMAID /var/www/CATMAID')
-        #sudo('mkdir -p /var/log/apache2/catmaid/')
+
+        generateConfigFiles()
 
         put('apache.conf')
         sudo('chown root:root apache.conf')
@@ -130,8 +194,8 @@ def installCatmaid():
         sudo('mv -vf /home/ubuntu/setup.inc.php /home/ubuntu/CATMAID/inc/setup.inc.php')
 
         put(op.join( tempfile.gettempdir(), 'pg_hba.conf'))
-        sudo('chown root:root pg_hba.conf')
-        sudo('chmod 600 pg_hba.conf')
+        sudo('chown postgres:postgres pg_hba.conf')
+        sudo('chmod 644 pg_hba.conf')
         sudo('mv -vf /home/ubuntu/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf')
 
         put(op.join( tempfile.gettempdir(), '.catmaid-db'))
@@ -140,16 +204,29 @@ def installCatmaid():
             run('scripts/createuser.sh {0} {1} {2} | sudo -u postgres psql'.format( catmaid_database_name, \
             catmaid_database_username, catmaid_database_password))
 
+        # TODO: remove files in tempdir
+
 # this has to be run AFTER visiting http://domain/catmaid/
 def installExampleProject():
     with settings(warn_only=True):
         with cd('CATMAID'):
             run('scripts/database/insert-example-projects.py')
 
+# See http://boto.readthedocs.org/en/latest/ec2_tut.html
+def stopInstance():
+    pass
+
+def terminateInstance():
+    pass
+
+def enablephppgadmin():
+    pass
+
+def disablephppgadmin():
+    pass
 
 def updateCATMAID():
-    """ Update the source code repository with the latest commit
-    """
+    """ Update the source code repository with the latest commit """
     with settings(warn_only=True):
         with cd('CATMAID'):
             run('git clone -b cmw-integration git://github.com/acardona/CATMAID.git')
---
+++ sphinx-doc/source/catmaid_aws.rst
@@ -0,0 +1,51 @@
+Running a CATMAID instance on Amazon AWS/EC2
+============================================
+
+First, you have to register for `Amazon Web Services <http://aws.amazon.com/>`_.
+We will use the free tier EC2, but you will need to provide an address, credit card
+and a phone number. Register for EC2.
+
+Prerequisites
+-------------
+
+You will need two Python packages to interface on the command-line with AWS::
+
+    sudo pip install fabric
+    sudo pip install boto
+
+
+Installation
+------------
+
+Copy `configuration.py.template` to `configuration.py` and fill in the details
+from your AWS registration.
+
+First, we have to initialize an instance. For an overview of your current instances,
+visit your `AWS Management Console` on the web. We create a new instance with fabric and boto::
+
+    fab buildApp
+
+You should see the new instance in the `AWS Management Console`. After a few minutes, status
+checks should be green. Fill in the public DNS name in your `configuration.py` file. Then, we
+can setup CATMAID on this instance with::
+
+    fab installCatmaid
+
+Once this is completed, try to visit your instance in the web browser with the URL: http://$ec2hostname/catmaid/
+You should see a message `No projects available`. You can create example projects with::
+
+    fab installExampleProject
+
+Log in using any with the demo username & password 'gerhard'.
+
+You can access your database with the URL http://$ec2hostname/phppgadmin/. Note that exposing phpPgAdmin
+to the public is highly insecure.
+
+Now, setup the Django-backend::
+
+    fab installDjangoBackend
+
+.. add a function to make enable/disable phppgadmin visibility
+
+In the next steps, you have customize and configure users, projects and stacks in the database,
+and host your image data stacks.

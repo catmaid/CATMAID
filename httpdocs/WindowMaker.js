7c5f1ccdc93349023b7cc748d91625e426c327b3
Stephan Gerhard <git@unidesign.ch>
2012-06-06 11:30:47 +0200

    Link to microcircuit package to work with NeuroHDF

--- httpdocs/WindowMaker.js
+++ httpdocs/WindowMaker.js
@@ -583,7 +583,8 @@ var WindowMaker = new function()
       content.appendChild( container );
 
       container.innerHTML =
-        '<h2>Download complete microcircuit reconstruction as <a target="_new" href="'+ django_url + project.id + '/microcircuit/neurohdf' + '">NeuroHDF</a></h2>' +
+        '<h2>Download complete microcircuit reconstruction as <a target="_new" href="'+ django_url + project.id + '/microcircuit/neurohdf' + '">NeuroHDF</a>.' +
+        'You can use the Python <a target="_new" href="https://github.com/unidesigner/microcircuit/">`microcircuit` package</a> to load the file and do analysis of the neural circuit.</h2>' +
         '<br />' +
         '<h2>Download annotation graph as <a target="_new" href="'+ django_url + project.id + '/annotationdiagram/nx_json ' + '">NetworkX JSON graph</a></h2>';
     

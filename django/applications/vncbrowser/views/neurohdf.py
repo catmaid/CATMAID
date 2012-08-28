dcc8a2bfc429d220516d6ab9b5b668118066a41d
ottj <ottj@student.ethz.ch>
2012-08-28 17:01:38 -0400

    refactoring

--- django/applications/vncbrowser/views/neurohdf.py
+++ django/applications/vncbrowser/views/neurohdf.py
@@ -24,6 +24,9 @@ import time
 import sys
 import cairo
 import rsvg
+import vtk
+import random
+#from bar.rec.pipeline import barPipeline, VTK_PIPELINE
 
 # This file defines constants used to correctly define the metadata for NeuroHDF microcircuit data
 
@@ -87,6 +90,57 @@ import time
 
 def get_drawing_enum(request, project_id=None, stack_id=None):
     return HttpResponse(json.dumps(DrawingTypes), mimetype="text/json")
+
+def generate_mesh(request, project_id=None, stack_id=None):
+    skeleton_id = int(request.GET['skeleton_id'])
+
+    # retrieve all components for a given skeleton id
+    components = Component.objects.filter(
+            project = project_id,
+            stack = stack_id,
+            skeleton_id = skeleton_id
+        ).all()
+
+    # retrieve stack information
+    stack_info = get_stack_info( project_id, stack_id )
+
+    # compute the skeleton bounding box
+    minX, minY = int(stack_info['dimension']['x']), int(stack_info['dimension']['y'])
+    maxX, maxY = 0,0
+    minZ, maxZ = int(stack_info['dimension']['z']), 0
+    for comp in components:
+        minX = min(minX, comp.min_x)
+        minY = min(minY, comp.min_y)
+        minZ = min(minZ, comp.z)
+        maxX = max(maxX, comp.max_x)
+        maxY = max(maxY, comp.max_y)
+        maxZ = max(maxZ, comp.z)
+
+    print 'found bounding box', minX, minY, maxX, maxY, minZ, maxZ
+
+    # create 3d array
+    data = np.zeros( (maxY-minY, maxX-minX, maxZ-minZ), dtype = np.uint8 )
+
+    # for all components, retrieve image and bounding box location
+    for comp in components:
+        print 'work on component', comp.id,  comp.component_id
+        img = extract_as_numpy_array( project_id, stack_id, comp.component_id, comp.z ).T
+        # store image in array
+
+        height = comp.max_y - comp.min_y + 1
+        width = comp.max_x - comp.min_x + 1
+        print 'height, width', height, width
+        print 'image shape (should match)', img.shape
+        try:
+            indX = comp.min_x - minX
+            indY = comp.min_y - minY
+            data[indY:indY+height,indX:indX+width,comp.z] = img
+        except:
+            pass
+
+
+    return None
+
 
 
 def retrieve_components_for_location(project_id, stack_id, x, y, z, limit=10):

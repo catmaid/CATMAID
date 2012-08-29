caf5d9cc265e858b42a6938e7cf6a6a024625bae
ottj <ottj@student.ethz.ch>
2012-08-29 11:36:35 -0400

    added mesh generator skeleton

--- django/applications/vncbrowser/views/neurohdf.py
+++ django/applications/vncbrowser/views/neurohdf.py
@@ -105,23 +105,27 @@ def generate_mesh(request, project_id=None, stack_id=None):
 
     # retrieve stack information
     stack_info = get_stack_info( project_id, stack_id )
+    resolution=stack_info['resolution']
+    dimension=stack_info['dimension']
+    translation=stack_info['translation']
+
 
     # compute the skeleton bounding box
-    minX, minY = int(stack_info['dimension']['x']), int(stack_info['dimension']['y'])
-    maxX, maxY = 0,0
-    minZ, maxZ = int(stack_info['dimension']['z']), 0
-    for comp in components:
-        minX = min(minX, comp.min_x)
-        minY = min(minY, comp.min_y)
-        minZ = min(minZ, comp.z)
-        maxX = max(maxX, comp.max_x)
-        maxY = max(maxY, comp.max_y)
-        maxZ = max(maxZ, comp.z)
-
-    print 'found bounding box', minX, minY, maxX, maxY, minZ, maxZ
+#    minX, minY = int(dimension['x']), int(dimension['y'])
+#    maxX, maxY = 0,0
+#    minZ, maxZ = int(dimension['z']), 0
+#    for comp in components:
+#        minX = min(minX, comp.min_x)
+#        minY = min(minY, comp.min_y)
+#        minZ = min(minZ, comp.z)
+#        maxX = max(maxX, comp.max_x)
+#        maxY = max(maxY, comp.max_y)
+#        maxZ = max(maxZ, comp.z)
+#
+#    print 'found bounding box', minX, minY, maxX, maxY, minZ, maxZ
 
     # create 3d array
-    data = np.zeros( (maxY-minY, maxX-minX, maxZ-minZ), dtype = np.uint8 )
+    data = np.zeros( (dimension['x'], dimension['y'], dimension['z']), dtype = np.uint8 )
 
     # for all components, retrieve image and bounding box location
     for comp in components:
@@ -134,14 +138,14 @@ def generate_mesh(request, project_id=None, stack_id=None):
         print 'height, width', height, width
         print 'image shape (should match)', img.shape
         try:
-            indX = comp.min_x - minX
-            indY = comp.min_y - minY
-            data[indY:indY+height,indX:indX+width,comp.z] = img
+            #indX = comp.min_x - minX
+            #indY = comp.min_y - minY
+            data[comp.min_y:comp.max_y,comp.min_x:comp.max_x,comp.z] = img
         except:
             pass
 
             # Load npy volume from given file, set origin and spacing of the volue
-    npVolWrapper = VTKStructuredPoints.loadVolumeDS(data, spacing = (1,1,20))
+    npVolWrapper = VTKStructuredPoints.loadVolumeDS(data, spacing = (resolution['x'],resolution['y'],resolution['z']))
 
     # Convert npy volume to vtkImageData so vtk can handle it
     vtkNumpyDataImport = dataImporterFromNumpy(npVolWrapper)

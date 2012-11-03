ffd9eaab0a009ed083789c7f127fa9f251ea5b31
Albert Cardona <sapristi@gmail.com>
2012-11-02 23:12:44 -0400

    Created report_error decorator function in transaction.py to be used to decorate functions and then, any raised Exception will be returned as a JSON object with an error field in it. Useful in particular for functions that don't do any commits and aren't therefore decorated wtih transaction_reportable_commit_on_success.

--- django/applications/catmaid/transaction.py
+++ django/applications/catmaid/transaction.py
@@ -9,6 +9,14 @@ class CatmaidException(Exception):
     def __str__(self):
         return self.error
 
+def report_error(f):
+    def decorated_function(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            return HttpResponse(json.dumps({'error': str(e)}))
+    return decorated_function
+
 def transaction_reportable_commit_on_success(f):
 
     @transaction.commit_manually

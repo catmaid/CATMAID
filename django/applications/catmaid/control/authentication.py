0cd2125935677f39fef61a317a1e6cca0f5cd3c4
Tom Kazimiers <tom@voodoo-arts.net>
2012-11-08 13:12:48 +0100

    Make building of permissions data structure faster
    Creating the permissions dictionary was very slow when there
    were many projects available. This commit will speedup this
    process considerably. When tested with about 900 projects, the time
    needed went down from about 30 seconds to 0.05 secconds. This
    structure is created when CATMAID is loaded. Therefore, the site
    loading time decreases by this amount as well.

--- django/applications/catmaid/control/authentication.py
+++ django/applications/catmaid/control/authentication.py
@@ -4,9 +4,11 @@ import urllib
 import json
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.db import connection
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import _get_queryset
 
 from catmaid.models import Project, UserRole
 from django.contrib.auth.models import User
@@ -16,9 +18,10 @@ from catmaid.control.common import my_render_to_response
 
 from django.contrib.auth import authenticate, logout, login
 
+from guardian.models import UserObjectPermission, GroupObjectPermission
 from guardian.shortcuts import get_perms_for_model, get_objects_for_user, get_perms, get_objects_for_group
 from functools import wraps
-
+from itertools import groupby
 
 def login_vnc(request):
     return my_render_to_response(request,
@@ -106,18 +109,80 @@ def requires_user_role(roles):
         return wraps(f)(inner_decorator)
     return decorated_with_requires_user_role
 
+def get_objects_and_perms_for_user(user, codenames, klass, use_groups=True, any_perm=False):
+    """ Similar to what guardian's get_objects_for_user method does,
+    this method return a dictionary of object IDs (!) of model klass
+    objects with the permissions the user has on them associated.
+    These permissions may result from explicit user permissions or
+    implicit group ones. Parts of the source code are takes from
+    django-guardian. Note, that there an object list is returned. In
+    contrast this method returns object IDs and the permissions on
+    the actual objects.
+    """
+    # Get QuerySet on and ContentType of the model
+    queryset = _get_queryset(klass)
+    ctype = ContentType.objects.get_for_model(queryset.model)
+
+    # A super user has all permissions available on all the objects
+    # of a model.
+    if user.is_superuser:
+        # Get all permissions for the model
+        perms = get_perms_for_model(klaas)
+        permNames = set(perm.codename for perm in perms)
+        pk_dict = {}
+        for p in queryset:
+            pk_dict[p.id] = permNames
+        return pk_dict
+
+    # Extract a list of tuples that contain an object's primary
+    # key and a permission codename that the user has on them.
+    user_obj_perms = UserObjectPermission.objects\
+        .filter(user=user)\
+        .filter(permission__content_type=ctype)\
+        .filter(permission__codename__in=codenames)\
+        .values_list('object_pk', 'permission__codename')
+    data = list(user_obj_perms)
+    if use_groups:
+        groups_obj_perms = GroupObjectPermission.objects\
+            .filter(group__user=user)\
+            .filter(permission__content_type=ctype)\
+            .filter(permission__codename__in=codenames)\
+            .values_list('object_pk', 'permission__codename')
+        data += list(groups_obj_perms)
+    # sorting/grouping by pk (first in result tuple)
+    keyfunc = lambda t: int(t[0])
+    data = sorted(data, key=keyfunc)
+
+    # Create the result dictionary, associating one object id
+    # with a set of permissions the user has for it.
+    pk_dict = {}
+    for pk, group in groupby(data, keyfunc):
+        obj_codenames = set((e[1] for e in group))
+        if any_perm or codenames.issubset(obj_codenames):
+            pk_dict[pk] = obj_codenames
+
+    return pk_dict
 
 def user_project_permissions(request):
+    """ If a user is authenticated, this method returns a dictionary
+    that stores whether the user has a specific permission on a project.
+    If a user is not authenticated, this dictionary will be empty.
+    """
     result = {}
     if request.user.is_authenticated():
         projectPerms = get_perms_for_model(Project)
         permNames = [perm.codename for perm in projectPerms]
-        projects = get_objects_for_user(request.user, permNames, Project, any_perm = True)
-        for project in projects:
-            userPerms = get_perms(request.user, project)
-            for permName in [p.codename for p in projectPerms]:
+        # Find out what permissions a user actually has for any of those projects.
+        projects = get_objects_and_perms_for_user(request.user, permNames,
+                                                  Project, any_perm = True)
+        # Build the result data structure
+        for project_id in projects:
+            userPerms = projects[project_id]
+            # Iterate the codenames of available permissions and store
+            # whether the user has them for a specific project
+            for permName in permNames:
                 if permName not in result:
                     result[permName] = {}
-                result[permName][project.id] = permName in userPerms
-    
+                result[permName][project_id] = permName in userPerms
+
     return HttpResponse(json.dumps(result))

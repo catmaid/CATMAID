89be00d392d015c2144b23bbf7499583b644166e
Albert Cardona <sapristi@gmail.com>
2012-11-12 16:40:44 -0500

    delete_link now checks if the user owns it or is superuser, and create_link now checks if such a link doesn't exist already.
    Also fixed:
    
    * potential error, the delete() was being called for all
    rather than for the first relation found, which was the intended behavior.
    * the ObjectDoesNotExist was not imported from django.core.exceptions.
    * IDs where not being cast to int.

--- django/applications/catmaid/control/link.py
+++ django/applications/catmaid/control/link.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 from catmaid.models import *
 from catmaid.control.authentication import *
@@ -10,8 +11,11 @@ from catmaid.transaction import *
 @requires_user_role(UserRole.Annotate)
 @transaction_reportable_commit_on_success
 def create_link(request, project_id=None):
-    from_id = request.POST.get('from_id', 0)
-    to_id = request.POST.get('to_id', 0)
+    """ Create a link, currently only a presynaptic_to or postsynaptic_to relationship
+    between a treenode and a connector.
+    """
+    from_id = int(request.POST.get('from_id', 0))
+    to_id = int(request.POST.get('to_id', 0))
     link_type = request.POST.get('link_type', 'none')
 
     try:
@@ -19,19 +23,24 @@ def create_link(request, project_id=None):
         relation = Relation.objects.get(project=project, relation_name=link_type)
         from_treenode = Treenode.objects.get(id=from_id)
         to_connector = Connector.objects.get(id=to_id, project=project)
+        links = TreenodeConnector.objects.filter(
+            connector=to_id,
+            treenode=from_id,
+            relation=relation.id)
     except ObjectDoesNotExist as e:
         return HttpResponse(json.dumps({'error': e.message}))
 
+    if links.count() > 0:
+        return HttpResponse(json.dumps({'error': "A relation '%s' between these two elements already exists!" % link_type}))
+
     related_skeleton_count = ClassInstance.objects.filter(project=project, id=from_treenode.skeleton.id).count()
-    if (related_skeleton_count > 1):
-        # I don't see the utility of this check, think it can only happen if
-        # treenodes with non-unique IDs exist in DB. Duplicating it from PHP
-        # though.
+    if related_skeleton_count > 1:
+        # Can never happen. What motivated this check for an error of this kind? Would imply that a treenode belongs to more than one skeleton, which was possible when skeletons owned treendoes via element_of relations rather than by the skeleton_id column.
         return HttpResponse(json.dumps({'error': 'Multiple rows for treenode with ID #%s found' % from_id}))
-    elif (related_skeleton_count == 0):
+    elif related_skeleton_count == 0:
         return HttpResponse(json.dumps({'error': 'Failed to retrieve skeleton id of treenode #%s' % from_id}))
 
-    if (link_type == 'presynaptic_to'):
+    if link_type == 'presynaptic_to':
         # Enforce only one presynaptic link
         presyn_links = TreenodeConnector.objects.filter(project=project, connector=to_connector, relation=relation)
         if (presyn_links.count() != 0):
@@ -52,16 +61,21 @@ def create_link(request, project_id=None):
 @requires_user_role(UserRole.Annotate)
 @transaction_reportable_commit_on_success
 def delete_link(request, project_id=None):
-    connector_id = request.POST.get('connector_id', 0)
-    treenode_id = request.POST.get('treenode_id', 0)
+    connector_id = int(request.POST.get('connector_id', 0))
+    treenode_id = int(request.POST.get('treenode_id', 0))
 
     links = TreenodeConnector.objects.filter(
         connector=connector_id,
         treenode=treenode_id)
 
-    if (links.count() == 0):
+    if links.count() == 0:
         return HttpResponse(json.dumps({'error': 'Failed to delete connector #%s from geometry domain.' % connector_id}))
 
-    links.delete()
+    # Could be done by filtering above when obtaining the links,
+    # but then one cannot distinguish between the link not existing
+    # and the user_id not matching or not being superuser.
+    can_edit_or_fail(request.user, links[0].id, 'treenode_connector')
+
+    links[0].delete()
     return HttpResponse(json.dumps({'result': 'Removed treenode to connector link'}))
 

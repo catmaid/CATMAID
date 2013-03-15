fd09143675fb712d0a5bc197bd4f8ba6b6a641f6
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2013-03-15 10:35:21 -0400

    bugfix: wrong variable name

--- httpdocs/stack.js
+++ httpdocs/stack.js
@@ -259,7 +259,7 @@ function Stack(
 		} else {
 			// Otherwise do the next layer's beforeMove():
 			layerWithBeforeMove = layersWithBeforeMove.shift();
-			l.beforeMove(function () {
+			layerWithBeforeMove.beforeMove(function () {
 				self.moveToAfterBeforeMoves( zp, yp, xp, sp, completionCallback, layersWithBeforeMove );
 			});
 		}

4a0e29ce83b2c4a1fb6cb11e77615d52651db1f0
Tom Kazimiers <tom@voodoo-arts.net>
2013-03-16 20:55:28 -0400

    Format static files URLs config for dev server like other URLs

--- django/projects/mysite/urls.py
+++ django/projects/mysite/urls.py
@@ -215,6 +215,5 @@ urlpatterns += patterns('',
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-                            (r'^static/(?P<path>.*)$',
-                             'django.views.static.serve',
-                             {'document_root': settings.STATIC_ROOT}))
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )

973d27b522ab6b3d8387c73ce05a952a08d99f1c
Tom Kazimiers <tom@voodoo-arts.net>
2013-03-17 18:45:58 -0400

    Use a custom template view for CATMAID's homepage

--- django/applications/catmaid/views.py
+++ django/applications/catmaid/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    """ This view returns the index page of CATMAID.
+    """
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(self.__class__, self).get_context_data(**kwargs)
+
+        return context

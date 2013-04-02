10ae6561cdd73640fbcad30c29751c907edd10c4
Larry Lindsey <llindsey@clm.utexas.edu>
2013-04-02 18:30:20 -0500

    Volume Tracing: Opacity slider

--- django/applications/catmaid/static/WindowMaker.js
+++ django/applications/catmaid/static/WindowMaker.js
@@ -1117,7 +1117,7 @@ var WindowMaker = new function()
         '<div id="area_segment_view_properties">'+
         '<div id="area_segment_view_caption">Display Properties</div>'+        
         '<div id="area_segment_colorwheel"></div>'+
-        '<div id="toolbar_volseg_opacity" class="toolbar" style="display:none;">'+
+        '<div id="toolbar_volseg_opacity" class="toolbar">'+
         '<div id="volseg_opacity_box"></div>'+
         '<div class="toolbar_fill"></div>'+
         '</div>'+

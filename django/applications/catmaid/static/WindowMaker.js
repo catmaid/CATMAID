8f2411fd39d98eaf766cb40608f9353eb39adf53
Stephan Gerhard <git@unidesign.ch>
2013-04-09 18:27:39 -0400

    Do not show zero-point users in statistics

--- django/applications/catmaid/static/WindowMaker.js
+++ django/applications/catmaid/static/WindowMaker.js
@@ -1331,7 +1331,7 @@ var WindowMaker = new function()
 			'<!-- piechart -->' +
       '<div class="project-stats">' +
         '<h3>Contribution Record</h3>' +
-        'columns: new nodes (nn), new connectors (nc), reviewed nodes (nrn), new tags (nt), points (pt); points are computed as pt=nn+10*nc+nrn' +
+        'columns: new nodes (nn), new connectors (nc), reviewed nodes (nrn), points (pt); points are computed as pt=nn+nc+nrn' +
         '<table cellpadding="0" cellspacing="0" border="1" class="project-stats" id="project_stats_history_table">' +
         '</table>' +
 				'<h3>Daily Statistics</h3>' +

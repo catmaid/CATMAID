0b2c9939774089c3623fadbc20915da2f80758f7
Stephan Gerhard <git@unidesign.ch>
2013-04-27 13:00:27 -0400

    Always update active node position

--- django/applications/catmaid/static/webglapp.js
+++ django/applications/catmaid/static/webglapp.js
@@ -1010,6 +1010,8 @@ var WebGLApp = new function () {
   self.look_at_active_node = function()
   {
     if( active_node ) {
+      // always fetch the update node coordinates first
+      self.updateActiveNodePosition();
       controls.target = new THREE.Vector3(active_node.position.x,
         active_node.position.y,
         active_node.position.z);

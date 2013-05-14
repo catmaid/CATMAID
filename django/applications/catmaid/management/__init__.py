44f026b78616fe7ff3d6965e73f33555c2d345c2
Tom Kazimiers <tom@voodoo-arts.net>
2013-05-14 22:59:04 +0200

    Only create dummy project when CATMAID models were synced
    The availibility of the CATMAID model tables are are a precondition for
    creating the dummy project. Therefore, this commit makes sure the dummy
    project is only created when the CATMAID's tables were synced.

--- django/applications/catmaid/management/__init__.py
+++ django/applications/catmaid/management/__init__.py
@@ -1,15 +1,19 @@
+import catmaid.models
+
 from django.db.models import signals
 from catmaid.models import Project
 from catmaid import models as catmaid_app
 from catmaid.conf import settings as catmaid_settings
 
-def init_classification(**kwargs):
+def init_classification(app, created_models, **kwargs):
     """ Creates a dummy project to store classification graphs in.
     """
-    try:
-        Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
-    except Project.DoesNotExist:
-        Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
-            title="Classification dummy project")
+    if app == catmaid.models:
+        try:
+            Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
+        except Project.DoesNotExist:
+            print("Creating ontology dummy project")
+            Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
+                title="Classification dummy project")
 
 signals.post_syncdb.connect(init_classification)

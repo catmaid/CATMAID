f5f6c659e4efe5e87599d2cd142486a30bd5cbd6
Tom Kazimiers <tom@voodoo-arts.net>
2013-05-28 09:47:31 +0200

    Make statistics widget work with new gRaphael version

--- django/applications/catmaid/static/widgets/stats.js
+++ django/applications/catmaid/static/widgets/stats.js
@@ -66,19 +66,20 @@ var ProjectStatistics = new function()
   var update_piechart = function(data, chart_name) {
     $(chart_name).empty();
     var rpie = Raphael(chart_name, 500, 200);
-    var pie = rpie.g.piechart(90, 100, 80, data.values, { legend: data.users, legendpos: "east", colors:['red', 'blue', 'green', 'yellow', 'orange', 'black', 'gray']});
+    var pie = rpie.piechart(90, 100, 80, data.values, { legend: data.users, legendpos: "east", colors:['red', 'blue', 'green', 'yellow', 'orange', 'black', 'gray']});
     pie.hover(function () {
+      // scale everything up
       this.sector.stop();
       this.sector.scale(1.1, 1.1, this.cx, this.cy);
       if (this.label) {
         this.label[0].stop();
-        this.label[0].scale(1.5);
+        this.label[0].attr({ r: 7.5 });
         this.label[1].attr({"font-weight": 800});
       }
     }, function () {
-      this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
+      this.sector.animate({ transform: "s1 1 " + this.cx + " " + this.cy }, 500, "bounce");
       if (this.label) {
-        this.label[0].animate({scale: 1}, 500, "bounce");
+        this.label[0].animate({ r: 5 }, 500, "bounce");
         this.label[1].attr({"font-weight": 400});
       }
     });

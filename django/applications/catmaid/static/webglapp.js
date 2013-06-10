8428f48d5a63cdde041f8ba22212ef5cc7c2c123
Albert Cardona <sapristi@gmail.com>
2013-06-10 08:31:48 -0400

    webglapp.js: add espresso/vim tabulation rules

--- django/applications/catmaid/static/webglapp.js
+++ django/applications/catmaid/static/webglapp.js
@@ -1,3 +1,5 @@
+/* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
 var WebGLApp = (function() { return new function () {
 
   self = this;

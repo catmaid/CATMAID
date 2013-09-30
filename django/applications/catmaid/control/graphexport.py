57e8448f5063d29f5350f3c8acf88afca24151e0
Stephan Gerhard <git@unidesign.ch>
2013-09-30 12:42:06 +0200

    Confidence threshold and bandwith as optional parameter

--- django/applications/catmaid/control/graphexport.py
+++ django/applications/catmaid/control/graphexport.py
@@ -1,5 +1,6 @@
+import json
+
 from django.http import HttpResponse
-from django.db.models import Count
 from django.shortcuts import get_object_or_404
 
 from catmaid.models import *
@@ -14,38 +15,22 @@ try:
 except ImportError:
     pass
 
-import json
-
 @requires_user_role([UserRole.Annotate, UserRole.Browse])
 def export_jsongraph(request, project_id):
-    
     skeletonlist = request.POST.getlist('skeleton_list[]')
+    confidence_threshold = int(request.GET.get('confidence_threshold', 0))
+    bandwidth = int(request.GET.get('bandwidth', 0))
     skeletonlist = map(int, skeletonlist)
     p = get_object_or_404(Project, pk=project_id)
 
-    # default: confidence_threshold = 0, bandwidth = 0
-    circuit = _skeleton_graph(project_id, skeletonlist, 0, 9000)
-    package = {'nodes': [{'data': props} for digraph, props in circuit.nodes_iter(data=True)],
-               'edges': []}
-    edges = package['edges']
-    for g1, g2, props in circuit.edges_iter(data=True):
-        id1 = circuit.node[g1]['id']
-        id2 = circuit.node[g2]['id']
-        edges.append({'data': {'id': '%s_%s' % (id1, id2),
-                               'source': id1,
-                               'target': id2,
-                               'weight': props['c'],
-                               'label': str(props['c']) if props['directed'] else None,
-                               'directed': props['directed'],
-                               'arrow': props['arrow'],
-                               'color': props['color']}})
-
+    circuit = _skeleton_graph(project_id, skeletonlist, confidence_threshold, bandwidth)
     newgraph = nx.DiGraph()
     for digraph, props in circuit.nodes_iter(data=True):
         newgraph.add_node( props['id'], {
             'node_count': props['node_count'],
             'skeleton_id': props['skeleton_id'],
             'label': props['label'],
+            'node_reviewed_count': props['node_reviewed_count']
         })
     for g1, g2, props in circuit.edges_iter(data=True):
         id1 = circuit.node[g1]['id']

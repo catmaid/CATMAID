2b823a38000dd7e66cd60fd407386d733270ba34
Albert Cardona <sapristi@gmail.com>
2013-10-20 21:40:33 -0400

    New file libs/catmaid/utils.js with class InstanceRegistry in it.

---
+++ django/applications/catmaid/static/libs/catmaid/utils.js
@@ -0,0 +1,31 @@
+
+var InstanceRegistry = function() {
+  this.instances = {};
+};
+
+InstanceRegistry.prototype = {};
+
+InstanceRegistry.prototype.register = function() {
+  var pids = Object.keys(this.instances).map(Number);
+  if (0 === pids.length) {
+    this.instances[1] = this;
+    return 1;
+  }
+
+  // Find lowest unused number
+  var max = Math.max.apply(Math, pids),
+      pid = max + 1;
+  for (var i = 0; i < max; ++i) {
+    if (typeof(pids[i]) === 'undefined') {
+      pid = i;
+      break;
+    }
+  }
+  this.instances[pid] = this;
+  return pid;
+};
+
+InstanceRegistry.prototype.unregister = function() {
+  delete this.instances[this.widgetID];
+};
+

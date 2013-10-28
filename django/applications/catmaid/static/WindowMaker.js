49429cb3dcfa09dc02364d4924db04ec3106e841
Albert Cardona <sapristi@gmail.com>
2013-10-28 11:34:37 -0400

    Connectivity: make OR default, and rename to "All partners". AND becomes "Common partners".
    Makes sense to have OR as default given the "append" button.

--- django/applications/catmaid/static/WindowMaker.js
+++ django/applications/catmaid/static/WindowMaker.js
@@ -1419,8 +1419,8 @@ var WindowMaker = new function()
 
         var op = document.createElement('select');
         op.setAttribute('id', 'connectivity_operation' + widgetID);
-        op.appendChild(new Option('AND', 'logic-AND')); // added prefix, otherwise gets sent as nonsense
-        op.appendChild(new Option('OR', 'logic-OR'));
+        op.appendChild(new Option('All partners', 'logic-OR'));
+        op.appendChild(new Option('Common partners', 'logic-AND')); // added prefix, otherwise gets sent as nonsense
         contentbutton.appendChild(op);
 
         var add = document.createElement('input');

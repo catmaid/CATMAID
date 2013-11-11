5a37baf6d22dd959f1b035e3ee3877fec17d8e85
Tom Kazimiers <tom@voodoo-arts.net>
2013-11-11 17:20:26 -0500

    Neuron annotations: remove debug output

--- django/applications/catmaid/control/neuron_annotations.py
+++ django/applications/catmaid/control/neuron_annotations.py
@@ -14,34 +14,27 @@ def query_neurons_by_annotations(request, project_id = None):
     
     neurons = ClassInstance.objects.filter(project = p, 
                                            class_column__class_name = 'neuron')
-    #print >> sys.stderr, 'Starting with ' + str(len(neurons)) + ' neurons.'
     for key in request.POST:
         if key.startswith('neuron_query_by_annotation'):
             tag = request.POST[key].strip()
             if len(tag) > 0:
                 neurons = neurons.filter(cici_via_b__relation__relation_name = 'annotated_with',
                                          cici_via_b__class_instance_a__name = tag)
-                #print >> sys.stderr, str(len(neurons)) + ' after adding '  + request.POST[key] + '.'
         elif key == 'neuron_query_by_annotator':
             userID = int(request.POST[key])
             if userID >= 0:
                 neurons = neurons.filter(cici_via_b__relation__relation_name = 'annotated_with',
                                          cici_via_b__user = userID)
-                #print >> sys.stderr, str(len(neurons)) + ' after adding user '  + str(userID) + '.'
         elif key == 'neuron_query_by_start_date':
             startDate = request.POST[key].strip()
             if len(startDate) > 0:
                 neurons = neurons.filter(cici_via_b__relation__relation_name = 'annotated_with',
                                          cici_via_b__creation_time__gte = startDate)
-                #print >> sys.stderr, str(len(neurons)) + ' after adding after '  + startDate + '.'
         elif key == 'neuron_query_by_end_date':
             endDate = request.POST[key].strip()
             if len(endDate) > 0:
                 neurons = neurons.filter(cici_via_b__relation__relation_name = 'annotated_with',
                                          cici_via_b__creation_time__lte = endDate)
-                #print >> sys.stderr, str(len(neurons)) + ' after adding before '  + endDate + '.'
-        else:
-            print >> sys.stderr, 'Unused POST arg: ' + key + '(' + request.POST[key] + ')'
             
     dump = [];
     for neuron in neurons:

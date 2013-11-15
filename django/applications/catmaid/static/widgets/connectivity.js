72e107648fcdee63a2e89861c48052af839ee1be
Albert Cardona <sapristi@gmail.com>
2013-11-15 10:37:04 -0500

    connectivity widget: use widgetID for the plots.

--- django/applications/catmaid/static/widgets/connectivity.js
+++ django/applications/catmaid/static/widgets/connectivity.js
@@ -443,7 +443,8 @@ SkeletonConnectivity.prototype.createConnectivityTable = function(status, text) 
 SkeletonConnectivity.prototype.createSynapseDistributionPlots = function(json) {
     // A grouped bar chart plot from d3.js
     
-    var skeletons = this.skeletons;
+    var skeletons = this.skeletons,
+        widgetID = this.widgetID;
 
     /** Generate a distribution of number of Y partners that have X synapses,
      * for each partner. The distribution then takes the form of an array of blocks,
@@ -524,7 +525,7 @@ SkeletonConnectivity.prototype.createSynapseDistributionPlots = function(json) {
             height = 500 - margin.top - margin.bottom;
 
         var svg = d3.select(container).append("svg")
-            .attr("id", "connectivity_plot_" + title)
+            .attr("id", "connectivity_plot_" + widgetID + "_" + title)
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")

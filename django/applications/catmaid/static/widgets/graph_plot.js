b2417e379517524cf1b6938ec3c796cfeb25d49a
Albert Cardona <sapristi@gmail.com>
2013-11-15 22:48:48 -0500

    Circuit Graph Plot: remove global variable, used for testing.

--- django/applications/catmaid/static/widgets/graph_plot.js
+++ django/applications/catmaid/static/widgets/graph_plot.js
@@ -130,7 +130,6 @@ CircuitGraphPlot.prototype.plot = function(skeleton_ids, models, AdjM) {
 
   // Compute signal flow and eigenvectors
   try {
-    window.AdjM = AdjM;
     var cga = new CircuitGraphAnalysis().init(AdjM, 100000);
   } catch (e) {
     this.clear();

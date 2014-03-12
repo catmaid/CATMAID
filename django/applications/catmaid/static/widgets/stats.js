30aeb7713b75bac99986e033ba961654ae86208f
Tom Kazimiers <tom@voodoo-arts.net>
2014-03-12 12:07:41 -0400

    Tracing tool's project statistics: fix small legend alignment issue
    The legend's labels in different columns where not aligned exactly in Y.
    This relates to issue #597.

--- django/applications/catmaid/static/widgets/stats.js
+++ django/applications/catmaid/static/widgets/stats.js
@@ -96,7 +96,7 @@ var ProjectStatistics = new function()
       // Make name columns if names don't fit under below each other
       if (l_y + line_height > y + radius) {
         shift_x = shift_x + max_label_width + text_indent + 16;
-        shift_y = shift_y - l_y;
+        shift_y = shift_y - (i + 1) * line_height;
       }
 
       var color = pie.series[i].attrs.fill;

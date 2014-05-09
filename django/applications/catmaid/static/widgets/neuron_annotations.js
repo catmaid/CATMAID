56e0c674e1c8d7935c7d36ce9a13a42967395992
Tom Kazimiers <tom@voodoo-arts.net>
2014-05-09 11:50:12 -0400

    Neuron search widget: reset "select all" check box on new search
    On a new search, the "select all" check box should not be checked if it was
    checked before. This fixes #649.

--- django/applications/catmaid/static/widgets/neuron_annotations.js
+++ django/applications/catmaid/static/widgets/neuron_annotations.js
@@ -352,6 +352,9 @@ NeuronAnnotations.prototype.query = function(initialize)
   if (initialize) {
     this.display_start = 0;
     this.total_n_results = 0;
+    // Reset "select all" check box
+    $('#neuron_annotations_toggle_neuron_selections_checkbox' + this.widgetID)
+        .prop('checked', false);
   }
 
   var form_data = $('#neuron_query_by_annotations' +

1e9f255364f46f72ae73dac963a228a2c3d787ff
Stephan Gerhard <git@unidesign.ch>
2014-06-26 11:42:08 +0200

    Added API to retrieve creation times of connector-associated links

--- django/applications/catmaid/control/skeletonexport.py
+++ django/applications/catmaid/control/skeletonexport.py
@@ -160,6 +160,7 @@ def _skeleton_for_3d_viewer(skeleton_id, project_id, with_connectors=True, lean=
 def skeleton_for_3d_viewer(request, project_id=None, skeleton_id=None):
     return HttpResponse(json.dumps(_skeleton_for_3d_viewer(skeleton_id, project_id, with_connectors=request.POST.get('with_connectors', True), lean=int(request.POST.get('lean', 0)), all_field=request.POST.get('all_fields', False)), separators=(',', ':')))
 
+@requires_user_role([UserRole.Annotate, UserRole.Browse])
 def skeleton_with_metadata(request, project_id=None, skeleton_id=None):
 
     def default(obj):
--- django/applications/catmaid/urls.py
+++ django/applications/catmaid/urls.py
@@ -127,6 +127,7 @@ urlpatterns += patterns('catmaid.control.connector',
     (r'^(?P<project_id>\d+)/connector/list/graphedge$', 'graphedge_list'),
     (r'^(?P<project_id>\d+)/connector/list/one_to_many$', 'one_to_many_synapses'),
     (r'^(?P<project_id>\d+)/connector/skeletons$', 'connector_skeletons'),
+    (r'^(?P<project_id>\d+)/connector/edgetimes$', 'connector_associated_edgetimes'),
 )
 
 # Neuron acess

f18452622e05c9456ce826a8a45b21162580c955
fzadow <felix@von-zadow.de>
2014-07-02 17:02:38 +0200

    Make comment field searchable in admin
    The 'comment' fields of Project and Stack can now be searched in admin.

--- django/applications/catmaid/admin.py
+++ django/applications/catmaid/admin.py
@@ -25,14 +25,14 @@ class ProjectStackInline(admin.TabularInline):
 
 class ProjectAdmin(GuardedModelAdmin):
     list_display = ('title', 'public')
-    search_fields = ['title']
+    search_fields = ['title','comment']
     inlines = [ProjectStackInline]
 
 
 class StackAdmin(GuardedModelAdmin):
     list_display = ('title', 'dimension', 'resolution', 'num_zoom_levels',
                     'image_base')
-    search_fields = ['title', 'image_base']
+    search_fields = ['title', 'comment', 'image_base']
     inlines = [ProjectStackInline]
 
 

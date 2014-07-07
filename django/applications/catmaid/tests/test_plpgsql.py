3f6c486d161b06366b340440961aa22908f95a0a
Tom Kazimiers <tom@voodoo-arts.net>
2014-07-07 16:20:39 -0400

    Intersection tables: add basic test cases for PL/pgSQL functions
    There is one test case to test if the new table is actually created in
    the expected format and the one tests tests the intersections. The tests
    can be run with this:
    
    ./manage.py test catmaid.tests.test_plpgsql

--- django/applications/catmaid/tests/test_plpgsql.py
+++ django/applications/catmaid/tests/test_plpgsql.py
@@ -1,7 +1,7 @@
 from django.db import connection
 from django.test import TestCase
 
-from catmaid.models import Treenode
+from catmaid.models import ProjectStack, Treenode
 
 class IntersectionTableTest(TestCase):
     """ This test case tests the PL/pgSQL functions creating the intersection
@@ -42,14 +42,48 @@ class IntersectionTableTest(TestCase):
         representation of the skeleton (i.e. a treenode on each slice that the
         skeleton intersects).
         """
+        project_id = 3
+        stack_id = 3
         table_name = 'catmaid_skeleton_intersections'
-        # Re-create intersection table
 
+        # Re-create intersection table
         cursor = connection.cursor()
         cursor.execute('''
         SELECT * FROM recreate_intersection_table('%s')
         ''' % table_name)
 
+        # Expect the new table to be created
+        cursor.execute('''
+        SELECT COUNT(*) FROM pg_class WHERE relname='%s'
+        ''' % table_name)
+        self.assertEqual(1, int(cursor.fetchall()[0][0]))
 
-        # Get all treenode positions of test skeleton))
-        Treenode.objects.filter(skeleton_id=1).
+        # Populate the intersection table for project one and stack one
+        cursor = connection.cursor()
+        cursor.execute('''
+        SELECT * FROM populate_intersection_table(%s,%s,'%s')
+        ''' % (project_id, stack_id, table_name))
+
+        # First, count the number of generated intersections and compare them
+        # to the number of treenodes. This test is correct here, because we
+        # test against an XY stack amd all treenodes lie on a section of the
+        # stack we used to create intersections against.
+        num_treenodes = Treenode.objects.filter(project_id=project_id).count()
+        cursor.execute('''
+        SELECT COUNT(*) FROM %s
+        ''' % table_name)
+        num_intersections = int(cursor.fetchall()[0][0])
+        self.assertEquals(num_treenodes, num_intersections)
+
+        # Second, compare the actual locations of all treenodes against the
+        # generated intersections. Because the generated intersections lie on
+        # sections, their child_id is equal to the actual treenode ID.
+        t_id_vs_loc = dict(Treenode.objects.filter(project_id=project_id) \
+                .values_list('id', 'location'))
+        cursor.execute('''
+        SELECT child_id, intersection FROM %s
+        ''' % table_name)
+        i_id_vs_loc = {r[0]: r[1] for r in cursor.fetchall()}
+
+        for tid, tloc in t_id_vs_loc.items():
+            self.assertEquals(tloc, i_id_vs_loc[tid])

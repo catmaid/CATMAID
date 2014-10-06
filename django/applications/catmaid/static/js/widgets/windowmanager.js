34be82d79cb1262a0671aaf30cf74eb2286f4983
Tom Kazimiers <tom@voodoo-arts.net>
2014-10-06 12:18:50 -0400

    Move resize handler back into window manager
    The window manager is too a large part self-contained. Having the resize handle
    in a different file breaks this and since the resize handle is only used by the
    window manager it fits better in its file than in its own.

--- django/applications/catmaid/static/js/widgets/windowmanager.js
+++ django/applications/catmaid/static/js/widgets/windowmanager.js
@@ -980,3 +980,74 @@ CMWWindow.signalName[CMWWindow.CLOSE] = 'CLOSE';
 CMWWindow.signalName[CMWWindow.RESIZE] = 'RESIZE';
 CMWWindow.signalName[CMWWindow.FOCUS] = 'FOCUS';
 CMWWindow.signalName[CMWWindow.BLUR] = 'BLUR';
+
+
+/**
+ * a vertical or horizontal resize handle
+ */
+function ResizeHandle(type, node) {
+  /**
+   * returns the html-element
+   */
+  this.getView = function () {
+    return view;
+  };
+
+  var onmousemove = {
+    h: function (e) {
+      node.changeWidth( ui.diffX );
+      return false;
+    },
+    v: function (e) {
+      node.changeHeight( ui.diffY );
+      return false;
+    }
+  };
+
+  var onmouseup = {
+    h: function (e) {
+      ui.releaseEvents();
+      ui.removeEvent("onmousemove", onmousemove.h);
+      ui.removeEvent("onmouseup", onmouseup.h);
+      return false;
+    },
+    v: function (e) {
+      ui.releaseEvents();
+      ui.removeEvent("onmousemove", onmousemove.v);
+      ui.removeEvent("onmouseup", onmouseup.v);
+      return false;
+    }
+  };
+
+  var onmousedown = {
+    h: function (e) {
+      ui.registerEvent("onmousemove", onmousemove.h);
+      ui.registerEvent("onmouseup", onmouseup.h);
+      ui.catchEvents("e-resize");
+      ui.onmousedown(e);
+      ui.catchFocus();
+
+      return false;
+    },
+    v: function (e) {
+      ui.registerEvent("onmousemove", onmousemove.v);
+      ui.registerEvent("onmouseup", onmouseup.v);
+      ui.catchEvents("s-resize");
+      ui.catchFocus();
+
+      return false;
+    }
+  };
+
+
+  // initialise
+  if (typeof ui === "undefined") ui = new UI();
+
+  var self = this;
+
+  if (type != "v") type = "h";
+  var view = document.createElement("div");
+  view.className = "resize_handle_" + type;
+  view.onmousedown = onmousedown[type];
+  view.onmouseup = onmouseup[type];
+};

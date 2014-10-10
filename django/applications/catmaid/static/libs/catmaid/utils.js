1b9a9a79cebaf69dd9554ddbc9be7b28cdfe8376
Albert Cardona <sapristi@gmail.com>
2014-10-10 15:47:32 -0400

    utils.js setup for vim tabulation at 2 spaces

--- django/applications/catmaid/static/libs/catmaid/utils.js
+++ django/applications/catmaid/static/libs/catmaid/utils.js
@@ -1,3 +1,6 @@
+/* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
+
 "use strict";
 
 var InstanceRegistry = function() {

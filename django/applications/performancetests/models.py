a095a9a5c7d9568cbdf624fbc54b913756f7019a
Tom Kazimiers <tom@voodoo-arts.net>
2015-01-29 13:35:05 -0500

    Performance tests: move models from CATMAID to own application
    All models and functionality can still be used stand alone with a
    script. It is not even needed to setup the database if no persistence is
    required. The example script in the PerformanceTest class documentation
    has been updated accordingly.
    
    Giving the performance test its own application allows for easier
    management of persistent test views and test results. Changes in these
    models (and therefore database tables) can now also be managed with
    migrations. Additionally, it is very easy to create views that can be
    used in CATMAID's admin interface to e.g. show test result data over
    time.

--- django/applications/performancetests/models.py
+++ django/applications/performancetests/models.py
@@ -1,3 +1,33 @@
+from datetime import datetime
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
+
+class TestView(models.Model):
+    """
+    Represents a views that should be tested. It expects 'GET' or 'POST'
+    as method, a URL and optionally a data dictionary.
+    """
+    method = models.CharField(max_length=50)
+    url = models.TextField()
+    data = JSONField(blank=True, default={})
+    creation_time = models.DateTimeField(default=datetime.now)
+
+    def __unicode__(self):
+        return "%s %s" % (self.method, self.url)
+
+
+class TestResult(models.Model):
+    """
+    Respresents the result of test of the given view. It expects a time and a
+    result.
+    """
+    view = models.ForeignKey(TestView)
+    time = models.IntegerField()
+    result_code = models.IntegerField()
+    result = models.TextField()
+    creation_time = models.DateTimeField(default=datetime.now)
+    version = models.CharField(blank=True, max_length=50)
+
+    def __unicode__(self):
+        return "%s (Time: %sms Status: %s)" % (self.view, self.time, self.result_code)

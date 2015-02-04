d3ab7112fa9bae5974b221c94ef1736caf2b788d
Albert Cardona <sapristi@gmail.com>
2015-02-03 22:03:25 -0500

    Export script that calls all other export scripts within the same transaction.

---
+++ scripts/export/export_all.py
@@ -0,0 +1,18 @@
+# Albert Cardona 2015-02-03
+# This file is meant to be run from within ./manager.py shell in the environment, like:
+# [1] load 'export_all.py'
+# [2] project_id = 12
+# [3] export(project_id)
+
+from django.db import transaction
+import export_all_graphml
+import export_all_csv
+import export_all_annotations
+
+@transaction.atomic
+def export(project_id):
+    project_id = int(project_id)
+    export_all_graphml.export(project_id, "all." + str(project_id) + ".graphml")
+    export_all_csv.export(project_id, "all")
+    export_all_annotations.export(project_id, "all", "all")
+

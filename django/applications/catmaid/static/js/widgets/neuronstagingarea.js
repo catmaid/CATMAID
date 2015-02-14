0942b0365e5d40e5bb9d2510b6ddd3879c1b7e41
Tom Kazimiers <tom@voodoo-arts.net>
2015-02-13 22:20:44 -0500

    Selection table: declare use of CATMAID global (to please JSHint)

--- django/applications/catmaid/static/js/widgets/neuronstagingarea.js
+++ django/applications/catmaid/static/js/widgets/neuronstagingarea.js
@@ -1,6 +1,7 @@
 /* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
 /* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
 /* global
+  CATMAID
   Colorizer,
   ErrorDialog,
   growlAlert,

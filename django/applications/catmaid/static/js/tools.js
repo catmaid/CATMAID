catmaid/CATMAID

commit 38b733c0f9666b5686720cdc997e2882fa484035

Refactor assignment in condition into sth. more clear

django/applications/catmaid/static/js/tools.js
@@ -147,7 +147,8 @@ window.LoginDialog.prototype.show = function() {
         var o, p, value;
         o = {};
         value = /([^&=]+)=([^&=]+)/gi;
-        while (p = value.exec(r[1])) {
+        var p = value.exec(r[1]);
+        while (p ) {
           o[p[1]] = p[2];
         }
         return o;

commit 3dd5cb1ec50f607648f4612dca5394fd78602d5e

Tools: fix bug in parseQuery
This bug has been introduced by me in commit 38b733c0 while rewriting the
looping condition. Thanks to @aschampion for the pointer.

django/applications/catmaid/static/js/tools.js
@@ -75,8 +75,7 @@ CATMAID.tools = CATMAID.tools || {};
         var o, p, value;
         o = {};
         value = /([^&=]+)=([^&=]+)/gi;
-        var p = value.exec(r[1]);
-        while (p ) {
+        while ((p = value.exec(r[1])) !== null) {
           o[p[1]] = p[2];
         }
         return o;
ab67eda0dbc69ae7d8127bcdfc8462f0f6237ada
Albert Cardona <sapristi@gmail.com>
2015-03-24 13:59:24 -0400

    CircuitGraphPlot: enable zooming out under 100%.
    This enables reading the names of neurons whose label falls off to the right.
    
    Still missing: panning when at 100% zoom or below.

--- django/applications/catmaid/static/js/widgets/graph_plot.js
+++ django/applications/catmaid/static/js/widgets/graph_plot.js
@@ -886,7 +886,7 @@ CircuitGraphPlot.prototype.draw = function(xVector, xTitle, yVector, yTitle) {
   };
 
   // Variables exist throughout the scope of the function, so zoom is reachable from zoomed
-  var zoom = d3.behavior.zoom().x(xR).y(yR).scaleExtent([1, 100]).on("zoom", zoomed);
+  var zoom = d3.behavior.zoom().x(xR).y(yR).on("zoom", zoomed);
 
   // Assign the zooming behavior to the encapsulating root group
   svg.call(zoom);

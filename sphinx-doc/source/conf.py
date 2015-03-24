652a5481b80687f6edc1fc39085910bfa9f41169
Andrew Champion <andrew.champion@gmail.com>
2015-03-24 17:28:18 -0400

    Doc: bump copyright year (minor)

--- sphinx-doc/source/conf.py
+++ sphinx-doc/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CATMAID'
-copyright = u'2014, CATMAID Developers'
+copyright = u'2015, CATMAID Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

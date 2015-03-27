494313ca01461e8369fc65125584cc1d414f249f
Tom Kazimiers <tom@voodoo-arts.net>
2015-03-27 14:22:45 -0400

    Tests: add test case to reproduce node update queuing error
    This test mocks the back-end with the help of Sinon and tests if a node update
    queued after node deleting acts as expected. Expected is that the update does
    not try to update deleted nodes. This is the problem reported in
    acardona/CATMAID#990.

---
+++ django/applications/catmaid/static/js/tests/test_svgoverlay.js
@@ -0,0 +1,177 @@
+/* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
+
+QUnit.test('SVG overlay test', function( assert ) {
+
+  // Create a mock server to fake XHR requests and replace CATMAID's request
+  // queue with a new instance that makes automatically use of the fake XHR
+  // requests.
+  var server = this.sandbox.useFakeServer();
+  // Don't use Sinon's fake timer
+  this.clock.restore();
+
+  // Test queuing of node deleting and update operation
+  (function() {
+    // Create an async test assertion
+    var done = assert.async();
+    // Store original values of things we want to mock
+    var orignialGlobals = ["requestQueue", "project", "user_permissions",
+        "django_url", "statusBar"].reduce(function(o, g)
+    {
+      // Store each requested global in target object
+      o[g] = window[g];
+      return o;
+    }, {});
+
+    // Override requestQueue that uses fake XHR requests
+    requestQueue = new RequestQueue();
+
+    // Set global project to custom mocking object
+    project = {
+      id: 1,
+      getId: function() { return 1; }
+    };
+
+    // Set global user permissions to mocking object
+    user_permissions = {
+      'can_annotate': {'1': true}
+    };
+
+    // Set global Django URL and configure CATMAID
+    /*global django_url:true */
+    django_url = '/';
+    CATMAID.configure(django_url, django_url);
+
+    // Set global status bar mocking object
+    statusBar = {
+      replaceLast: function() {}
+    };
+
+    // Mock SVG overlay
+    var nodeID = 42;
+    var fakeOverlay = {
+      nodes: {
+        '41': {
+          id: 41,
+          can_edit: true,
+          type: SkeletonAnnotations.TYPE_NODE,
+          needsync: true
+        },
+        '42': {
+          id: 42,
+          can_edit: true,
+          type: SkeletonAnnotations.TYPE_NODE,
+          needsync: true
+        }
+      },
+      selectNode: function() {},
+      submit: submitterFn(),
+      pix2physX: function() { return 0; },
+      pix2physY: function() { return 0; },
+      pix2physZ: function() { return 0; }
+    };
+
+    // Indicates which nodes are available in our fake backend
+    var availableNodes = { 41: {}, 42: {} };
+
+    // Response to the deletion request
+    var deletionResonse = {
+      "deleted_neuron": false,
+      "skeleton_id": 199,
+      "success": "Removed treenode successfully.",
+      "parent_id": 41
+    };
+
+    // Let the fake server reply to a deletion request with the prepared answer
+    // above.
+    server.respondWith("POST", "/1/treenode/delete",
+     [200, { "Content-Type": "application/json" },
+      JSON.stringify(deletionResonse)]);
+    // Let the fake server return with a successful update if all nodes to
+    // update are available in the backend. Return error otherwise.
+    server.respondWith("POST", "/1/node/update",
+      function(xhr) {
+        // Parse request (see node.py)
+        var items = xhr.requestBody.split("&");
+        var pattern = /^[tc]\[(\d+)\]\[(\d+)\]=(\d+)$/;
+
+        var nodeIDs = items.reduce(function(nodes, item) {
+          var matches = pattern.exec(item);
+          // If there are matches and the second index of the element is zero,
+          // it means a node is specified (other indices are coordinateS).
+          if (matches && matches[2] === "0") {
+            // For the test there is no need to parse the node ID as int
+            nodes.push(matches[3]);
+          }
+          return nodes;
+        }, []);
+        // Find all nodes that are not available anymore
+        var nodesNotFound = nodeIDs.filter(function(n) {
+          return !availableNodes.hasOwnProperty(n);
+        });
+        var response;
+        if (nodesNotFound.length > 0) {
+          response = JSON.stringify({
+            "error": "One or more of the " + nodeIDs.length +
+                " unique objects were not found in table treenode",
+            "traceback": "Not needed in test"
+          });
+        } else {
+          response = JSON.stringify({"updated": nodeIDs.length});
+        }
+        return xhr.respond(200, { "Content-Type": "application/json" }, response);
+      });
+
+    // Delete node
+    SkeletonAnnotations.SVGOverlay.prototype.deleteNode.call(
+        fakeOverlay, nodeID);
+    // Mark the node as deleted in fake backend, once the last request is done
+    fakeOverlay.submit.then(function() {
+      delete availableNodes[nodeID]; 
+    });
+    // Update the tracing layer immediately after queing the deleting
+    SkeletonAnnotations.SVGOverlay.prototype.updateNodeCoordinatesinDB.call(
+        fakeOverlay, function(json) {
+          assert.deepEqual(json, {"updated": 2},
+              "The node update returns with expected response.");
+        });
+    // Update the tracing layer immediately after queing the deleting
+    fakeOverlay.submit.then(function() {
+      // Reset request queue to original queue
+      reset();
+      assert.ok(true, "Queing an update after node deletion works as expected.");
+      done();
+    }, function() {
+      // Reset request queue to original queue
+      reset();
+      assert.ok(false, "Queing an update after node deletion works as expected.");
+      done();
+    });
+
+    // Expect two responses from the server, try in 100ms intervals.
+    respond(2, 100);
+
+    function respond(requestsLeft, delay) {
+      if (requestsLeft > 0) {
+        setTimeout(function() {
+          // The fake server will respond to all requests in queue
+          var nResponds = server.requests.length;
+          server.respond();
+          respond(requestsLeft - nResponds, delay);
+        }, delay);
+      }
+    }
+
+    /**
+     * Leave the environment as we found it.
+     */
+    function reset() {
+      // Restore original globals
+      for (var g in orignialGlobals) {
+        if (orignialGlobals.hasOwnProperty(g)) {
+          window[g] = orignialGlobals[g]; 
+        }
+      }
+    }
+  })();
+});

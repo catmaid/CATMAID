25d14bc0c7ce4c51d45f59a6bd999a4b768e430a
Tom Kazimiers <tom@voodoo-arts.net>
2015-03-29 11:09:47 -0400

    Catch exceptions in version display template tags
    According to the Django documentation, exceptions raised from a template
    tags or filter will be exposed as a server error. To avoid this, the
    version template tag now returns "unknown", if an exception is raised.

--- django/applications/catmaid/templatetags/common.py
+++ django/applications/catmaid/templatetags/common.py
@@ -68,8 +68,11 @@ def catmaid_version():
     Return output of "git describe" executed in the directory of this file. If
     this results in an error, "unknown" is returned.
     """
-    dir = os.path.dirname(os.path.realpath(__file__))
-    p = subprocess.Popen("/usr/bin/git describe", cwd=os.path.dirname(dir),
-            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (out, error) = p.communicate()
-    return "unknown" if error else out
+    try:
+        dir = os.path.dirname(os.path.realpath(__file__))
+        p = subprocess.Popen("/usr/bin/git describe", cwd=os.path.dirname(dir),
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (out, error) = p.communicate()
+        return "unknown" if error else out
+    except:
+        return "unknown"

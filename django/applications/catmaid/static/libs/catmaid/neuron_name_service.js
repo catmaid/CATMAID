063370e4ab2a33cefc0fe2592e9e3d83cfcca0f2
Tom Kazimiers <tom@voodoo-arts.net>
2015-03-29 01:19:09 -0400

    Neuron name service: load info of unknown neurons on update implicitly
    When a name update is requested from the neuron name service, unknown
    skeletons from the request list (if one is passed) are now taken into
    account, too. They are now implicitly added to the list of managed
    skeletons. This is more fault tolerant than what was done before.
    
    See acardona/CATMAID#992

--- django/applications/catmaid/static/libs/catmaid/neuron_name_service.js
+++ django/applications/catmaid/static/libs/catmaid/neuron_name_service.js
@@ -411,7 +411,7 @@ var NeuronNameService = (function()
           requestQueue.register(django_url + project.id + '/skeleton/annotationlist',
             'POST',
             {
-              skeleton_ids: Object.keys(managedSkeletons),
+              skeleton_ids: skids,
               metaannotations: needsMetaAnnotations ? 1 : 0,
               neuronnames: needsNeueonNames ? 1 : 0,
             },

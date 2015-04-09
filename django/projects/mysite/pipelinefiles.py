067da6b379890677ab37608ae4cee8badc0a2347
Andrew Champion <andrew.champion@gmail.com>
2015-04-09 13:27:05 -0400

    Update jQuery to 2.1.3
    An update to jQuery 1.9+ was required by libraries we would like to use.
    Since CATMAID does not support IE < 9, except displaying a warning
    message thorugh Modernizr, update to jQuery 2.x instead of 1.x.
    
    1.9 introduces many API changes [1] which are fixed in other commits.
    
    [1] https://jquery.com/upgrade-guide/1.9

--- django/projects/mysite/pipelinefiles.py
+++ django/projects/mysite/pipelinefiles.py
@@ -43,7 +43,7 @@ PIPELINE_CSS = {
 
 libraries_js = {
     'modernizr': ['*.js'],
-    'jquery': ['jquery.js', 'jquery-ui-1.10.1.custom.min.js',
+    'jquery': ['jquery-2.1.3.min.js', 'jquery-ui-1.10.1.custom.min.js',
                'jquery.dataTables.min.js', '*.js'],
     'fabric.js': ['all.modified.js'],
     'raphael': ['raphael.js', 'g.raphael.js', 'g.pie-min.js', 'g.line.altered.js',

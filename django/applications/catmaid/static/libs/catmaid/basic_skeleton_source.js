11988fdd7b301d9d0ebfed9ad439639ebab29b8b
Tom Kazimiers <tom@voodoo-arts.net>
2015-04-10 14:46:42 -0400

    Support groups in basic skeleton source
    If loadAsGroup() is called on this source, all added skeletons are added
    to a group of the given name. The order of skeletons that that are part
    of this source remains the same, even if new skeletons are added to a
    group. If new skeletons are added as a group and some of them exist
    already in this source, they are moved into the group (by default). This
    behavior can be switched off, so that existing skeletons won't be added
    to the new group.

--- django/applications/catmaid/static/libs/catmaid/basic_skeleton_source.js
+++ django/applications/catmaid/static/libs/catmaid/basic_skeleton_source.js
@@ -2,16 +2,29 @@
 /* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
 
 (function(CATMAID) {
- 
+
+  // If set, the next skeletons appended are added to this group.
+  var nextGroupName;
+
   /**
    * The basic skeleton source implements the skeleton source interface and
-   * maintains an ordered list of skeletons.
+   * maintains an ordered list of skeletons. Additionally, grouping of the
+   * managed skeletons is possible.
    */
   var BasicSkeletonSource = function(name) {
     this.name = name;
     this.registerSource();
     this.skeletonModels = {};
     this.orderedSkeletonIDs = [];
+    // Elements can be groups or single skeletons. A group is represented with
+    // its name in here. If it is present as a field in the groups object, it is
+    // a real group otherwise, it is treated as a skeleton ID.
+    this.orderedElements = [];
+    // A group maps a name to a list of skeleton IDs.
+    this.groups = {};
+    // Indicate if newly appended skeletons should be removed from existing
+    // groups.
+    this.moveExistingToNewGroup = true;
   };
 
   BasicSkeletonSource.prototype = new CATMAID.SkeletonSource();
@@ -35,22 +48,52 @@
 
   /**
    * Append a list of skeleton models. If a model is already known, the stored
-   * model is updated.
+   * model is updated. If the appended skeletons should be added as a group, all
+   * added skeletons that are already grouped will be removed from those groups.
    *
    * @param models {object} An object mapping skeleton IDs to skeleton models.
    */
   BasicSkeletonSource.prototype.append = function(models) {
+    // Indicate if a new group should be created
+    var createGroup = !!nextGroupName;
+
     var skeleton_ids = Object.keys(models);
-    for (var skid in models) {
-      // Update existing model, if this skeleton is known
+    for (var strSkid in models) {
+      var skid = parseInt(strSkid, 10);
+
+      // If a skeleton is known already, either continue or move it to the end
+      // of the ordered list, if a group should be created and known skeletons
+      // should be moved to the new group.
       if (skid in this.skeletonModels) {
-        this.skeletonModels[skid] = models[skid];
-        continue; 
-      } else {
-        this.skeletonModels[skid] = models[skid];
-        // Store skeleton ID reference (as number)
-        this.orderedSkeletonIDs.push(parseInt(skid, 10));
+        if (createGroup) {
+          if (this.moveExistingToNewGroup) {
+            var skeletonIndex = this.orderedSkeletonIDs.indexOf(skid);
+            this.orderedSkeletonIDs.splice(skeletonIndex, 1);
+            var groupIndex = this.orderedElements.indexOf(skid);
+            this.orderedElements.splice(groupIndex, 1);
+          } else {
+            skeleton_ids.splice(skeleton_ids.indexOf(strSkid), 1);
+          }
+        } else {
+          // Update existing model and continue
+          this.skeletonModels[skid] = models[skid];
+          continue;
+        }
       }
+
+      // Store skeleton ID reference (as number) and model
+      this.orderedSkeletonIDs.push(skid);
+      this.skeletonModels[skid] = models[skid];
+
+      // If no group should be created, add the ID of every added skeleton to
+      // the group index, but don't create an entry in the groups object.
+      if (!createGroup) this.orderedElements.push(skid);
+    }
+
+    if (createGroup) {
+      // Add group if a new group was requested
+      this.orderedElements.push(nextGroupName);
+      this.groups[nextGroupName] = skeleton_ids;
     }
 
     this.updateLink(models);
@@ -62,6 +105,8 @@
   BasicSkeletonSource.prototype.clear = function(source_chain) {
     this.orderedSkeletonIDs = [];
     this.skeletonModels = {};
+    this.orderedElements = [];
+    this.groups = {};
     // Clear link target, if any
     this.clearLink(source_chain);
   };
@@ -77,10 +122,23 @@
       return -1 === skeletonIDs.indexOf(skid);
     });
 
-    // Remove models
     skeletonIDs.forEach(function(skid) {
+      var nSkid = parseInt(skid, 10);
+      // Remove models
       if (skid in this.skeletonModels) {
         delete this.skeletonModels[skid];
+      }
+      // Remove from element index
+      var groupIndex = this.orderedElements.indexOf(nSkid);
+      if (-1 !== groupIndex) {
+        this.orderedElements.splice(groupIndex, 1);
+      }
+      // Remove from groups
+      for (var key in this.groups) {
+        var index = this.groups[key].indexOf(nSkid);
+        if (-1 !== index) {
+          this.groups[key].splice(index, 1);
+        }
       }
     }, this);
 
@@ -157,6 +215,31 @@
    * specific.
    */
   BasicSkeletonSource.prototype.highlight = function() {};
+
+
+  /* Non-interface methods */
+
+  BasicSkeletonSource.prototype.loadAsGroup = function(groupName) {
+    if (!groupName) {
+      throw CATMAID.ValueError("Please give a valid group name");
+    }
+    if (this.groups[groupName]) {
+      throw CATMAID.ValueError("The group '" + groupName + "' exists already");
+    }
+
+    // Add the skeletons loaded next to the new group
+    nextGroupName = groupName;
+    this.loadSource();
+    nextGroupName = undefined;
+  };
+
+  /**
+   * Returns true if the given name is the one of a group managed by this
+   * source. False otherwise.
+   */
+  BasicSkeletonSource.prototype.isGroup = function(groupName) {
+    return !!this.orderedElements[groupName];
+  };
 
   // Make basic skeleton source available in CATMAID namespace
   CATMAID.BasicSkeletonSource = BasicSkeletonSource;

eddad1cdb95051c78c8554e7877ff3f8113408d0
Tom Kazimiers <tom@voodoo-arts.net>
2015-04-15 10:28:39 -0400

    Fix missing include of jquery-ui.combobox.js (used for auto-completion)
    This was removed by accident during the recent jQuery update.

--- django/projects/mysite/pipelinefiles.py
+++ django/projects/mysite/pipelinefiles.py
@@ -44,7 +44,7 @@ PIPELINE_CSS = {
 libraries_js = {
     'modernizr': ['*.js'],
     'jquery': ['jquery-2.1.3.min.js', 'jquery-migrate-1.2.1.js',
-               'jquery-ui.min.js',
+               'jquery-ui.min.js', 'jquery-ui.*.js',
                'jquery.dataTables.min.js', 'jquery.*.js'],
     'fabric.js': ['all.modified.js'],
     'raphael': ['raphael.js', 'g.raphael.js', 'g.pie-min.js', 'g.line.altered.js',

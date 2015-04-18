4bf67e6108407cc700c6bf11c8eeaedaca94ca6f
Tom Kazimiers <tom@voodoo-arts.net>
2015-04-17 23:56:44 -0400

    Connectivity matrix: add globals and semicolon for JSHint

--- django/applications/catmaid/static/libs/catmaid/connectivity_matrix.js
+++ django/applications/catmaid/static/libs/catmaid/connectivity_matrix.js
@@ -1,5 +1,10 @@
 /* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
 /* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
+/* global
+  CATMAID,
+  project,
+  requestQueue
+*/
 
 (function(CATMAID) {
 

5adc6408afa0a91430755e68f41e4bc3f91a0779
Tom Kazimiers <tom@voodoo-arts.net>
2015-05-21 18:17:50 -0400

    Namespacing: move some helper functions into CATMAID namespace

--- django/applications/catmaid/static/js/CATMAID.js
+++ django/applications/catmaid/static/js/CATMAID.js
@@ -68,6 +68,59 @@
    */
   CATMAID.warn = CATMAID.msg.bind(window, "Warning");
 
+  /**
+   * Creates a generic JSON response handler that complains when the response
+   * status is different from 200 or a JSON error is set.
+   *
+   * @param success Called on success
+   * @param error Called on error
+   * @param silent No error dialogs are shown, if true
+   */
+  CATMAID.jsonResponseHandler = function(success, error, silent)
+  {
+    return function(status, text, xml) {
+      if (status === 200 && text) {
+        var json = $.parseJSON(text);
+        if (json.error) {
+          // Call error handler, if any, and force silence if it returned true.
+          if (CATMAID.tools.isFn(error)) {
+            silent = silent || error(json);
+          }
+          if (!silent) {
+            CATMAID.error(json.error, json.detail);
+          }
+        } else {
+          CATMAID.tools.callIfFn(success, json);
+        }
+      } else {
+        // Call error handler, if any, and force silence if it returned true.
+        if (CATMAID.tools.isFn(error)) {
+          silent = silent || error();
+        }
+        if (!silent) {
+          CATMAID.error("An error occured", "The server returned an unexpected " +
+              "status: " + status);
+        }
+      }
+    };
+  };
+
+  /**
+   * Convenience function to show an error dialog.
+   */
+  CATMAID.error = function(msg, detail)
+  {
+    new CATMAID.ErrorDialog(msg, detail).show();
+  };
+
+  /**
+   * Make status information available through the front-ends status bar.
+   */
+  CATMAID.status = function(msg)
+  {
+    CATMAID.statusBar.replaceLast(msg);
+  };
+
 })(CATMAID);
 
 
@@ -217,57 +270,4 @@ CATMAID.LoginDialog.prototype = {};
  */
 CATMAID.LoginDialog.prototype.show = function() {
   this.dialog.show('400', 'auto', true);
-};
-
-/**
- * Creates a generic JSON response handler that complains when the response
- * status is different from 200 or a JSON error is set.
- *
- * @param success Called on success
- * @param error Called on error
- * @param silent No error dialogs are shown, if true
- */
-CATMAID.jsonResponseHandler = function(success, error, silent)
-{
-  return function(status, text, xml) {
-    if (status === 200 && text) {
-      var json = $.parseJSON(text);
-      if (json.error) {
-        // Call error handler, if any, and force silence if it returned true.
-        if (CATMAID.tools.isFn(error)) {
-          silent = silent || error(json);
-        }
-        if (!silent) {
-          CATMAID.error(json.error, json.detail);
-        }
-      } else {
-        CATMAID.tools.callIfFn(success, json);
-      }
-    } else {
-      // Call error handler, if any, and force silence if it returned true.
-      if (CATMAID.tools.isFn(error)) {
-        silent = silent || error();
-      }
-      if (!silent) {
-        CATMAID.error("An error occured", "The server returned an unexpected " +
-            "status: " + status);
-      }
-    }
-  };
-};
-
-/**
- * Convenience function to show an error dialog.
- */
-CATMAID.error = function(msg, detail)
-{
-  new CATMAID.ErrorDialog(msg, detail).show();
-};
-
-/**
- * Make status information available through the front-ends status bar.
- */
-CATMAID.status = function(msg)
-{
-  CATMAID.statusBar.replaceLast(msg);
 };

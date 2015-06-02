catmaid/CATMAID

commit 9f6b0b77d5af39ded91d9e677711a47ecc074dfb

Let back-end read the changed database location columns correctly

django/applications/catmaid/control/treenodeexport.py
@@ -155,12 +155,12 @@ class TreenodeExporter:
         and writable.
         """
         # Calculate bounding box for current connector
-        x_min = treenode.location.x - self.job.x_radius
-        x_max = treenode.location.x + self.job.x_radius
-        y_min = treenode.location.y - self.job.y_radius
-        y_max = treenode.location.y + self.job.y_radius
-        z_min = treenode.location.z - self.job.z_radius
-        z_max = treenode.location.z + self.job.z_radius
+        x_min = treenode.location_x - self.job.x_radius
+        x_max = treenode.location_x + self.job.x_radius
+        y_min = treenode.location_y - self.job.y_radius
+        y_max = treenode.location_y + self.job.y_radius
+        z_min = treenode.location_z - self.job.z_radius
+        z_max = treenode.location_z + self.job.z_radius
         rotation_cw = 0
         zoom_level = 0
 
@@ -212,9 +212,9 @@ class TreenodeExporter:
         p = n.parent.id if n.parent else 'null'
         n_pre = presynaptic_map.get(n.id, 0)
         n_post = postsynaptic_map.get(n.id, 0)
-        x = n.location.x
-        y = n.location.y
-        z = n.location.z
+        x = n.location_x
+        y = n.location_y
+        z = n.location_z
         line = ', '.join([str(e) for e in (n.id, p, n_pre, n_post, x, y, z)])
         ls.append(line)
 
@@ -321,12 +321,12 @@ class ConnectorExporter(TreenodeExporter):
         connector = connector_link.connector
 
         # Calculate bounding box for current connector
-        x_min = connector.location.x - self.job.x_radius
-        x_max = connector.location.x + self.job.x_radius
-        y_min = connector.location.y - self.job.y_radius
-        y_max = connector.location.y + self.job.y_radius
-        z_min = connector.location.z - self.job.z_radius
-        z_max = connector.location.z + self.job.z_radius
+        x_min = connector.location_x - self.job.x_radius
+        x_max = connector.location_x + self.job.x_radius
+        y_min = connector.location_y - self.job.y_radius
+        y_max = connector.location_y + self.job.y_radius
+        z_min = connector.location_z - self.job.z_radius
+        z_max = connector.location_z + self.job.z_radius
         rotation_cw = 0
         zoom_level = 0
 
@@ -340,9 +340,9 @@ class ConnectorExporter(TreenodeExporter):
         for i, img in enumerate(cropped_stack):
             # Save image in output path, named after the image center's coordinates,
             # rounded to full integers.
-            x = int(connector.location.x + 0.5)
-            y = int(connector.location.y + 0.5)
-            z = int(z_min + i * crop_self.stacks[0].resolution.z  + 0.5)
+            x = int(connector.location_x + 0.5)
+            y = int(connector.location_y + 0.5)
+            z = int(z_min + i * crop_self.stacks[0].resolution_z  + 0.5)
             image_name = "%s_%s_%s.tiff" % (x, y, z)
             connector_image_path = os.path.join(connector_path, image_name)
             img.write(connector_image_path)

django/applications/catmaid/control/connector.py
@@ -31,9 +31,9 @@ def graphedge_list(request, project_id=None):
             edge[ q.connector_id ] = {'pre': [], 'post': [], 'pretreenode': [], 'posttreenode': []}
             connectordata[ q.connector_id ] = { 
                 'connector_id': q.connector_id,
-                'x': q.connector.location.x,
-                'y': q.connector.location.y,
-                'z': q.connector.location.z,
+                'x': q.connector.location_x,
+                'y': q.connector.location_y,
+                'z': q.connector.location_z,
                 'user': q.connector.user.username }
 
         if q.relation.relation_name == 'presynaptic_to':
@@ -143,17 +143,17 @@ def list_connector(request, project_id=None):
             connector.id AS connector_id,
             tn_other.user_id AS connector_user_id,
             treenode_user.username AS connector_username,
-            (connector.location).x AS connector_x,
-            (connector.location).y AS connector_y,
-            (connector.location).z AS connector_z,
+            connector.location_x AS connector_x,
+            connector.location_y AS connector_y,
+            connector.location_z AS connector_z,
             tn_other.id AS other_treenode_id,
-            (tn_other.location).x AS other_treenode_x,
-            (tn_other.location).y AS other_treenode_y,
-            (tn_other.location).z AS other_treenode_z,
+            tn_other.location_x AS other_treenode_x,
+            tn_other.location_y AS other_treenode_y,
+            tn_other.location_z AS other_treenode_z,
             tn_other.skeleton_id AS other_skeleton_id,
-            (tn_this.location).x AS this_treenode_x,
-            (tn_this.location).y AS this_treenode_y,
-            (tn_this.location).z AS this_treenode_z,
+            tn_this.location_x AS this_treenode_x,
+            tn_this.location_y AS this_treenode_y,
+            tn_this.location_z AS this_treenode_z,
             tn_this.id AS this_treenode_id,
             tc_this.relation_id AS this_to_connector_relation_id,
             tc_other.relation_id AS connector_to_other_relation_id,
@@ -202,9 +202,9 @@ def list_connector(request, project_id=None):
             connector.id AS connector_id,
             connector.user_id AS connector_user_id,
             connector_user.username AS connector_username,
-            (connector.location).x AS connector_x,
-            (connector.location).y AS connector_y,
-            (connector.location).z AS connector_z,
+            connector.location_x AS connector_x,
+            connector.location_y AS connector_y,
+            connector.location_z AS connector_z,
             tn_this.id AS this_treenode_id,
             tc_this.relation_id AS this_to_connector_relation_id,
             to_char(connector.edition_time, 'DD-MM-YYYY HH24:MI') AS last_modified

django/applications/catmaid/control/graph2.py
@@ -228,7 +228,7 @@ def dual_split_graph(project_id, skeleton_ids, confidence_threshold, bandwidth, 
 
     # Now fetch all treenodes of all skeletons to expand
     cursor.execute('''
-    SELECT skeleton_id, id, parent_id, confidence, (location).x, (location).y, (location).z
+    SELECT skeleton_id, id, parent_id, confidence, location_x, location_y, location_z
     FROM treenode
     WHERE project_id = %s
       AND skeleton_id IN (%s)

django/applications/catmaid/control/node.py
@@ -77,18 +77,18 @@ def node_list_tuples(request, project_id=None):
         SELECT
             t1.id,
             t1.parent_id,
-            (t1.location).x,
-            (t1.location).y,
-            (t1.location).z,
+            t1.location_x,
+            t1.location_y,
+            t1.location_z,
             t1.confidence,
             t1.radius,
             t1.skeleton_id,
             t1.user_id,
             t2.id,
             t2.parent_id,
-            (t2.location).x,
-            (t2.location).y,
-            (t2.location).z,
+            t2.location_x,
+            t2.location_y,
+            t2.location_z,
             t2.confidence,
             t2.radius,
             t2.skeleton_id,
@@ -98,11 +98,11 @@ def node_list_tuples(request, project_id=None):
                (   (t1.id = t2.parent_id OR t1.parent_id = t2.id)
                 OR (t1.parent_id IS NULL AND t1.id = t2.id))
         WHERE
-            (t1.location).z = %(z)s
-            AND (t1.location).x > %(left)s
-            AND (t1.location).x < %(right)s
-            AND (t1.location).y > %(top)s
-            AND (t1.location).y < %(bottom)s
+            t1.location_z = %(z)s
+            AND t1.location_x > %(left)s
+            AND t1.location_x < %(right)s
+            AND t1.location_y > %(top)s
+            AND t1.location_y < %(bottom)s
             AND t1.project_id = %(project_id)s
         LIMIT %(limit)s
         ''', params)
@@ -140,9 +140,9 @@ def node_list_tuples(request, project_id=None):
             response_on_error = 'Failed to query connector locations.'
             cursor.execute('''
             SELECT connector.id,
-                (connector.location).x,
-                (connector.location).y,
-                (connector.location).z,
+                connector.location_x,
+                connector.location_y,
+                connector.location_z,
                 connector.confidence,
                 treenode_connector.relation_id,
                 treenode_connector.treenode_id,
@@ -162,9 +162,9 @@ def node_list_tuples(request, project_id=None):
 
         cursor.execute('''
         SELECT connector.id,
-            (connector.location).x,
-            (connector.location).y,
-            (connector.location).z,
+            connector.location_x,
+            connector.location_y,
+            connector.location_z,
             connector.confidence,
             treenode_connector.relation_id,
             treenode_connector.treenode_id,
@@ -173,11 +173,11 @@ def node_list_tuples(request, project_id=None):
         FROM connector LEFT OUTER JOIN treenode_connector
                        ON connector.id = treenode_connector.connector_id
         WHERE connector.project_id = %(project_id)s
-          AND (connector.location).z = %(z)s
-          AND (connector.location).x > %(left)s
-          AND (connector.location).x < %(right)s
-          AND (connector.location).y > %(top)s
-          AND (connector.location).y < %(bottom)s
+          AND connector.location_z = %(z)s
+          AND connector.location_x > %(left)s
+          AND connector.location_x < %(right)s
+          AND connector.location_y > %(top)s
+          AND connector.location_y < %(bottom)s
         ''', params)
 
         crows.extend(cursor.fetchall())
@@ -246,9 +246,9 @@ def node_list_tuples(request, project_id=None):
             cursor.execute('''
             SELECT id,
                 parent_id,
-                (location).x,
-                (location).y,
-                (location).z,
+                location_x,
+                location_y,
+                location_z,
                 confidence,
                 radius,
                 skeleton_id,
@@ -363,9 +363,9 @@ def most_recent_treenode(request, project_id=None):
     return HttpResponse(json.dumps({
         'id': tn.id,
         #'skeleton_id': tn.skeleton.id,
-        'x': int(tn.location.x),
-        'y': int(tn.location.y),
-        'z': int(tn.location.z),
+        'x': int(tn.location_x),
+        'y': int(tn.location_y),
+        'z': int(tn.location_z),
         #'most_recent': str(tn.most_recent) + tn.most_recent.strftime('%z'),
         #'most_recent': tn.most_recent.strftime('%Y-%m-%d %H:%M:%S.%f'),
         #'type': 'treenode'
@@ -458,9 +458,9 @@ def node_nearest(request, project_id=None):
             minDistance = -1
             nearestTreenode = None
             for tn in treenodes:
-                xdiff = x - tn.location.x
-                ydiff = y - tn.location.y
-                zdiff = z - tn.location.z
+                xdiff = x - tn.location_x
+                ydiff = y - tn.location_y
+                zdiff = z - tn.location_z
                 distanceSquared = xdiff ** 2 + ydiff ** 2 + zdiff ** 2
                 if distanceSquared < minDistance or minDistance < 0:
                     nearestTreenode = tn
@@ -477,9 +477,9 @@ def node_nearest(request, project_id=None):
 
         return HttpResponse(json.dumps({
             'treenode_id': nearestTreenode.id,
-            'x': int(nearestTreenode.location.x),
-            'y': int(nearestTreenode.location.y),
-            'z': int(nearestTreenode.location.z),
+            'x': int(nearestTreenode.location_x),
+            'y': int(nearestTreenode.location_y),
+            'z': int(nearestTreenode.location_z),
             'skeleton_id': nearestTreenode.skeleton_id}))
 
     except Exception as e:
@@ -510,9 +510,9 @@ def _fetch_location(treenode_id):
     cursor.execute('''
         SELECT
           id,
-          (location).x AS x,
-          (location).y AS y,
-          (location).z AS z,
+          location_x AS x,
+          location_y AS y,
+          location_z AS z,
           skeleton_id
         FROM treenode
         WHERE id=%s''', [treenode_id])
@@ -524,9 +524,9 @@ def _fetch_location_connector(connector_id):
     cursor.execute('''
         SELECT
           id,
-          (location).x AS x,
-          (location).y AS y,
-          (location).z AS z
+          location_x AS x,
+          location_y AS y,
+          location_z AS z
         FROM connector
         WHERE id=%s''', [connector_id])
     return cursor.fetchone()

django/applications/catmaid/control/roi.py
@@ -39,7 +39,7 @@ def get_roi_info(request, project_id=None, roi_id=None):
     info = {
         'id': roi.id,
         'zoom_level': roi.zoom_level,
-        'location': [roi.location.x, roi.location.y, roi.location.z],
+        'location': [roi.location_x, roi.location_y, roi.location_z],
         'width': roi.width,
         'height': roi.height,
         'rotation_cw': roi.rotation_cw,
@@ -174,11 +174,11 @@ def create_roi_image_task(user, project_id, roi_id, file_path):
         roi = RegionOfInterest.objects.get(id=roi_id)
         # Prepare parameters
         hwidth = roi.width * 0.5
-        x_min = roi.location.x - hwidth
-        x_max = roi.location.x + hwidth
+        x_min = roi.location_x - hwidth
+        x_max = roi.location_x + hwidth
         hheight = roi.height * 0.5
-        y_min = roi.location.y - hheight
-        y_max = roi.location.y + hheight
+        y_min = roi.location_y - hheight
+        y_max = roi.location_y + hheight
         z_min = z_max = roi.location.z
         single_channel = False
         # Create a cropping job

django/applications/catmaid/control/search.py
@@ -18,12 +18,11 @@ def search(request, project_id=None):
         .values('treenode__xxx'), and will then be able to access a proper location
         object.
         '''
-        location = Double3D.from_str(node['treenode__location'])
         return {
             'id': node['treenode'],
-            'x': int(location.x),
-            'y': int(location.y),
-            'z': int(location.z),
+            'x': int(node['treenode__location_x']),
+            'y': int(node['treenode__location_y']),
+            'z': int(node['treenode__location_z']),
             'skid': node['treenode__skeleton']}
 
     search_string = request.GET.get('substring', "")

django/applications/catmaid/control/skeleton.py
@@ -374,9 +374,9 @@ def root_for_skeleton(request, project_id=None, skeleton_id=None):
         skeleton_id=skeleton_id)
     return HttpResponse(json.dumps({
         'root_id': tn.id,
-        'x': tn.location.x,
-        'y': tn.location.y,
-        'z': tn.location.z}),
+        'x': tn.location_x,
+        'y': tn.location_y,
+        'z': tn.location_z}),
         mimetype='text/json')
 
 @requires_user_role([UserRole.Annotate, UserRole.Browse])

django/applications/catmaid/control/skeletonexport.py
@@ -43,9 +43,9 @@ def get_swc_string(treenodes_qs):
     for tn in treenodes_qs:
         swc_row = [tn.id]
         swc_row.append(0)
-        swc_row.append(tn.location.x)
-        swc_row.append(tn.location.y)
-        swc_row.append(tn.location.z)
+        swc_row.append(tn.location_x)
+        swc_row.append(tn.location_y)
+        swc_row.append(tn.location_z)
         swc_row.append(max(tn.radius, 0))
         swc_row.append(-1 if tn.parent_id is None else tn.parent_id)
         all_rows.append(swc_row)
@@ -83,7 +83,7 @@ def compact_skeleton(request, project_id=None, skeleton_id=None, with_connectors
 
     cursor.execute('''
         SELECT id, parent_id, user_id,
-               (location).x, (location).y, (location).z,
+               location_x, location_y, location_z,
                radius, confidence
         FROM treenode
         WHERE skeleton_id = %s
@@ -109,7 +109,7 @@ def compact_skeleton(request, project_id=None, skeleton_id=None, with_connectors
         # Fetch all connectors with their partner treenode IDs
         cursor.execute('''
             SELECT tc.treenode_id, tc.connector_id, tc.relation_id,
-                   (c.location).x, (c.location).y, (c.location).z
+                   c.location_x, c.location_y, c.location_z
             FROM treenode_connector tc,
                  connector c
             WHERE tc.skeleton_id = %s
@@ -174,7 +174,7 @@ def compact_arbor(request, project_id=None, skeleton_id=None, with_nodes=None, w
     if 0 != with_nodes:
         cursor.execute('''
             SELECT id, parent_id, user_id,
-                (location).x, (location).y, (location).z,
+                location_x, location_y, location_z,
                 radius, confidence
             FROM treenode
             WHERE skeleton_id = %s
@@ -272,7 +272,7 @@ def _skeleton_for_3d_viewer(skeleton_id, project_id, with_connectors=True, lean=
 
     # Fetch all nodes, with their tags if any
     cursor.execute(
-        '''SELECT id, parent_id, user_id, (location).x, (location).y, (location).z, radius, confidence %s
+        '''SELECT id, parent_id, user_id, location_x, location_y, location_z, radius, confidence %s
           FROM treenode
           WHERE skeleton_id = %s
         ''' % (added_fields, skeleton_id) )
@@ -314,7 +314,8 @@ def _skeleton_for_3d_viewer(skeleton_id, project_id, with_connectors=True, lean=
 
             # Fetch all connectors with their partner treenode IDs
             cursor.execute(
-                ''' SELECT tc.treenode_id, tc.connector_id, r.relation_name, c.location %s
+                ''' SELECT tc.treenode_id, tc.connector_id, r.relation_name,
+                           c.location_x, c.location_y, c.location_z %s
                     FROM treenode_connector tc,
                          connector c,
                          relation r
@@ -327,8 +328,8 @@ def _skeleton_for_3d_viewer(skeleton_id, project_id, with_connectors=True, lean=
             # List of (treenode_id, connector_id, relation_id, x, y, z)n with relation_id replaced by 0 (presynaptic) or 1 (postsynaptic)
             # 'presynaptic_to' has an 'r' at position 1:
             for row in cursor.fetchall():
-                x, y, z = imap(float, row[3][1:-1].split(','))
-                connectors.append((row[0], row[1], 0 if 'r' == row[2][1] else 1, x, y, z, row[4]))
+                x, y, z = imap(float, (row[3], row[4], row[5]))
+                connectors.append((row[0], row[1], 0 if 'r' == row[2][1] else 1, x, y, z, row[6]))
             return name, nodes, tags, connectors, reviews
 
     return name, nodes, tags, connectors, reviews

commit 3a735d7113a4575fd666cf1b95e09cf2dd99c1a8

Connector exportor: fix exception breaking export
Fix an exception introduced by 9f6b0b77 caused by referencing the z
resolution as its own field rather than a member of a compound field.
This broke connector node export.

django/applications/catmaid/control/treenodeexport.py
@@ -341,7 +341,7 @@ class ConnectorExporter(TreenodeExporter):
             # rounded to full integers.
             x = int(connector.location_x + 0.5)
             y = int(connector.location_y + 0.5)
-            z = int(z_min + i * crop_self.stacks[0].resolution_z  + 0.5)
+            z = int(z_min + i * crop_self.stacks[0].resolution.z  + 0.5)
             image_name = "%s_%s_%s.tiff" % (x, y, z)
             connector_image_path = os.path.join(connector_path, image_name)
             img.write(connector_image_path)
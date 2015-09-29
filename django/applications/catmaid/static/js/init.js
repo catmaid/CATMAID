8364fcc18c8f29486cbf05cd406a48e1293090f3
Tom Kazimiers <tom@voodoo-arts.net>
2015-09-29 14:13:16 -0400

    Stack groups: auto-layout after loading
    After a stack group has been loaded, a layout function is called. This
    function currently only changes the window layout if there are three or
    four windows of which three are stack viewers, each with a primary stack
    of different orientation. If there is no fourth window, a selection
    table is created. These four windows then are laid out so that they all
    have the same size, that the XY window is on the left side on top of
    the XZ window and that the ZY window is on the right on top of the
    fourth window.
    
    Closes catmaid/CATMAID#1082

--- django/applications/catmaid/static/js/init.js
+++ django/applications/catmaid/static/js/init.js
@@ -620,6 +620,8 @@ function openStackGroup(pid, sgid, successFn) {
             if (0 < stacks.length) {
               var sv = firstStackViewer ? firstStackViewer : newStackViewer;
               loadNextStack(pid, stacks.shift(), stacks, sv)
+            } else {
+              CATMAID.layoutStackViewers();
             }
           })
           .catch(function(error) {
@@ -632,6 +634,55 @@ function openStackGroup(pid, sgid, successFn) {
       CATMAID.error("Couldn't load stack group: " + error.msg, error.detail);
     });
 }
+
+/**
+ * Layout currently open stack viewers. Currently, this only changes the layout
+ * if there are three ortho-views present.
+ */
+CATMAID.layoutStackViewers = function() {
+  var stackViewers = project.getStackViewers();
+  var orientations = stackViewers.reduce(function(o, s) {
+    o[s.primaryStack.orientation] = s;
+    return o;
+  }, {});
+
+  // If there are three different ortho stacks, arrange viewers in four-pane
+  // layout. On the left side XY on top of XZ, on the righ ZY on top of a
+  // selection table.
+  var Stack = CATMAID.Stack;
+  if (3 === stackViewers.length && orientations[Stack.ORIENTATION_XY] &&
+      orientations[Stack.ORIENTATION_XZ] && orientations[Stack.ORIENTATION_ZY]) {
+    // Test if a fourth window has to be created
+    var windows = rootWindow.getWindows();
+    if (3 === windows.length) {
+      // Create fourth window for nicer layout
+      WindowMaker.create('neuron-staging-area');
+    } else if (4 < windows.length) {
+      // Stop layouting if there are more than four windows
+      return;
+    }
+
+    // Get references to stack viewer windows
+    var xyWin = orientations[Stack.ORIENTATION_XY].getWindow();
+    var xzWin = orientations[Stack.ORIENTATION_XZ].getWindow();
+    var zyWin = orientations[Stack.ORIENTATION_ZY].getWindow();
+
+    // Find fourth window
+    var extraWin = rootWindow.getWindows().filter(function(w) {
+      return w !== xyWin && w !== xzWin && w !== zyWin;
+    });
+
+    // Raise error if there is more than one extra window
+    if (1 !== extraWin.length) {
+      throw CATMAID.Error("Couldn't find extra window for layouting");
+    }
+
+    // Arrange windows in four-pane layout
+    var left = new CMWVSplitNode(xyWin, xzWin)
+    var right = new CMWVSplitNode(zyWin, extraWin[0])
+    rootWindow.replaceChild(new CMWHSplitNode(left, right));
+  }
+};
 
 /**
  * Check if the client CATMAID version matches the server version. If it does

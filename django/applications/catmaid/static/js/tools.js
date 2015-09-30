1367fd8053ef99efee1c7069bdbe4279a3bf3634
Tom Kazimiers <tom@voodoo-arts.net>
2015-09-30 13:18:53 -0400

    Make hex-to-rgb conversion able to deal with short-hand hex
    It now can convert strings like #555 as well.

--- django/applications/catmaid/static/js/tools.js
+++ django/applications/catmaid/static/js/tools.js
@@ -244,6 +244,13 @@ CATMAID.tools = CATMAID.tools || {};
    * Convert a hex color string to an RGB object.
    */
   tools.hexToRGB = function(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    // See http://stackoverflow.com/questions/5623838
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),

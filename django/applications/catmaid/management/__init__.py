catmaid/CATMAID

commit be76376704b8853e4f7c439bcc0970ca77ebae53

Ensure projects have all expected classes and relations
After every database migration run (manage.py migrate) it is checked if
all projects have the classes and relations they expect. If a project is
added the same thing is done for the new instance. This allows default
classes and relations to be available for projects.

There is currently only one default class: stackgroup. There are two
default relations: has_view, has_channel.

django/applications/catmaid/management/__init__.py
@@ -1,8 +1,12 @@
 import catmaid.models
 
 from django.db.models import signals
-from catmaid.models import Project
+from catmaid import get_system_user
+from catmaid.models import Project, User
 from catmaid.conf import settings as catmaid_settings
+from catmaid.control import project
+from south import signals as south_signals
+
 
 def init_classification(sender, app, created_models, **kwargs):
     """ Creates a dummy project to store classification graphs in.
@@ -15,4 +19,24 @@ def init_classification(sender, app, created_models, **kwargs):
             Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
                 title="Classification dummy project")
 
+
+def validate_projects(app, **kwargs):
+    """Make sure all projects have the relations and classes available they
+    expect."""
+
+    has_users = User.objects.all().exists()
+    has_projects = Project.objects.exclude(
+        pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID).exists()
+    if not (has_users and has_projects):
+        # In case there is no user and only no project except thei ontology
+        # dummy project, don't do the check. Otherwise, getting a system user
+        # will fail.
+        return
+
+    user = get_system_user()
+    for p in Project.objects.all():
+        project.validate_project_setup(p.id, user.id)
+
+
 signals.post_syncdb.connect(init_classification)
+south_signals.post_migrate.connect(validate_projects)

django/applications/catmaid/__init__.py
@@ -49,6 +49,25 @@ def validate_configuration():
             raise ImproperlyConfigured("Please make sure settings field %s "
                     "is of type %s" % (field, data_type))
 
+def get_system_user():
+    """Return a User instance of a superuser. This is either the superuser
+    having the ID configured in SYSTEM_USER_ID or the superuser with the lowest
+    ID."""
+    if hasattr(settings, "SYSTEM_USER_ID"):
+        try:
+            return User.objects.get(id=settings.SYSTEM_USER_ID, is_superuser=True)
+        except User.DoesNotExist:
+            raise ImproperlyConfigured("Could not find any super user with ID "
+                                       "configured in SYSTEM_USER_ID (%s), "
+                                       "please fix this in settings.py" % settings.SYSTEM_USER_ID)
+    else:
+        # Find admin user with lowest id
+        users = User.objects.filter(is_superuser=True).order_by('id')
+        if not len(users):
+            raise ImproperlyConfigured("Couldn't find any super user, " +
+                                       "please make sure you have one")
+        return users[0]
+
 
 def check_superuser():
     """Make sure there is at least one superuser available and, if configured,

django/applications/catmaid/control/project.py
@@ -8,8 +8,32 @@ from django.db import connection
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from catmaid.models import UserRole, Class, Project, Stack
+from catmaid.models import UserRole, Class, Project, Stack, Relation
 from catmaid.control.authentication import requires_user_role
+
+# All classes needed by the tracing system alongside their
+# descriptions.
+needed_classes = {
+    'stackgroup': "An identifier for a group of stacks",
+}
+
+# All relations needed by the tracing system alongside their
+# descriptions.
+needed_relations = {
+    'has_channel': "A stack group can have assosiated channels",
+    'has_view': "A stack group can have assosiated orthogonal views",
+}
+
+def validate_project_setup(project_id, user_id):
+    """Will create needed class and relations if they don't exist.
+    """
+    for nc, desc in needed_classes.iteritems():
+        Class.objects.get_or_create(project_id=project_id,
+                class_name=nc, defaults={'user_id': user_id})
+
+    for nr, desc in needed_relations.iteritems():
+        Relation.objects.get_or_create(project_id=project_id,
+                relation_name=nr, defaults={'user_id': user_id})
 
 @requires_user_role([UserRole.Annotate, UserRole.Browse])
 def list_project_tags(request, project_id=None):

django/applications/catmaid/models.py
@@ -49,6 +49,18 @@ class Project(models.Model):
     def __unicode__(self):
         return self.title
 
+def on_project_save(sender, instance, created, **kwargs):
+    """ Make sure all required classes and relations are set up.
+    """
+    if created and sender == Project:
+        from control.project import validate_project_setup
+        from catmaid import get_system_user
+        user = get_system_user()
+        validate_project_setup(instance.id, user.id)
+
+# Validate project when they are saved
+post_save.connect(on_project_save, sender=Project)
+
 class Stack(models.Model):
     class Meta:
         db_table = "stack"

commit 0d02fc036776175c1a40d5ab8bd2d85366cb3263

Do project validation only after CATMAID migrations
Project validation expects of course the project database tables to be
present. Therefore, it is not the correct thing to run this test after
every the South migration of *every* Django application, but only after
the migration of catmaid itself.

Also move both project and ontology dummy project test in one function
that makes sure both are run only after the migration of CATMAID.

Fixes a problem introduced with be76376. Thanks @aschampion for the
pointer.

django/applications/catmaid/management/__init__.py
@@ -8,22 +8,20 @@ from catmaid.control import project
 from south import signals as south_signals
 
 
-def init_classification(sender, app, created_models, **kwargs):
+def init_classification():
     """ Creates a dummy project to store classification graphs in.
     """
-    if sender == catmaid.models:
-        try:
-            Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
-        except Project.DoesNotExist:
-            print("Creating ontology dummy project")
-            Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
-                title="Classification dummy project")
+    try:
+        Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
+    except Project.DoesNotExist:
+        print("Creating ontology dummy project")
+        Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
+            title="Classification dummy project")
 
 
 def validate_projects(app, **kwargs):
     """Make sure all projects have the relations and classes available they
     expect."""
-
     has_users = User.objects.all().exists()
     has_projects = Project.objects.exclude(
         pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID).exists()
@@ -38,5 +36,15 @@ def validate_projects(app, **kwargs):
         project.validate_project_setup(p.id, user.id)
 
 
-signals.post_syncdb.connect(init_classification)
-south_signals.post_migrate.connect(validate_projects)
+def validate_environment(app, **kwargs):
+    """Make sure CATMAID is set up correctly."""
+    # Only validate after catmaid was migrated
+    if app != catmaid:
+        return
+
+    validate_projects()
+    init_classification()
+
+
+# Validate CATMAID environment after all South migrations have been run
+south_signals.post_migrate.connect(validate_environment)
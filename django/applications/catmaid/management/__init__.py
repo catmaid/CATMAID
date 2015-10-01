0d02fc036776175c1a40d5ab8bd2d85366cb3263
Tom Kazimiers <tom@voodoo-arts.net>
2015-09-30 21:18:47 -0400

    Do project validation only after CATMAID migrations
    Project validation expects of course the project database tables to be
    present. Therefore, it is not the correct thing to run this test after
    every the South migration of *every* Django application, but only after
    the migration of catmaid itself.
    
    Also move both project and ontology dummy project test in one function
    that makes sure both are run only after the migration of CATMAID.
    
    Fixes a problem introduced with be76376. Thanks @aschampion for the
    pointer.

--- django/applications/catmaid/management/__init__.py
+++ django/applications/catmaid/management/__init__.py
@@ -8,22 +8,20 @@ from catmaid.control import project
 from south import signals as south_signals
 
 
-def init_classification(sender, app, created_models, **kwargs):
+def init_classification():
     """ Creates a dummy project to store classification graphs in.
     """
-    if sender == catmaid.models:
-        try:
-            Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
-        except Project.DoesNotExist:
-            print("Creating ontology dummy project")
-            Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
-                title="Classification dummy project")
+    try:
+        Project.objects.get(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID)
+    except Project.DoesNotExist:
+        print("Creating ontology dummy project")
+        Project.objects.create(pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID,
+            title="Classification dummy project")
 
 
 def validate_projects(app, **kwargs):
     """Make sure all projects have the relations and classes available they
     expect."""
-
     has_users = User.objects.all().exists()
     has_projects = Project.objects.exclude(
         pk=catmaid_settings.ONTOLOGY_DUMMY_PROJECT_ID).exists()
@@ -38,5 +36,15 @@ def validate_projects(app, **kwargs):
         project.validate_project_setup(p.id, user.id)
 
 
-signals.post_syncdb.connect(init_classification)
-south_signals.post_migrate.connect(validate_projects)
+def validate_environment(app, **kwargs):
+    """Make sure CATMAID is set up correctly."""
+    # Only validate after catmaid was migrated
+    if app != catmaid:
+        return
+
+    validate_projects()
+    init_classification()
+
+
+# Validate CATMAID environment after all South migrations have been run
+south_signals.post_migrate.connect(validate_environment)

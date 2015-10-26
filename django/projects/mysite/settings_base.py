f7270fb9a4aad067952a6fe917c6eecc98359b7f
Tom Kazimiers <tom@voodoo-arts.net>
2015-10-25 22:24:18 -0400

    Add GeoDjango app to configuration
    This is required to be able to use GeoDjango models. Without it e.g.
    South won't be able to create migrations for those fields.

--- django/projects/mysite/settings_base.py
+++ django/projects/mysite/settings_base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'devserver',
     'djcelery',
     'taggit',

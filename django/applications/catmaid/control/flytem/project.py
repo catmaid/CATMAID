883b928968a36ca4c486f450a9461c21d997a500
Eric Trautman <trautmane@janelia.hhmi.org>
2015-11-24 13:27:30 -0500

    committing change for tomka: group render service stacks by project

--- django/applications/catmaid/control/flytem/project.py
+++ django/applications/catmaid/control/flytem/project.py
@@ -9,21 +9,27 @@ def projects(request):
     """
     project_stacks = FlyTEMProjectStacks()
 
-    projects = []
-    for p in project_stacks.data:
-        projects.append({
-            'id': p['stack'],
-            'title': '%s: %s' % (p['project'], p['stack']),
-            'comment': '',
-            'stacks': [
-                {
-                    'id': p['stack'],
-                    'title': p['stack'],
-                    'comment': '<p>Owner: %s</p>' % p['owner'],
-                }
-            ],
-            'stackgroups': []
+    projects = {}
+    for ps in project_stacks.data:
+        pid = ps['project']
+        p = projects.get(pid)
+        if not p:
+            p = {
+                'id': pid,
+                'title': ps['project'],
+                'comment': '<p>Owner: %s</p>' % ps['owner'],
+                'stacks': [],
+                'stackgroups': []
+            }
+            projects[pid] = p
+
+        p['stacks'].append({
+            'id': ps['stack'],
+            'title': ps['stack'],
+            'comment': ''
         })
 
-    return HttpResponse(json.dumps(projects, sort_keys=True, indent=2),
+    response = [v for k,v in projects.items()]
+
+    return HttpResponse(json.dumps(response, sort_keys=True, indent=2),
                         content_type="text/json")

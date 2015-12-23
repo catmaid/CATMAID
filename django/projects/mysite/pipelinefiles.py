de54efa970e5a1719bbc26c8dce88df1e8086893
Andrew Champion <andrew.champion@gmail.com>
2015-12-23 15:29:51 -0500

    Pipeline: docstring and whitespace cleanup

--- django/projects/mysite/pipelinefiles.py
+++ django/projects/mysite/pipelinefiles.py
@@ -1,19 +1,23 @@
-# In this module all the static files are specified that are required by the
-# CATMAID front-end. The configuration is separated in libraries and CATMAID's
-# own files:
-#
-# Libraries: To add a new library, add a new entry into the libraries_js
-# dictionary and, if needed, add the libraries CSS files to sourcefiles
-# tuple of the 'library' entry in the PIPELINE_CSS dictionary.
-#
-# CATMAID files: By default all CSS files in the static/css directory are
-# included as well as all JavaScript files in static/js and CATMAID's
-# subdirectories in it. However, if you want to add new files explicitly, add
-# CSS to the source_filenames tuple in the 'catmaid' entry of the PIPELINE_CSS
-# dictionary. JavaScript files go into the 'catmaid' entry of the PIPELINE_JS
-# dictonary at the end of this file.
+"""Specifies static assets (CSS, JS) required by the CATMAID front-end.
+
+This module specifies all the static files that are required by the
+CATMAID front-end. The configuration is separated in libraries and CATMAID's
+own files:
+
+Libraries: To add a new library, add a new entry into the libraries_js
+dictionary and, if needed, add the libraries CSS files to sourcefiles
+tuple of the 'library' entry in the ``PIPELINE_CSS`` dictionary.
+
+CATMAID files: By default all CSS files in the ``static/css`` directory are
+included as well as all JavaScript files in ``static/js`` and CATMAID's
+subdirectories in it. However, if you want to add new files explicitly, add
+CSS to the source_filenames tuple in the 'catmaid' entry of the ``PIPELINE_CSS``
+dictionary. JavaScript files go into the 'catmaid' entry of the ``PIPELINE_JS``
+dictonary at the end of this file.
+"""
 
 from collections import OrderedDict
+
 
 PIPELINE_CSS = {
     'libraries': {
@@ -72,9 +76,9 @@ libraries_js = {
 
 PIPELINE_JS = OrderedDict()
 
-for k,v in libraries_js.iteritems():
+for k, v in libraries_js.iteritems():
     PIPELINE_JS[k + '-lib'] = {
-        'source_filenames': ['libs/%s/%s' % (k,f) for f in v],
+        'source_filenames': ['libs/%s/%s' % (k, f) for f in v],
         'output_filename': 'js/libs/%s-lib.js' % k,
     }
 
@@ -93,7 +97,7 @@ non_pipeline_js = {
 
 # Even non-pipeline files have to be made known to pipeline, because it takes
 # care of collecting them into the STATIC_ROOT directory.
-for k,v in non_pipeline_js.iteritems():
+for k, v in non_pipeline_js.iteritems():
     PIPELINE_JS[k] = {
         'source_filenames': (v,),
         'output_filename': v

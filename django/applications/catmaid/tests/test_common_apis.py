6e27501d39cab6d756a2c145c0600de0e6cb09ca
Andrew Champion <andrew.champion@gmail.com>
2015-12-26 12:22:25 -0500

    Tests: test trivial compact skeleton/arbor export

--- django/applications/catmaid/tests/test_common_apis.py
+++ django/applications/catmaid/tests/test_common_apis.py
@@ -3429,6 +3429,48 @@ class ViewPageTests(TestCase):
                 '/%d/treenodes/%d/suppressed-virtual/%d' % (self.test_project_id, child_id, suppressed_id))
         self.assertEqual(response.status_code, 204)
 
+    def test_export_compact_skeleton(self):
+        self.fake_authentication()
+
+        skeleton_id = 373
+        response = self.client.post(
+                '/%d/%d/1/1/compact-skeleton' % (self.test_project_id, skeleton_id))
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        expected_response = [
+                [[377, None, 3, 7620.0, 2890.0, 0.0, -1.0, 5],
+                 [403, 377, 3, 7840.0, 2380.0, 0.0, -1.0, 5],
+                 [405, 377, 3, 7390.0, 3510.0, 0.0, -1.0, 5],
+                 [407, 405, 3, 7080.0, 3960.0, 0.0, -1.0, 5],
+                 [409, 407, 3, 6630.0, 4330.0, 0.0, -1.0, 5]],
+                [[377, 356, 1, 6730.0, 2700.0, 0.0],
+                 [409, 421, 1, 6260.0, 3990.0, 0.0]],
+                {"uncertain end": [403]}]
+        self.assertItemsEqual(parsed_response[0], expected_response[0])
+        self.assertItemsEqual(parsed_response[1], expected_response[1])
+        self.assertEqual(parsed_response[2], expected_response[2])
+
+    def test_export_compact_arbor(self):
+        self.fake_authentication()
+
+        skeleton_id = 373
+        response = self.client.post(
+                '/%d/%d/1/1/1/compact-arbor' % (self.test_project_id, skeleton_id))
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        expected_response = [
+                [[377, None, 3, 7620.0, 2890.0, 0.0, -1.0, 5],
+                 [403, 377, 3, 7840.0, 2380.0, 0.0, -1.0, 5],
+                 [405, 377, 3, 7390.0, 3510.0, 0.0, -1.0, 5],
+                 [407, 405, 3, 7080.0, 3960.0, 0.0, -1.0, 5],
+                 [409, 407, 3, 6630.0, 4330.0, 0.0, -1.0, 5]],
+                [[377, 5, 356, 5, 285, 235, 1, 0],
+                 [409, 5, 421, 5, 415, 235, 1, 0]],
+                {"uncertain end": [403]}]
+        self.assertItemsEqual(parsed_response[0], expected_response[0])
+        self.assertItemsEqual(parsed_response[1], expected_response[1])
+        self.assertEqual(parsed_response[2], expected_response[2])
+
 
 class TreenodeTests(TestCase):
     fixtures = ['catmaid_testdata']

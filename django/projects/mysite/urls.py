83652e1cdfebf31d4b771a7ffdfdaed0d47281bb
Tom Kazimiers <tom@voodoo-arts.net>
2015-12-30 00:57:14 -0500

    Use new urlconf definition style
    Instead of collecting result of calls to the pattern(prefix, url_list)
    function, since Django 1.8 urlpatterns can now be a simple list of url()
    instances. Due to this change now actual view function references are
    used (like it is already common with class based views). For more
    details see:
    
    https://docs.djangoproject.com/en/1.9/releases/1.8/#django-conf-urls-patterns

--- django/projects/mysite/urls.py
+++ django/projects/mysite/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+import re
+
+from django.conf.urls import include, url
+from django.views.static import serve
 from django.conf import settings
 
-from catmaid.views import *
 from catmaid.control.authentication import ObtainAuthToken
-
-import catmaid
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,28 +13,33 @@ admin.site = AdminSitePlus()
 admin.autodiscover()
 
 # CATMAID
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include('catmaid.urls')),
-)
+]
 
 # Admin site
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^admin/', include(admin.site.urls))
-)
+]
 
 # API Documentation
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^apis/', include('rest_framework_swagger.urls')),
     url(r'^api-token-auth/', ObtainAuthToken.as_view()),
-)
+]
 
-
+# Serve static files in debug mode and if explicitely requested
 if settings.DEBUG or settings.SERVE_STATIC:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-        # Access to static estensions in debug mode, remove leading slash.
-        (r'^%s(?P<path>.*)$' % settings.STATIC_EXTENSION_URL[1:],
-            'django.views.static.serve', {'document_root': settings.STATIC_EXTENSION_ROOT}),
-        (r'^%s(?P<path>.*)$' % settings.MEDIA_URL.replace(settings.CATMAID_URL, ''),
-            'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+    def serve_static(prefix, root):
+        return url(r'^%s(?P<path>.*)$' % re.escape(prefix), serve,
+                kwargs={'document_root': root})
+
+    urlpatterns += [
+        # General static files
+        serve_static('static/', settings.STATIC_ROOT),
+        # Access to static extensions in debug mode, remove leading slash.
+        serve_static(settings.STATIC_EXTENSION_URL[1:], settings.STATIC_EXTENSION_ROOT),
+        # Media files, i.e. cropped images or exports
+        serve_static(settings.MEDIA_URL.replace(settings.CATMAID_URL, ''),
+            settings.MEDIA_ROOT)
+    ]

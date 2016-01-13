6857dec5306ed472b91286983bc8972c2ae7a558
Albert Cardona <sapristi@gmail.com>
2016-01-13 17:22:13 -0500

    Geometry: tiny javascript file to generate geometry.js containing the convexHull function from node's 'convex-hull' package:
    https://github.com/mikolalysenko/convex-hull

---
+++ django/applications/catmaid/static/libs/geometry/geometry-wrapper.js
@@ -0,0 +1,14 @@
+/** First install the convex-hull node package:
+*
+* $ npm install convex-hull
+*
+* Then convert this tiny script into a browser-ready file
+* than makes the convex hull function reachable from CATMAID:
+*
+* $ browserify geometry-wrapper.js -o geometry.js
+*
+* The resulting geometry.js file is committed to CATMAID's git repository.
+*/
+window.CATMAID.geometryTools = {
+	convexHull: require("convex-hull")
+};

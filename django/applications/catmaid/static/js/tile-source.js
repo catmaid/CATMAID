d3cbb5dc6f682c97a4d5bcd16e204bdf2dba7edd
Tom Kazimiers <tom@voodoo-arts.net>
2016-03-18 19:36:32 -0400

    FlixServer tile source: mention new quality value range in help text

--- django/applications/catmaid/static/js/tile-source.js
+++ django/applications/catmaid/static/js/tile-source.js
@@ -305,7 +305,7 @@
         {name: 'gamma', displayName: 'Gamma', type: 'text', range: [0, Number.MAX_VALUE],
           value: this.gamma, help: 'Exponent of non-linear mapping, e.g. 1.0, use comma for multiple channels'},
         {name: 'quality', displayName: 'Quality', type: 'number', range: [0, 100],
-          value: this.quality, help: 'Image quality in range 0-1, use comma for multiple channels'}
+          value: this.quality, help: 'Image quality in range 0-100, use comma for multiple channels'}
       ];
     };
 

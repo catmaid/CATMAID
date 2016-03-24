a165ca73e384e81bcdfe9b98b55ce93b57b2d69d
Tom Kazimiers <tom@voodoo-arts.net>
2016-03-24 17:15:31 -0400

    Return edition time of updated nodes on radius edit

--- django/applications/catmaid/control/treenode.py
+++ django/applications/catmaid/control/treenode.py
@@ -407,13 +407,13 @@ def update_node_radii(node_ids, radii, cursor=None):
 	      INNER JOIN (VALUES {}) y(id, new_radius)
 	      ON x.id=y.id FOR UPDATE) target
 	WHERE t.id = target.id
-	RETURNING t.id, target.old_radius, target.new_radius;
+	RETURNING t.id, target.old_radius, target.new_radius, t.edition_time;
     '''.format(node_radii))
 
     updated_rows = cursor.fetchall()
     if 0 == len(updated_rows):
         raise ValueError('Coudn\'t find treenode #' + treenode_id)
-    return {r[0]: {'old': r[1], 'new': float(r[2])} for r in updated_rows}
+    return {r[0]: {'old': r[1], 'new': float(r[2]), 'edition_time': r[3]} for r in updated_rows}
 
 @requires_user_role(UserRole.Annotate)
 def update_radii(request, project_id=None):

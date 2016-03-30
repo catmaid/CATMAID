458ecdde7a983c7d12e3249fc1a70b04f45d19de
Andrew Champion <andrew.champion@gmail.com>
2016-03-30 15:41:04 -0400

    Ensure settings client datastore exists
    Because the migration creating the settings client datastore does not
    exist in the new Django migrations system, make sure this datastore
    exists for new installs. This could be accomplished by the client
    without a migration, but would be much more complex and brittle to
    future changes in, e.g., permissions.
    
    Closes catmaid/CATMAID#1289.

---
+++ django/applications/catmaid/migrations/0002_create_settings_datastore.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def create_settings_datastore(apps, schema_editor):
+    ClientDatastore = apps.get_model("catmaid", "ClientDatastore")
+    ClientDatastore.objects.get_or_create(name="settings")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catmaid', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_settings_datastore),
+    ]

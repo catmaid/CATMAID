5e1788fbc9a2c33584ec6259f8608c00479c05c1
Tom Kazimiers <tom@voodoo-arts.net>
2016-08-08 14:45:21 -0400

    Log table: use prototypal function definitions
    This is done in preparation of some additional features.

--- django/applications/catmaid/static/js/widgets/table_log.js
+++ django/applications/catmaid/static/js/widgets/table_log.js
@@ -9,116 +9,116 @@
 
     var self = this;
     var asInitValsSyn = [];
+  };
 
-    /**
-     * Redraw the complete log table.
-     */
-    this.update = function() {
-      this.logTable.fnClearTable( 0 );
-      this.logTable.fnDraw();
-    };
+  /**
+   * Redraw the complete log table.
+   */
+  LogTable.prototype.update = function() {
+    this.logTable.fnClearTable( 0 );
+    this.logTable.fnDraw();
+  };
 
-    this.init = function (pid) {
-      var tableid = '#logtable';
-      var possibleLengths = CATMAID.pageLengthOptions;
-      var possibleLengthsLabels = CATMAID.pageLengthLabels;
+  LogTable.prototype.init = function (pid) {
+    var tableid = '#logtable';
+    var possibleLengths = CATMAID.pageLengthOptions;
+    var possibleLengthsLabels = CATMAID.pageLengthLabels;
 
-      self.logTable = $(tableid).dataTable({
-        // http://www.datatables.net/usage/options
-        "bDestroy": true,
-        "sDom": '<"H"lr>t<"F"ip>',
-        // default: <"H"lfr>t<"F"ip>
-        "bProcessing": true,
-        "bServerSide": true,
-        "bAutoWidth": false,
-        "iDisplayLength": possibleLengths[0],
-        "sAjaxSource": django_url + project.id + '/logs/list',
-        "fnServerData": function (sSource, aoData, fnCallback) {
-            var user_id = $('#logtable_username').val();
-            if (!isNaN(user_id)) {
-                aoData.push({
-                    name: "user_id",
-                    value: user_id
-                });
-            } else if (user_id === 'Team') {
-                aoData.push({
-                    name: "whitelist",
-                    value: true
-                });
-            }
-            aoData.push({
-                "name" : "pid",
-                "value" : pid
-            });
-            aoData.push({
-                "name": "operation_type",
-                "value" : $('#logtable_operationtype').val()
-            });
-            aoData.push({
-                "name": "search_freetext",
-                "value" : $('#search_freetext').val()
-            });
-            $.ajax({
-                "dataType": 'json',
-                "cache": false,
-                "type": "POST",
-                "url": sSource,
-                "data": aoData,
-                "success": fnCallback
-            });
-        },
-        "aLengthMenu": [
-            possibleLengths,
-            possibleLengthsLabels
-        ],
-        "bJQueryUI": true,
-        "aaSorting": [[ 2, "desc" ]],
-        "aoColumns": [
-            { // user
-                "bSearchable": false,
-                "bSortable": true
-            },
-            { // operation
-                "sClass": "center",
-                "bSearchable": false,
-                "bSortable": true
-            },
-            { // timestamp
-                "sClass": "center",
-                "bSearchable": false,
-                "bSortable": true
-            },
-            { // x
-                "sClass": "center",
-                "bSearchable": false,
-                "bSortable": false
-            },
-            { // y
-                "sClass": "center",
-                "bSearchable": false,
-                "bSortable": false
-            },
-            { // z
-                "sClass": "center",
-                "bSearchable": false,
-                "bSortable": false
-            },
-            { // freetext
-                "bSearchable": false,
-                "bSortable": false
-            }
-        ]
-      });
+    self.logTable = $(tableid).dataTable({
+      // http://www.datatables.net/usage/options
+      "bDestroy": true,
+      "sDom": '<"H"lr>t<"F"ip>',
+      // default: <"H"lfr>t<"F"ip>
+      "bProcessing": true,
+      "bServerSide": true,
+      "bAutoWidth": false,
+      "iDisplayLength": possibleLengths[0],
+      "sAjaxSource": django_url + project.id + '/logs/list',
+      "fnServerData": function (sSource, aoData, fnCallback) {
+          var user_id = $('#logtable_username').val();
+          if (!isNaN(user_id)) {
+              aoData.push({
+                  name: "user_id",
+                  value: user_id
+              });
+          } else if (user_id === 'Team') {
+              aoData.push({
+                  name: "whitelist",
+                  value: true
+              });
+          }
+          aoData.push({
+              "name" : "pid",
+              "value" : pid
+          });
+          aoData.push({
+              "name": "operation_type",
+              "value" : $('#logtable_operationtype').val()
+          });
+          aoData.push({
+              "name": "search_freetext",
+              "value" : $('#search_freetext').val()
+          });
+          $.ajax({
+              "dataType": 'json',
+              "cache": false,
+              "type": "POST",
+              "url": sSource,
+              "data": aoData,
+              "success": fnCallback
+          });
+      },
+      "aLengthMenu": [
+          possibleLengths,
+          possibleLengthsLabels
+      ],
+      "bJQueryUI": true,
+      "aaSorting": [[ 2, "desc" ]],
+      "aoColumns": [
+          { // user
+              "bSearchable": false,
+              "bSortable": true
+          },
+          { // operation
+              "sClass": "center",
+              "bSearchable": false,
+              "bSortable": true
+          },
+          { // timestamp
+              "sClass": "center",
+              "bSearchable": false,
+              "bSortable": true
+          },
+          { // x
+              "sClass": "center",
+              "bSearchable": false,
+              "bSortable": false
+          },
+          { // y
+              "sClass": "center",
+              "bSearchable": false,
+              "bSortable": false
+          },
+          { // z
+              "sClass": "center",
+              "bSearchable": false,
+              "bSortable": false
+          },
+          { // freetext
+              "bSearchable": false,
+              "bSortable": false
+          }
+      ]
+    });
 
-      $(tableid + " tbody").on('dblclick', 'tr', function () {
-          var aData = self.logTable.fnGetData(this);
-          // retrieve coordinates and moveTo
-          var x = parseFloat(aData[3]);
-          var y = parseFloat(aData[4]);
-          var z = parseFloat(aData[5]);
-          project.moveTo(z, y, x);
-      });
-    };
+    $(tableid + " tbody").on('dblclick', 'tr', function () {
+        var aData = self.logTable.fnGetData(this);
+        // retrieve coordinates and moveTo
+        var x = parseFloat(aData[3]);
+        var y = parseFloat(aData[4]);
+        var z = parseFloat(aData[5]);
+        project.moveTo(z, y, x);
+    });
   };
 
   // Export log table as singleton instance

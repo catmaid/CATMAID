catmaid/CATMAID

commit 2aad8482a7f5f89a5f02d21c61ef437a6f247f94

Annotations: prevent infinite loop for annotation cycles

django/applications/catmaid/control/neuron_annotations.py
@@ -149,9 +149,10 @@ def get_sub_annotation_ids(project_id, annotation_sets, relations, classes):
                 child_ids = aaa.get(parent_id) or set_wrapper()
                 for child_id in child_ids.data:
                     if child_id not in sa_ids:
-                        # Add all children as sub annotations
-                        ls.add(child_id)
-                        working_set.add(child_id)
+                        if child_id not in ls:
+                            # Add all children as sub annotations
+                            ls.add(child_id)
+                            working_set.add(child_id)
         # Store the result list for this ID
         sa_ids[annotation_set] = list(ls)
 

commit e1131e66d64a077aaa7efcbff852d35310654045

Annotations: fix query targets with only non-annotation criteria
Fix query targets when no annotation criteria are provided but other
criteria are. This was broken by
2aad8482a7f5f89a5f02d21c61ef437a6f247f94.

django/applications/catmaid/control/neuron_annotations.py
@@ -98,7 +98,12 @@ def create_basic_annotated_entity_query(project, params, relations, classes,
             entities = entities.filter(
                 cici_via_a__relation_id=annotated_with,
                 cici_via_a__class_instance_b_id__in=annotation_id_set)
+    elif len(filters) > 2:
+        # If some filters were set, but no annotations, apply them.
+        entities = ClassInstance.objects.filter(**filters)
     else:
+        # Otherwise no constraints were specified, so return nothing rather
+        # than everything.
         entities = ClassInstance.objects.none()
 
     # Create final query. Without any restriction, the result set will contain

django/applications/catmaid/tests/test_common_apis.py
@@ -4078,6 +4078,21 @@ class ViewPageTests(TestCase):
         parsed_response = json.loads(response.content)
         self.assertEqual(parsed_response['totalRecords'], 0)
 
+        # Test that searching by name without any annotation still works.
+        response = self.client.post(
+            '/%d/annotations/query-targets' % (self.test_project_id,),
+            {'name': 'downstream-A'})
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        expected_entities = sorted([
+            {'skeleton_ids': [373],
+            'type': 'neuron',
+            'id': 374,
+            'name': 'downstream-A'}])
+        self.assertEqual(parsed_response['totalRecords'], 1)
+        self.assertItemsEqual(parsed_response['entities'], expected_entities)
+
+
     def test_review_status(self):
         self.fake_authentication()
 
03e5485b9df83334a6f0552c51c6ca24dfa7b249
Tom Kazimiers <tom@voodoo-arts.net>
2016-08-23 21:21:57 -0400

    Transaction log: add tes für basic treenode location lookup

--- django/applications/catmaid/tests/test_transaction_log.py
+++ django/applications/catmaid/tests/test_transaction_log.py
@@ -1,15 +1,17 @@
 import json
 import time
 
+from django.core.cache import cache
 from django.db import connection, transaction, InternalError
-from django.test import Client, TestCase
+from django.test import Client, TestCase, TransactionTestCase
 from guardian.shortcuts import assign_perm
 from catmaid import history
 from catmaid.models import Class, Project, User
 from catmaid.control import tracing
+from catmaid.state import make_nocheck_state
 
 
-class TransactionLogTests(TestCase):
+class TransactionLogTests(TransactionTestCase):
     """Test the transaction log implementation, expecting an empty database.
     """
 
@@ -17,6 +19,8 @@ class TransactionLogTests(TestCase):
         """Create a project and a suer with browse/annotate permissions on
         it. Both are referenced when creating
         """
+        cache.clear()
+
         self.client = Client()
         self.user = User.objects.create(username="test")
         self.user.set_password("test")
@@ -30,9 +34,26 @@ class TransactionLogTests(TestCase):
 
         # Set project up for tracing
         tracing.setup_tracing(self.project.id, self.admin)
+        self.authenticate()
+
+    def run(self, *args):
+        """Wrap running of individual tests to make sure the transaction log is
+        truncated after every test run. This table isn't taken care of by
+        Django, because it is not known as a model to it.
+        """
+        result = super(TransactionLogTests, self).run(*args)
+        self.reset_tx_log()
+        return result
 
     def authenticate(self):
         self.client.login(username="test", password="test")
+
+    @staticmethod
+    def reset_tx_log(cursor=None):
+        cursor = cursor or connection.cursor()
+        cursor.execute("""
+            TRUNCATE TABLE catmaid_transaction_info;
+        """)
 
     @staticmethod
     def get_tx_entries(cursor):
@@ -61,9 +82,20 @@ class TransactionLogTests(TestCase):
         else:
             return "[\"{}\",)".format(timestamp1)
 
+    def get_location(self, txid, exec_time, label):
+        """ Get location based on the transaction ID and time"""
+        response = self.client.get('/%d/transactions/location' % self.project.id,
+                {
+                    'transaction_id': txid,
+                    'execution_time': exec_time,
+                    'label': label
+                })
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        return parsed_response
+
     def test_transaction_log_entry(self):
         """Every node creation in the back-end should cause a new"""
-        self.authenticate()
         cursor = connection.cursor()
         original_tx_entries = self.get_tx_entries(cursor)
         n_original_tx_entries = len(original_tx_entries)
@@ -78,6 +110,7 @@ class TransactionLogTests(TestCase):
             'radius': 2})
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content)
+        transaction.commit()
 
         self.assertTrue('treenode_id' in parsed_response)
         self.assertTrue('skeleton_id' in parsed_response)
@@ -89,6 +122,7 @@ class TransactionLogTests(TestCase):
 
         self.assertEqual('Backend', after_insert_tx['change_type'])
         self.assertEqual('treenodes.create', after_insert_tx['label'])
+
         self.assertEqual(self.user.id, after_insert_tx['user_id'])
         self.assertEqual(self.project.id, after_insert_tx['project_id'])
 
@@ -110,3 +144,77 @@ class TransactionLogTests(TestCase):
         self.assertEqual(2, nt['radius'])
         self.assertEqual(None, nt['parent_id'])
         self.assertEqual(self.project.id, nt['project_id'])
+
+    def test_locaton_lookup_treenode(self):
+        """"""
+        cursor = connection.cursor()
+
+        # Create a new treenode
+        response = self.client.post('/%d/treenode/create' % self.project.id, {
+            'x': 5.5,
+            'y': 10.5,
+            'z': 15.5,
+            'confidence': 5,
+            'parent_id': -1,
+            'radius': 2})
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        transaction.commit()
+
+        self.assertTrue('treenode_id' in parsed_response)
+        self.assertTrue('skeleton_id' in parsed_response)
+        treenode_id = parsed_response['treenode_id']
+
+        after_insert_tx_entries = self.get_tx_entries(cursor)
+        n_after_insert_tx_entries = len(after_insert_tx_entries)
+        after_insert_tx = after_insert_tx_entries[0][0]
+
+        txid = after_insert_tx['transaction_id']
+        exec_time = after_insert_tx['execution_time']
+        label = after_insert_tx['label']
+
+        # Get location based on the transaction ID and time, expect it to be the
+        # same like the location of the created node above.
+        txid_locaton = self.get_location(txid, exec_time, label)
+
+        self.assertEqual(5.5, txid_locaton['x'])
+        self.assertEqual(10.5, txid_locaton['y'])
+        self.assertEqual(15.5, txid_locaton['z'])
+
+        # Update the node location and
+        response = self.client.post(
+                '/%d/node/update' % self.project.id, {
+                    'state': make_nocheck_state(),
+                    't[0][0]': treenode_id,
+                    't[0][1]': 6.2,
+                    't[0][2]': 11.2,
+                    't[0][3]': 16.2})
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content)
+        transaction.commit()
+
+        after_update_tx_entries = self.get_tx_entries(cursor)
+        n_after_update_tx_entries = len(after_update_tx_entries)
+        after_update_tx = after_update_tx_entries[1][0]
+
+        self.assertEqual(n_after_insert_tx_entries + 1,
+                n_after_update_tx_entries)
+
+        txid_2 = after_update_tx['transaction_id']
+        exec_time_2 = after_update_tx['execution_time']
+        label_2 = after_update_tx['label']
+
+        # Test locaton of original node with original transaction
+        txid_locaton = self.get_location(txid, exec_time, label)
+
+        self.assertEqual(5.5, txid_locaton['x'])
+        self.assertEqual(10.5, txid_locaton['y'])
+        self.assertEqual(15.5, txid_locaton['z'])
+
+        # Test locaton of updated node with new transaction
+        txid_locaton_2 = self.get_location(txid_2, exec_time_2, label_2)
+        transaction.commit()
+
+        self.assertEqual(6.2, txid_locaton_2['x'])
+        self.assertEqual(11.2, txid_locaton_2['y'])
+        self.assertEqual(16.2, txid_locaton_2['z'])

f35cef885387189c4aae3c66e909eaad3bb65fe8
Tom Kazimiers <tom@voodoo-arts.net>
2016-11-10 22:30:00 -0500

    Add migration to make sure ontology restriction tables have default value
    All tables inheriting from the concept table share the same ID sequence.
    Those ID sequences are however not used by both the restriction table
    and the cardinality_restriction table, even though they inherit from
    concept. This commit ensures the concept ID sequence is also used with
    those two tables, which is needed for the Ontology Editor to work
    properly.

---
+++ django/applications/catmaid/migrations/0014_ensure_restriction_tables_have_auto_id.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+forward = """
+    ALTER TABLE ONLY restriction ALTER COLUMN id SET DEFAULT nextval('concept_id_seq'::regclass);
+    ALTER TABLE ONLY cardinality_restriction ALTER COLUMN id SET DEFAULT nextval('concept_id_seq'::regclass);
+"""
+
+
+class Migration(migrations.Migration):
+    """This migration makes sure that both restriction tables have proper
+    default values for their ID field.
+    """
+
+    dependencies = [
+        ('catmaid', '0013_add_missing_tnci_and_cnci_indices'),
+    ]
+
+    operations = [
+        migrations.RunSQL(forward, migrations.RunSQL.noop)
+    ]

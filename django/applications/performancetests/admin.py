2b7a58997c3f1cf78b0a4c5702979f1f02b35d22
Tom Kazimiers <tom@voodoo-arts.net>
2017-01-30 15:09:09 -0500

    Performance tests: add duplicate action
    Since this is only a simple function, the code is copied from the
    catmaid Django applicaition.

--- django/applications/performancetests/admin.py
+++ django/applications/performancetests/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import TestView, TestResult, Event
 from .views import TestResultDisplay
 
+
+def duplicate_action(modeladmin, request, queryset):
+    """
+    An action that can be added to individual model admin forms to duplicate
+    selected entries. Currently, it only duplicates each object without any
+    foreign key or many to many relationships.
+    """
+    for object in queryset:
+        object.id = None
+        object.save()
+duplicate_action.short_description = "Duplicate selected without relations"
+
+
 def trimmed_result(obj):
     last_character = min(len(obj.result), 100)
     return "%s ..." % obj.result[:last_character]
@@ -9,6 +22,7 @@ def trimmed_result(obj):
 class TestViewAdmin(admin.ModelAdmin):
     list_display = ('url', 'method', 'data', 'creation_time')
     search_fields = ('url', 'method', 'data')
+    actions = (duplicate_action,)
 
 
 class TestResultAdmin(admin.ModelAdmin):
@@ -21,6 +35,7 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('creation_time', 'title')
     search_fields = ('title',)
     order_by = ('creation_time',)
+    actions = (duplicate_action,)
 
 # Register models with admin site
 admin.site.register(TestView, TestViewAdmin)

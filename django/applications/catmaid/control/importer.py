703f20d44d771fd965ee746bfd4af7e4d5ef7431
Tom Kazimiers <tom@voodoo-arts.net>
2017-02-01 17:39:15 -0500

    Importer: make image base generation more robust
    There doesn't need to be a folder field all the times.

--- django/applications/catmaid/control/importer.py
+++ django/applications/catmaid/control/importer.py
@@ -81,11 +81,11 @@ class ImageBaseMixin:
         elif project_url:
             # The image base of a stack is the combination of the
             # project URL and the stack's folder name.
-            folder = info_object['folder']
             path = info_object.get('path', '')
-            if path:
-                folder = urljoin(path, folder)
-            self.image_base = urljoin(project_url, folder)
+            folder = info_object.get('folder', '')
+            if folder:
+                path = urljoin(path, folder)
+            self.image_base = urljoin(project_url, path)
 
         if data_folder:
             # Only retrieve file extension and zoom level if one of

1f75b5d922da215cbb33059264669bbc869562ed
Tom Kazimiers <tom@voodoo-arts.net>
2017-02-02 00:21:43 -0500

    Tests: add import/export test
    See catmaid/CATMAID#1430

--- django/applications/catmaid/tests/test_import_export.py
+++ django/applications/catmaid/tests/test_import_export.py
@@ -1,4 +1,7 @@
+import yaml
+
 from ast import literal_eval
+from guardian.shortcuts import assign_perm
 
 from django.test import TestCase
 from django.test.client import Client
@@ -19,12 +22,17 @@ class ImportExportTests(TestCase):
         self.username = "test2"
         self.password = "test"
         self.user = User.objects.create(username=self.username, is_superuser=True)
+        self.user.set_password(self.password)
+        self.user.save()
         self.test_project_id = 3
 
         self.client = Client()
         self.client.login(username=self.username, password=self.password)
 
         super(ImportExportTests, self).setUp();
+
+    def fake_authentication(self):
+        self.client.force_login(self.user)
 
     def test_import_projects(self):
         """Import a set of new projects, stacks and stack groups. This tests
@@ -166,3 +174,119 @@ class ImportExportTests(TestCase):
                         relation=Relation.objects.get(project=p2,
                         relation_name=sg_cfg['relation']))
                 self.assertEqual(sg_cfg['name'], stack_group_link.class_instance.name)
+
+    def test_import_export_projects(self):
+        """Export all projects, stacks and stack groups (without class instance
+        and tracing data). Make then sure, they match the fixture.
+        """
+
+        p1_config = {
+            'project': {
+                'name': 'test-no-stacks',
+                'stacks': tuple(),
+            }
+        }
+
+        p2_config = {
+            'project': {
+                'name': 'test-two-stacks',
+                'stacks': [{
+                    'comment': None,
+                    'name': 'test-stack-1',
+                    'url': 'https://catmaid-test/',
+                    'tile_height': 512,
+                    'dimension': '(7,17,23)',
+                    'zoomlevels': -1,
+                    'tile_width': 256,
+                    'fileextension': 'jpg',
+                    'tile_source_type': 5,
+                    'resolution': '(2,3,5)',
+                    'metadata': ''
+                }, {
+                    'comment': None,
+                    'name': 'test-stack-2',
+                    'url': 'https://this.is.my.stack/',
+                    'tile_height': 512,
+                    'dimension': '(7,17,23)',
+                    'zoomlevels': -1,
+                    'tile_width': 256,
+                    'fileextension': 'jpg',
+                    'tile_source_type': 5,
+                    'resolution': '(2,3,5)',
+                    'metadata': ''
+                }, {
+                    'comment': None,
+                    'name': 'test-stack-3',
+                    'url': 'https://catmaid-test/abc/',
+                    'tile_height': 456,
+                    'resolution': '(1,2,3)',
+                    'dimension': '(4,34,9)',
+                    'zoomlevels': -1,
+                    'tile_width': 123,
+                    'fileextension': 'jpg',
+                    'tile_source_type': 2,
+                    'stackgroups': [{
+                        'relation': 'has_view',
+                        'name': u'Test group 1'
+                    }],
+                    'metadata': 'Test meta data'
+                }]
+            }
+        }
+
+        project_url = 'https://catmaid-test/'
+        data_folder = '/tmp/catmaid-test/'
+        pre_projects = [
+            importer.PreProject(p1_config, project_url, data_folder),
+            importer.PreProject(p2_config, project_url, data_folder),
+        ]
+        config = [p1_config, p2_config]
+
+        tags = []
+        permissions = []
+        default_tile_width = 256
+        default_tile_height = 512
+        default_tile_source_type = 5
+        cls_graph_ids_to_link = []
+        remove_unref_stack_data = False
+
+        # Make sure there are no existing projects or stacks
+        Project.objects.all().delete()
+        Stack.objects.all().delete()
+
+        imported, not_imported = importer.import_projects(self.user,
+            pre_projects, tags, permissions, default_tile_width,
+            default_tile_height, default_tile_source_type,
+            cls_graph_ids_to_link, remove_unref_stack_data)
+
+        self.assertEqual(0, len(not_imported))
+        self.assertEqual(len(config), len(imported))
+
+        # Make sure we can see all projects
+        for p in Project.objects.all():
+            assign_perm('can_browse', self.user, p)
+
+        # Export imported data
+        self.fake_authentication()
+        response = self.client.get('/projects/export')
+        self.assertEqual(response.status_code, 200)
+        result = yaml.load(response.content)
+
+        def strip_ids(d):
+            """ Recursively, strip all 'id' fields of dictionaries.
+            """
+            if type(d) == dict:
+                if 'id' in d:
+                    d.pop('id')
+                for _,v in d.items():
+                    strip_ids(v)
+            if type(d) == list:
+                for v in d:
+                    strip_ids(v)
+
+        # Results come with IDs, which we don't have in our input data. Strip
+        # them to be able to simply compare dictionaries.
+        strip_ids(result)
+
+        for cp, p in zip(config, result):
+            self.assertDictEqual(cp, p)

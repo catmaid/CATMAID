1667673dad5c7713863087e3dcbde11958d81092
Tom Kazimiers <tom@voodoo-arts.net>
2017-02-15 22:36:05 -0500

    Add workflow implementation

--- django/applications/catmaid/static/libs/catmaid/error.js
+++ django/applications/catmaid/static/libs/catmaid/error.js
@@ -88,4 +88,14 @@
   CATMAID.NetworkAccessError.prototype = Object.create(CATMAID.Error.prototype);
   CATMAID.NetworkAccessError.prototype.constructor = CATMAID.NetworkAccessError;
 
+  /*
+   * An error type to indicate a pre-condition for some action is not met.
+   */
+  CATMAID.PreConditionError = function(message, detail) {
+    CATMAID.Error.call(this, message, detail);
+  };
+
+  CATMAID.PreConditionError.prototype = Object.create(CATMAID.Error.prototype);
+  CATMAID.PreConditionError.prototype.constructor = CATMAID.PreConditionError;
+
 })(CATMAID);
---
+++ django/applications/catmaid/static/libs/catmaid/workflow.js
@@ -0,0 +1,95 @@
+/* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
+
+(function(CATMAID) {
+
+  "use strict";
+
+  /**
+   * Encapsulates a step-by-step process.
+   */
+  var Workflow = function(options) {
+    options = options || {};
+    this.state = options['state'] || {};
+    this.currentStepIndex = 0;
+    this.steps = options['steps'];
+    if (!this.steps) {
+      throw new CATMAID.ValueError("Need at least one step for workflow");
+    }
+
+    if (options['step']) {
+      this.selectStep(options['step']);
+    }
+  };
+
+  Workflow.prototype.advance = function() {
+    this.selectStep(this.currentStepIndex + 1);
+  };
+
+  Workflow.prototype.goBack = function() {
+    this.selectStep(this.currentStepIndex - 1);
+  };
+
+  Workflow.prototype.getCurrentStep = function() {
+    return this.steps[this.currentStepIndex];
+  };
+
+  Workflow.prototype.selectStep = function(stepIndex) {
+    if (stepIndex < 0 || stepIndex >= this.steps.length) {
+      throw new CATMAID.ValueError("No valid step index: " + stepIndex);
+    }
+
+    // Make sure all previous steps are complete
+    var allPreviousComplete = true;
+    for (var i=0; i<stepIndex; ++i) {
+        allPreviousComplete = allPreviousComplete &&
+            this.steps[i].isComplete(this.state);
+        if (!allPreviousComplete) {
+          break;
+        }
+    }
+
+    var success = false;
+    if (allPreviousComplete) {
+      var newStep = this.steps[stepIndex];
+      try {
+        newStep.activate(this.state);
+        this.currentStepIndex = stepIndex;
+        success = true;
+      } catch (error) {
+        if (stepIndex !== this.currentStepIndex) {
+          newStep.activate(this.state);
+        }
+        if (error instanceof CATMAID.PreConditionError) {
+          CATMAID.warn("Step " + (stepIndex + 1) + " can not be activated yet");
+        } else {
+          CATMAID.handleError(error);
+        }
+      }
+    } else {
+      CATMAID.warn("Previous step(s) not complete");
+    }
+
+    return success;
+  };
+
+
+  /**
+   * A workflow step that can be used by a workflow.
+   */
+  var WorkflowStep = function(title) {
+    this.title = title;
+  };
+
+  WorkflowStep.activate = function(state) {};
+
+  WorkflowStep.isComplete = function(state) {
+    return true;
+  };
+
+
+  // Export
+  CATMAID.Workflow = Workflow;
+  CATMAID.WorkflowStep = WorkflowStep;
+
+})(CATMAID);

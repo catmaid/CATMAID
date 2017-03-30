catmaid/CATMAID

commit d6218a11153b30a57fe109ed6a5fe455b0fd64c7

Connectors: make pre-post-info endpoint more flexible
This renames the {project_id}/connectors/pre-post-info endpoint to
{project_id}/connectors/info. It is now more flexible and does not
require a pre and post parameter anymore, but still supports them. If
ommitted, all connectors are returned.

django/applications/catmaid/control/connector.py
@@ -628,15 +628,15 @@ def _list_completed(project_id, completed_by=None, from_date=None, to_date=None)
 @requires_user_role(UserRole.Browse)
 def connectors_info(request, project_id):
     """
-    Given a list of connectors, a list of presynaptic skeletons and a list of postsynatic skeletons,
-    return a list of rows, one per synaptic connection, in the same format as one_to_many_synapses.
-    The list of connectors is optional.
+    Given a list of connectors, a list of presynaptic skeletons and a list of
+    postsynatic skeletons, return a list of rows, one per synaptic connection,
+    in the same format as one_to_many_synapses. The list of connectors (cids),
+    pre-skeletons (pre) and post-skeletons (post) is optional.
     """
 
-    int_to_str = lambda x: str(int(x))
-    cids = get_request_list(request.POST, 'cids', map_fn=int_to_str)
-    skids_pre = get_request_list(request.POST, 'pre', map_fn=int_to_str)
-    skids_post = get_request_list(request.POST, 'post', map_fn=int_to_str)
+    cids = get_request_list(request.POST, 'cids', map_fn=int)
+    skids_pre = get_request_list(request.POST, 'pre', map_fn=int)
+    skids_post = get_request_list(request.POST, 'post', map_fn=int)
 
     cursor = connection.cursor()
 
@@ -644,34 +644,75 @@ def connectors_info(request, project_id):
     pre = relations['presynaptic_to']
     post = relations['postsynaptic_to']
 
-    cursor.execute('''
-    SELECT DISTINCT
-           tc1.connector_id, c.location_x, c.location_y, c.location_z,
-           tc1.treenode_id, tc1.skeleton_id, tc1.confidence, tc1.user_id,
-           t1.location_x, t1.location_y, t1.location_z,
-           tc2.treenode_id, tc2.skeleton_id, tc2.confidence, tc2.user_id,
-           t2.location_x, t2.location_y, t2.location_z
-    FROM treenode_connector tc1,
-         treenode_connector tc2,
-         treenode t1,
-         treenode t2,
-         connector c
-    WHERE %s
-          tc1.connector_id = c.id
-      AND tc1.connector_id = tc2.connector_id
-      AND tc1.skeleton_id IN (%s)
-      AND tc2.skeleton_id IN (%s)
-      AND tc1.relation_id = %s
-      AND tc2.relation_id = %s
-      AND tc1.id != tc2.id
-      AND tc1.treenode_id = t1.id
-      AND tc2.treenode_id = t2.id
-    ORDER BY tc2.skeleton_id
-    ''' % ("c.id IN (%s) AND" % ",".join(cids) if cids else "",
-           ",".join(skids_pre),
-           ",".join(skids_post),
-           pre,
-           post))
+    # Construct base query
+    query_parts = ['''
+        SELECT DISTINCT
+               tc1.connector_id, c.location_x, c.location_y, c.location_z,
+               tc1.treenode_id, tc1.skeleton_id, tc1.confidence, tc1.user_id,
+               t1.location_x, t1.location_y, t1.location_z,
+               tc2.treenode_id, tc2.skeleton_id, tc2.confidence, tc2.user_id,
+               t2.location_x, t2.location_y, t2.location_z
+        FROM connector c
+    ''']
+
+    query_params = []
+
+    # Add connector filter, if requested
+    if cids:
+        cid_template = ",".join(("(%s)",) * len(cids))
+        query_parts.append('''
+            JOIN (VALUES {}) rc(id) ON c.id = rc.id
+        '''.format(cid_template))
+        query_params.extend(cids)
+
+    # Add pre-synaptic skeleton filter, if requested
+    query_parts.append('''
+        JOIN treenode_connector tc1 ON tc1.connector_id = c.id
+        JOIN treenode t1 ON tc1.treenode_id = t1.id
+    ''')
+    if skids_pre:
+        pre_skid_template = ",".join(("(%s)",) * len(skids_pre))
+        query_parts.append('''
+            JOIN (VALUES {}) sk_pre(id) ON tc1.skeleton_id = sk_pre.id
+        '''.format(pre_skid_template))
+        query_params.extend(skids_pre)
+
+    # Add post-synaptic skeleton filter, if requested
+    query_parts.append('''
+        JOIN treenode_connector tc2 ON tc2.connector_id = c.id
+        JOIN treenode t2 ON tc2.treenode_id = t2.id
+    ''')
+    if skids_post:
+        post_skid_template = ",".join(("(%s)",) * len(skids_post))
+        query_parts.append('''
+            JOIN (VALUES {}) sk_post(id) ON tc2.skeleton_id = sk_post.id
+        '''.format(post_skid_template))
+        query_params.extend(skids_post)
+
+    # Prevent self-joins of connector partners
+    query_parts.append('''
+        WHERE tc1.id != tc2.id
+    ''')
+
+    # Pre-synaptic skeleton filters also constrain the relation
+    if skids_pre:
+        query_parts.append('''
+            AND tc1.relation_id = %s
+        ''')
+        query_params.append(pre)
+
+    # Post-synaptic skeleton filters also constrain the relation
+    if skids_post:
+        query_parts.append('''
+            AND tc2.relation_id = %s
+        ''')
+        query_params.append(post)
+
+    query_parts.append('''
+        ORDER BY tc2.skeleton_id
+    ''')
+
+    cursor.execute("\n".join(query_parts), query_params)
 
     rows = tuple((row[0], (row[1], row[2], row[3]),
                   row[4], row[5], row[6], row[7],

django/applications/catmaid/static/js/widgets/compartment_graph_widget.js
@@ -634,7 +634,7 @@
             post: target_skids
           };
 
-          CATMAID.fetch(project.id + '/connector/pre-post-info', "POST", params)
+          CATMAID.fetch(project.id + '/connector/info', "POST", params)
             .then(function(result) {
               CATMAID.ConnectorSelection.show_connectors(result);
             })

django/applications/catmaid/tests/test_common_apis.py
@@ -1189,7 +1189,7 @@ class ViewPageTests(TestCase):
     def test_connector_info(self):
         self.fake_authentication()
         response = self.client.post(
-                '/%d/connector/pre-post-info' % self.test_project_id,
+                '/%d/connector/info' % self.test_project_id,
                 {'pre[0]': 235, 'post[0]': 373})
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content)
@@ -1203,7 +1203,7 @@ class ViewPageTests(TestCase):
         self.assertEqual(expected_result, parsed_response)
 
         response = self.client.post(
-                '/%d/connector/pre-post-info' % self.test_project_id,
+                '/%d/connector/info' % self.test_project_id,
                 {'pre[0]': 235, 'post[0]': 373, 'cids[0]': 421})
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content)
@@ -1214,7 +1214,7 @@ class ViewPageTests(TestCase):
         self.assertEqual(expected_result, parsed_response)
 
         response = self.client.post(
-                '/%d/connector/pre-post-info' % self.test_project_id,
+                '/%d/connector/info' % self.test_project_id,
                 {'pre[0]': 2462, 'post[0]': 2468})
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content)
@@ -1226,7 +1226,7 @@ class ViewPageTests(TestCase):
         self.assertEqual(expected_result, parsed_response)
 
         response = self.client.post(
-                '/%d/connector/pre-post-info' % self.test_project_id,
+                '/%d/connector/info' % self.test_project_id,
                 {'pre[0]': 2462, 'post[0]': 2462})
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content)

django/applications/catmaid/urls.py
@@ -162,7 +162,7 @@ urlpatterns += [
     url(r'^(?P<project_id>\d+)/connector/list/completed$', connector.list_completed),
     url(r'^(?P<project_id>\d+)/connector/skeletons$', connector.connector_skeletons),
     url(r'^(?P<project_id>\d+)/connector/edgetimes$', connector.connector_associated_edgetimes),
-    url(r'^(?P<project_id>\d+)/connector/pre-post-info$', connector.connectors_info),
+    url(r'^(?P<project_id>\d+)/connector/info$', connector.connectors_info),
     url(r'^(?P<project_id>\d+)/connector/user-info$', connector.connector_user_info),
     url(r'^(?P<project_id>\d+)/connectors/(?P<connector_id>\d+)/$',
         connector.connector_detail),

commit 643ab75ec9bcb08f8e68961fdc5c1c9ece7ed1c2

Connector back-end: fix wrong results in /info endpoint
The connector/info endpoint updated in d6218a11153 accidentally broke
this API if not both 'pre' and 'post' parameters were provided. If for
instance only a post-skeleton was passed in, it is expected that only
those links are returned that have the respective post-skeleton as end
and any pre-skeleton as beginning. This was broken, which caused all
links to or from the result connector where returned with the passed
post-skeleton as end. This is fixed now and only the expected links
retrurned. A regression test has been added.

Fixes catmaid/CATMAID#1554

django/applications/catmaid/control/connector.py
@@ -561,11 +561,9 @@ def connectors_info(request, project_id):
             raise ValueError("The skids parameter can't be used together with "
                     "pre and/or post.")
 
-        relations = get_relation_to_id_map(project_id, ('presynaptic_to', 'postsynaptic_to'), cursor)
-        pre = relations['presynaptic_to']
-        post = relations['postsynaptic_to']
-    else:
-        pre = post = None
+    relations = get_relation_to_id_map(project_id, ('presynaptic_to', 'postsynaptic_to'), cursor)
+    pre = relations['presynaptic_to']
+    post = relations['postsynaptic_to']
 
     # Construct base query
     query_parts = ['''
@@ -588,11 +586,13 @@ def connectors_info(request, project_id):
         '''.format(cid_template))
         query_params.extend(cids)
 
-    # Add pre-synaptic skeleton filter, if requested
+    # Get first partner of connection
     query_parts.append('''
         JOIN treenode_connector tc1 ON tc1.connector_id = c.id
         JOIN treenode t1 ON tc1.treenode_id = t1.id
     ''')
+
+    # Add pre-synaptic skeleton filter, if requested
     if skids_pre:
         pre_skid_template = ",".join(("(%s)",) * len(skids_pre))
         query_parts.append('''
@@ -600,11 +600,13 @@ def connectors_info(request, project_id):
         '''.format(pre_skid_template))
         query_params.extend(skids_pre)
 
-    # Add post-synaptic skeleton filter, if requested
+    # Get second partner of connection
     query_parts.append('''
         JOIN treenode_connector tc2 ON tc2.connector_id = c.id
         JOIN treenode t2 ON tc2.treenode_id = t2.id
     ''')
+
+    # Add post-synaptic skeleton filter, if requested
     if skids_post:
         post_skid_template = ",".join(("(%s)",) * len(skids_post))
         query_parts.append('''
@@ -625,19 +627,14 @@ def connectors_info(request, project_id):
         WHERE tc1.id != tc2.id
     ''')
 
-    # Pre-synaptic skeleton filters also constrain the relation
-    if skids_pre:
-        query_parts.append('''
-            AND tc1.relation_id = %s
-        ''')
-        query_params.append(pre)
-
-    # Post-synaptic skeleton filters also constrain the relation
-    if skids_post:
-        query_parts.append('''
-            AND tc2.relation_id = %s
-        ''')
-        query_params.append(post)
+    # The result is expected to be stictly pre-synaptic and post-synaptic at the
+    # moment.
+    query_parts.append('''
+        AND tc1.relation_id = %s
+        AND tc2.relation_id = %s
+    ''')
+    query_params.append(pre)
+    query_params.append(post)
 
     if skids:
         query_parts.append('''

django/applications/catmaid/tests/apis/test_connectors.py
@@ -305,6 +305,16 @@ class ConnectorsApiTests(CatmaidApiTestCase):
 
         self.assertEqual(expected_result, parsed_response)
 
+        response = self.client.post(
+                '/%d/connector/info' % self.test_project_id,
+                {'post[0]': 361})
+        self.assertEqual(response.status_code, 200)
+        parsed_response = json.loads(response.content.decode('utf-8'))
+        expected_result = [
+                [356, [6730.0, 2700.0, 0.0],
+                 285, 235, 5, 3, [6100.0, 2980.0, 0.0],
+                 367, 361, 5, 3, [7030.0, 1980.0, 0.0]]]
+        self.assertEqual(expected_result, parsed_response)
 
     def test_connector_detail(self):
         self.fake_authentication()
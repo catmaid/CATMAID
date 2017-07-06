37ebb9cde96a8ed5ecb4d14bdf528c8403bf597d
Tom Kazimiers <tom@voodoo-arts.net>
2017-07-06 14:10:49 -0400

    Add missing celery configuration module

---
+++ django/projects/mysite/celery.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+app = Celery('mysite')
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('mysite.settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+if __name__ == '__main__':
+    app.start()

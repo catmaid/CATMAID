1aa1f86b6ee27d0fdd73659279b0bc8a24748e37
Tom Kazimiers <tom@voodoo-arts.net>
2017-07-20 13:46:58 -0400

    Remove unneeded global definition
    The login() function is meanwhile in a namespace and not used in this
    file.

--- django/applications/catmaid/static/js/CATMAID.js
+++ django/applications/catmaid/static/js/CATMAID.js
@@ -1,8 +1,5 @@
 /* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
 /* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
-/* global
-  login
-*/
 
 // Attach a general error handler
 window.onerror = function(msg, url, lineno, colno, err)

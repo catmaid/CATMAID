0b81668fe934988873e16b626cb3d8185589b62b
Tom Kazimiers <tom@voodoo-arts.net>
2017-09-15 17:54:04 -0400

    Python 3: fix git based version retrieval
    In Python 3, the data returned by subprocess.communicate() is no string
    unless the text mode is used. Text mode can be enforced by various
    means, among them accepting universal newlines. This is done here,
    because it works for both Python 2 and 3.
    
    A test for this has already been added in last commit.

--- django/projects/mysite/utils.py
+++ django/projects/mysite/utils.py
@@ -14,8 +14,10 @@ def get_version():
     """
     try:
         dir = os.path.dirname(os.path.realpath(__file__))
+        # Universal newlines is used to get both Python 2 and 3 to use text mode.
         p = subprocess.Popen("/usr/bin/git describe", cwd=os.path.dirname(dir),
-                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                universal_newlines=True)
         (out, error) = p.communicate()
         return "unknown" if error else out.rstrip().encode('utf-8')
     except:

c5173bf5cbd28b8c32bde91667c43745effd2106
Tom Kazimiers <tom@voodoo-arts.net>
2017-11-16 16:06:26 -0500

    3D viewer: fix first line indentation (minor)

--- django/applications/catmaid/static/js/widgets/3dviewer.js
+++ django/applications/catmaid/static/js/widgets/3dviewer.js
@@ -1,4 +1,4 @@
-      /* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
+/* -*- mode: espresso; espresso-indent-level: 2; indent-tabs-mode: nil -*- */
 /* vim: set softtabstop=2 shiftwidth=2 tabstop=2 expandtab: */
 /* global
   CATMAID,

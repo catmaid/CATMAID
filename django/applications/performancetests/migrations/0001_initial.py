b0d983b25641d8fe21fc68e48d9f450a150f069a
Tom Kazimiers <tom@voodoo-arts.net>
2018-02-14 10:27:55 -0500

    Mark cascading deletions in initial migration explicitely
    This is the current default behavior and what we set in our models
    module. This change is required from Django 2 on and makes the logs
    easier to read, becasue the respective warnigns won't be shown anymore.

--- django/applications/performancetests/migrations/0001_initial.py
+++ django/applications/performancetests/migrations/0001_initial.py
@@ -53,7 +53,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='testresult',
             name='view',
-            field=models.ForeignKey(to='performancetests.TestView'),
+            field=models.ForeignKey(to='performancetests.TestView', on_delete=models.CASCADE),
             preserve_default=True,
         ),
     ]

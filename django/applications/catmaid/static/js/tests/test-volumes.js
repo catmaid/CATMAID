2de624e2b840d6d5e9e1963b2f2aa472b6c0efbb
Tom Kazimiers <tom@voodoo-arts.net>
2018-04-17 17:41:39 -0400

    Tests: update volume intersection tests to use buffer geometry

--- django/applications/catmaid/static/js/tests/test-volumes.js
+++ django/applications/catmaid/static/js/tests/test-volumes.js
@@ -5,7 +5,7 @@ QUnit.test('Volume test', function( assert ) {
 
   // Test volume intersector on box
   (function() {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
     var mesh = new THREE.Mesh(geometry);
     var intersector = CATMAID.Volumes.makeIntersector(mesh);
     var point = new THREE.Vector3();
@@ -22,7 +22,7 @@ QUnit.test('Volume test', function( assert ) {
 
   // Test volume intersector on sphere
   (function() {
-    var geometry = new THREE.SphereGeometry(0.5, 16, 16);
+    var geometry = new THREE.SphereBufferGeometry(0.5, 16, 16);
     var mesh = new THREE.Mesh(geometry);
     var intersector = CATMAID.Volumes.makeIntersector(mesh);
     var point = new THREE.Vector3();
@@ -39,7 +39,7 @@ QUnit.test('Volume test', function( assert ) {
 
   // Test volume intersector on torus
   (function() {
-    var geometry = new THREE.TorusGeometry(100, 40, 32, 32);
+    var geometry = new THREE.TorusBufferGeometry(100, 40, 32, 32);
     var mesh = new THREE.Mesh(geometry);
     var intersector = CATMAID.Volumes.makeIntersector(mesh);
     var point = new THREE.Vector3();

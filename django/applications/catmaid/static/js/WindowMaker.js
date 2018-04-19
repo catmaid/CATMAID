96d504cbc39518bf1fc74240096ed51eb38a33d6
Albert Cardona <sapristi@gmail.com>
2018-04-19 11:09:06 -0400

    GroupGraph: completed functionality for layout as columns by selections.
    The "As columns" lays out the nodes of a selection as a column, sorted
    by natural alphanumeric order. So e.g. a node named "LH-5" will be listed
    before a node named "LH-12".
    The "Fade edges" will set the opacity (as in the text field) to edges
    not related to the flow of edges that passes through selected nodes.
    The "Restore edges" will restore the opacity of all edges.

--- django/applications/catmaid/static/js/WindowMaker.js
+++ django/applications/catmaid/static/js/WindowMaker.js
@@ -1631,6 +1631,16 @@ var WindowMaker = new function()
          ['Remove', GG.removeSelection.bind(GG)],
          ['Select all', GG.activateAllSelections.bind(GG)],
          [document.createTextNode(' - ')],
+         ['As columns', GG.alignSelectionsAsColumns.bind(GG)],
+         {type: 'checkbox',
+          label: 'hide other nodes',
+          title: 'Hide nodes not part of any selection',
+          value: true,
+          onclick: null, // TODO consider adding a function to show/hide nodes not in selections
+          id: "gg_hide_nodes_not_in_selections" + GG.widgetID},
+         [DOM.createNumericField('gg_columns_edge_opacity' + GG.widgetID, 'edge opacity:', null, '30', '%', GG.showRelevantEdgesToColumns.bind(GG), 2, GG.showRelevantEdgesToColumns.bind(GG))],
+         ['Fade edges', GG.showRelevantEdgesToColumns.bind(GG)],
+         ['Restore edges', GG.updateEdgeGraphics.bind(GG, true)],
         ]);
         
 

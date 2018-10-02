catmaid/CATMAID

commit 33c568874cd5f5871e6eb351d503448c24e6c20c

Improve performance of /skeletons/ endpoint
The skeleton summary table should be a reliable source for all skeleton
IDs in a project.

django/applications/catmaid/control/skeleton.py
@@ -2329,9 +2329,9 @@ def _list_skeletons(project_id, created_by=None, reviewed_by=None, from_date=Non
             query += " AND r.review_time < %(to_date)s"
     else:
         query = '''
-            SELECT DISTINCT skeleton_id
-            FROM treenode t
-            WHERE t.project_id=%(project_id)s
+            SELECT skeleton_id
+            FROM catmaid_skeleton_summary css
+            WHERE css.project_id=%(project_id)s
         '''
 
     if created_by:

commit 919fc6f1f7be357db32d2c95cb3051f3fa3ef455

Skeletons: fix listing endpoint
Fix bug introduced in 33c568874cd5f5871e6eb351d503448c24e6c20c listing
endpoint when querying by creator. Caught by CI tests.

django/applications/catmaid/control/skeleton.py
@@ -2335,8 +2335,13 @@ def _list_skeletons(project_id, created_by=None, reviewed_by=None, from_date=Non
         '''
 
     if created_by:
+        query = '''
+            SELECT DISTINCT skeleton_id
+            FROM treenode t
+            WHERE t.project_id=%(project_id)s
+              AND t.user_id=%(created_by)s
+        '''
         params['created_by'] = created_by
-        query += " AND t.user_id=%(created_by)s"
 
         if from_date:
             params['from_date'] = from_date.isoformat()
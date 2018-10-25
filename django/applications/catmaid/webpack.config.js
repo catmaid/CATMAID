aed61beec0430996495f9d3351d7ebf6589860ff
Andrew Champion <andrew.champion@gmail.com>
2018-10-25 15:24:05 -0400

    WIP

---
+++ django/applications/catmaid/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+  entry: './static/js/index',
+  devtool: 'eval-source-map',
+  output: {
+      path: path.resolve('./static/js/webpack_bundles/'),
+      filename: "[name]-[hash].js"
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
+};

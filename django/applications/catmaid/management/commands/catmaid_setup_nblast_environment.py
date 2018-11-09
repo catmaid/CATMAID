41b78058de2bceb4148958b49d02404381bbcba9
Tom Kazimiers <tom@voodoo-arts.net>
2018-11-09 01:40:58 -0500

    NBLAST setup management command: fix wrong setting variable reference

--- django/applications/catmaid/management/commands/catmaid_setup_nblast_environment.py
+++ django/applications/catmaid/management/commands/catmaid_setup_nblast_environment.py
@@ -10,16 +10,16 @@ class Command(BaseCommand):
     help = 'Set up the required R dependencies for NBLAST support'
 
     def handle(self, *args, **options):
-        if hasattr(settings, 'R_USER_LIBS'):
-            if not os.path.exists(settings.R_USER_LIBS):
-                raise CommandError('The path defined by R_USER_LIBS in '
-                        'settings.py ({}) does not exist.'.format(settings.R_USER_LIBS))
-            if not os.access(settings.R_USER_LIBS, os.W_OK):
-                raise CommandError('The path defined by R_USER_LIBS in '
-                        'settings.py ({}) is not writable.'.format(settings.R_USER_LIBS))
+        if hasattr(settings, 'R_LIBS_USER'):
+            if not os.path.exists(settings.R_LIBS_USER):
+                raise CommandError('The path defined by R_LIBS_USER in '
+                        'settings.py ({}) does not exist.'.format(settings.R_LIBS_USER))
+            if not os.access(settings.R_LIBS_USER, os.W_OK):
+                raise CommandError('The path defined by R_LIBS_USER in '
+                        'settings.py ({}) is not writable.'.format(settings.R_LIBS_USER))
             install_dependencies()
         else:
-            raise CommandError('Please define the R_USER_LIBS setting in '
+            raise CommandError('Please define the R_LIBS_USER setting in '
                     'settings.py and set it to a path writable and readable '
                     'by the user running CATMAID and the user running this command.')
 

c8be170d31bced0f3f8b8b4ff8f4aa024a5897d5
Andrew Champion <andrew.champion@gmail.com>
2018-11-09 13:09:21 -0500

    Stack groups: remove unused variable

--- django/applications/catmaid/control/stackgroup.py
+++ django/applications/catmaid/control/stackgroup.py
@@ -17,7 +17,6 @@ def get_stackgroup_info(request, project_id, stackgroup_id):
         .filter(stack_group=stackgroup_id) \
         .order_by('position') \
         .select_related('group_relation')
-    stacks = [l.stack_id for l in stackgroup_links]
 
     result = {
         'id': stackgroup.id,

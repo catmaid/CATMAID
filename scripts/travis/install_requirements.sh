fd01a31c3b46fa455e8118c3967bf939ad289463
Tom Kazimiers <tom@voodoo-arts.net>
2018-11-14 14:41:45 -0500

    CI: use Ubuntu 16.04 instead of 14.04
    This is needed to be able to use Python 3.7.
    
    See catmaid/CATMAID#1766
    See catmaid/CATMAID#1770

--- scripts/travis/install_requirements.sh
+++ scripts/travis/install_requirements.sh
@@ -4,7 +4,7 @@ set -ev
 
 source `dirname ${BASH_SOURCE[0]}`/travis_functions.sh
 
-travis_retry sudo apt-get install -y -qq $(< packagelist-ubuntu-14.04-apt.txt)
+travis_retry sudo apt-get install -y -qq $(< packagelist-ubuntu-16.04-apt.txt)
 travis_retry python -m pip install -U pip
 travis_retry travis_wait 60 pip install -q -r django/requirements.txt
 pip list

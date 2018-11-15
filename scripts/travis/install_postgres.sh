c551e0c1078a289fcb04afbcea925d565924b84e
Tom Kazimiers <tom@voodoo-arts.net>
2018-11-15 13:03:47 -0500

    CI: setup basic Postgres 10 through Travis
    This is needed in order to create the actual data directory of Postgres.
    Using the PG deb packages don't seem to do this otherwise.
    
    Also print all active clusters for better future debugging. Postgres is
    now also started through systemd, because this is the default on Ubuntu
    16.04.

--- scripts/travis/install_postgres.sh
+++ scripts/travis/install_postgres.sh
@@ -3,11 +3,14 @@
 set -ex
 
 echo "Installing Postgres 10"
-sudo service postgresql stop
+sudo systemctl stop postgresql
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-10 postgresql-client-10
-sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
+#sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
-sudo service postgresql restart
+sudo systemctl start postgresql@10-main
+
+echo "The following Postgres clusters are installed:"
+pg_lsclusters

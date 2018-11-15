5d1bbe75affa9cb10c59706753ffda604687a609
Tom Kazimiers <tom@voodoo-arts.net>
2018-11-15 14:20:47 -0500

    CI: explicitly install PostGIS along with Postgres

--- scripts/travis/install_postgres.sh
+++ scripts/travis/install_postgres.sh
@@ -6,7 +6,10 @@ echo "Installing Postgres 10"
 sudo systemctl stop postgresql
 sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
-sudo apt-get install -q postgresql-10 postgresql-client-10
+sudo apt-get install -q postgresql-10 postgresql-client-10 postgresql-10-postgis-2.4 postgresql-10-postgis-2.4-scripts
+
+# In case future Postgres version are not available on Travis and have to
+# installed manually, copying the existing hba file can save some time.
 #sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"

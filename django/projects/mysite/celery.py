d5f3e6bfadbe8b4a3757e4702c2ccfe89bc22dba
Tom Kazimiers <tom@voodoo-arts.net>
2019-05-08 22:24:56 -0400

    Celery: propagate Django logging settings to Celery worker
    This allows to control the log output of Celery workers through the
    settings.py configuration file.

--- django/projects/mysite/celery.py
+++ django/projects/mysite/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.signals import setup_logging
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -18,6 +20,17 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwags):
+    """This will set the logging environment for workers so that we can get
+    their log output where we expect it.
+    """
+    from logging.config import dictConfig
+    from django.conf import settings
+    dictConfig(settings.LOGGING)
+
 
 if __name__ == '__main__':
     app.start()

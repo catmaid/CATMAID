f9d8cfa4cef606cefdff90dc1586774dc27e7313
Tom Kazimiers <tom@voodoo-arts.net>
2019-07-30 20:35:56 -0400

    Pair statistics widget: add matching pair tab

--- django/applications/catmaid/static/js/widgets/pair-statistics-widget.js
+++ django/applications/catmaid/static/js/widgets/pair-statistics-widget.js
@@ -15,7 +15,7 @@
 
     // The current edit mode
     this.mode = 'skeleton-pairs';
-    this.modes = ['skeleton-pairs', 'pair-statistics'];
+    this.modes = ['skeleton-pairs', 'matching-pairs', 'pair-statistics'];
 
     this.neuronNameService = CATMAID.NeuronNameService.getInstance();
 
@@ -26,7 +26,27 @@
     this.annotationGroupingPattern = '/(.*)_([lr])';
     this.minGroupInstances = 2;
     this.maxGroupInstances = 2;
+
+    // Annotation groups
+    this.groups = new Map();
     this.extraGroups = new Map();
+
+    // Matching parameters
+    this.newMatchName = '';
+
+    // Group completeness parameters
+    this.mainMaxOpenEnds = 0.03;
+    this.mainMinNodes = 500;
+    this.mainMinCable = 0;
+    this.mainIgnoreFragments = true;
+
+    // Extra group completeness parameters
+    this.extraMaxOpenEnds = 0.05;
+    this.extraMinNodes = 300;
+    this.extraMinCable = 0;
+    this.extraIgnoreFragments = false;
+
+    this.groupSources = [];
 
     // Some parts of the widget need to update when skeleton sources are added
     // or removed.
@@ -52,6 +72,14 @@
         this.handleUpdatedSkeletonSources, this);
     CATMAID.skeletonListSources.off(CATMAID.SkeletonSourceManager.EVENT_SOURCE_REMOVED,
         this.handleUpdatedSkeletonSources, this);
+    this.clearGroupSources();
+  };
+
+  PairStatisticsWidget.prototype.clearGroupSources = function() {
+    for (let i=0; i<this.groupSources; ++i) {
+      this.groupSources[i].destroy();
+    }
+    this.groupSources.length = [];
   };
 
   PairStatisticsWidget.prototype.getWidgetConfiguration = function() {
@@ -147,6 +175,28 @@
     this.mode = mode;
     this.update();
     return true;
+  };
+
+  /**
+   * Take all neurons that are grouped by the annotation configuration in the
+   * first tab (active neurons), remove incomple ones and match pairs between
+   * the groups.
+   */
+  PairStatisticsWidget.prototype.matchActiveNeurons = function() {
+    
+  };
+
+  PairStatisticsWidget.extractSubGroupSets = function(source, target) {
+    return Array.from(source.keys()).reduce((t,g) => {
+              let subgroups = source.get(g);
+              for (let [sg, aid] of subgroups.entries()) {
+                if (!t.has(sg)) {
+                  t.set(sg, new Set());
+                }
+                t.get(sg).add(aid);
+              }
+              return t;
+            }, target);
   };
 
   PairStatisticsWidget.MODES = {
@@ -325,6 +375,9 @@
                 widget.annotationGroupingPattern, widget.minGroupInstances,
                 widget.maxGroupInstances, CATMAID.annotations)
               .then(groups => {
+                // Store latest copy in widget
+                widget.groups = groups;
+                // Update table
                 callback({
                   'draw': data.draw,
                   'data': Array.from(groups.keys()).reduce((o,k) => {
@@ -407,6 +460,278 @@
           widget.extraGroups.delete(data[0]);
           widget.update();
         });
+      }
+    },
+    'matching-pairs': {
+      title: 'Matching pairs',
+      createControls: function(target) {
+        let controls = [];
+
+        controls.push({
+          type: 'text',
+          label: 'Match name',
+          value: target.newGroupName,
+          placeholder: '(optional)',
+          onchange: e => {
+            target.newGroupName = e.target.value;
+          },
+        });
+
+        let mainCompletenessSection = document.createElement('span');
+        mainCompletenessSection.classList.add('section-header');
+        mainCompletenessSection.appendChild(document.createTextNode('Main completeness'));
+        mainCompletenessSection.title = 'Completeness properties for all neurons ' +
+            'annotated with annotations from the primary annotation grouping in the first tab.';
+        controls.push({
+          type: 'child',
+          element: mainCompletenessSection,
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Max open ends',
+          title: 'The percentage of of open ends per neurons to be considered complete.',
+          value: target.mainMaxOpenEnds,
+          length: 4,
+          min: 0,
+          max: 1,
+          step: 0.01,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.mainMaxOpenEnds = value;
+          }
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Min nodes',
+          title: 'The minimum number of nodes for a neuron to be considered complete.',
+          value: target.mainMinNodes,
+          length: 4,
+          min: 0,
+          step: 50,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.mainMinNodes = value;
+          }
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Min cable',
+          title: 'The minimum cable length in nm for a neuron to be considered complete.',
+          value: target.mainMinCable,
+          length: 5,
+          min: 0,
+          step: 500,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.mainMinCable = value;
+          }
+        });
+
+        controls.push({
+          type: 'checkbox',
+          label: 'Ignore fragments',
+          title: 'Ignore all neurons that don\'t have a node tagged "soma" or a node tagged "out to nerve"',
+          value: target.mainIgnoreFragments,
+          onchange: e => {
+            target.mainIgnoreFragments = e.target.checked;
+          }
+        });
+
+        let extraCompletenessSection = document.createElement('span');
+        extraCompletenessSection.classList.add('section-header');
+        extraCompletenessSection.appendChild(document.createTextNode('Extra completeness'));
+        mainCompletenessSection.title = 'Completeness properties for all neurons ' +
+            'annotated with annotations from the extra annotation grouping in the first tab.';
+        controls.push({
+          type: 'child',
+          element: extraCompletenessSection,
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Max open ends',
+          title: 'The percentage of of open ends per neurons to be considered complete.',
+          value: target.extraMaxOpenEnds,
+          length: 4,
+          min: 0,
+          max: 1,
+          step: 0.01,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.extraMaxOpenEnds = value;
+          }
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Min nodes',
+          title: 'The minimum number of nodes for a neuron to be considered complete.',
+          value: target.extraMinNodes,
+          length: 4,
+          min: 0,
+          step: 50,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.extraMinNodes = value;
+          }
+        });
+
+        controls.push({
+          type: 'numeric',
+          label: 'Min cable',
+          title: 'The minimum cable length in nm for a neuron to be considered complete.',
+          value: target.extraMinCable,
+          length: 5,
+          min: 0,
+          step: 500,
+          onchange: e => {
+            let value = Number(e.target.value);
+            if (Number.isNaN(value)) return;
+            target.extraMinCable = value;
+          }
+        });
+
+        controls.push({
+          type: 'checkbox',
+          label: 'Ignore fragments',
+          title: 'Ignore all neurons that don\'t have a node tagged "soma" or a node tagged "out to nerve"',
+          value: target.extraIgnoreFragments,
+          onchange: e => {
+            target.extraIgnoreFragments = e.target.checked;
+          }
+        });
+
+        // Filter complete
+        controls.push({
+          type: 'button',
+          label: 'Match complete pairs',
+          title: 'Find all matching skeleton pairs between active annotation groups',
+          onclick: e => {
+            target.matchActiveNeurons();
+            target.update();
+          },
+        });
+
+        return controls;
+      },
+      createContent: function(content, widget) {
+        let currentGroupsHeader = content.appendChild(document.createElement('h1'));
+        currentGroupsHeader.style.clear = 'both';
+        currentGroupsHeader.appendChild(document.createTextNode('Active groups'));
+
+        // Map subgroup identifier to sets of annotations
+        let subGroupMap = new Map();
+        PairStatisticsWidget.extractSubGroupSets(widget.groups, subGroupMap);
+        PairStatisticsWidget.extractSubGroupSets(widget.extraGroups, subGroupMap);
+
+        let annotationIdSet = new Set();
+        for (let [sg, annotations] of subGroupMap.entries()) {
+          annotationIdSet = annotationIdSet.union(annotations);
+        }
+        let annotationIds = Array.from(annotationIdSet);
+
+        let subGroupList = content.appendChild(document.createElement('p'));
+        if (subGroupMap.size === 0) {
+          subGroupList.appendChild(document.createTextNode('Could not find any sub-groups'));
+        } else {
+          subGroupList.style.display = 'flex';
+          subGroupList.style.flexWrap = 'wrap';
+        }
+
+        let prepare = [
+          // Get skeletons for all annotations. Combining multiple annotations
+          // in one entry, results in an OR query
+          CATMAID.fetch(project.id + '/annotations/query-targets', 'POST', {
+            'annotated_with': [annotationIds.join(',')],
+            'annotation_reference': 'id',
+            'type': ['neuron'],
+            'with_annotations': true,
+          }),
+        ];
+
+        Promise.all(prepare)
+          .then(results => {
+            let annotationMap = results[0].entities.reduce((t, e) => {
+              for (let i=0; i<e.annotations.length; ++i) {
+                let annotation = e.annotations[i];
+                if (!t.has(annotation.id)) {
+                  t.set(annotation.id, new Set());
+                }
+                let targetSet = t.get(annotation.id);
+                for (let i=0; i<e.skeleton_ids.length; ++i) {
+                  targetSet.add(e.skeleton_ids[i]);
+                }
+              }
+              return t;
+            }, new Map());
+
+            widget.clearGroupSources();
+
+            let skeletonIds = results[0];
+            if (subGroupMap.size > 0) {
+              let lut = new THREE.Lut("rainbow", annotationIds.length);
+              lut.setMin(0);
+              lut.setMax(annotationIds.length);
+              // List number of active neurons for all available groups and update
+              // skeleton sources.
+              let counter = 0;
+              for (let [sg, annotationIds] of subGroupMap.entries()) {
+                let source = new CATMAID.BasicSkeletonSource('Pair statistics - sub-group ' + sg);
+                widget.groupSources.push(source);
+
+                let span1 = subGroupList.appendChild(document.createElement('span'));
+                span1.style.width = '10%';
+                span1.appendChild(document.createTextNode(sg));
+                let annotationNames = Array.from(annotationIds).map(e => {
+                  let color = lut.getColor(counter);
+                  ++counter;
+
+                  let name = CATMAID.annotations.getName(e);
+                  let skeletonIds = Array.from(annotationMap.get(e));
+
+                  let skeletonModels = skeletonIds ? skeletonIds.reduce((o,e) => {
+                    o[e] = new CATMAID.SkeletonModel(e, undefined, color);
+                    return o;
+                  }, {}) : null;
+                  if (skeletonModels) {
+                    source.append(skeletonModels);
+                  }
+
+                  let skeletonLinks = skeletonIds ?
+                      skeletonIds.map(skid => `<a class="neuron-selection-link" href="#" data-id="${skid}">${skid}</a>`) :
+                      ['(none)'];
+                  return `${name}: ${skeletonLinks.join(', ')}`;
+                });
+                let span2 = subGroupList.appendChild(document.createElement('span'));
+                span2.style.width = '85%';
+                span2.innerHTML = annotationNames.join(', ');
+              }
+            }
+
+            // List matching ID pair information
+          })
+          .catch(CATMAID.handleError);
+
+        $(subGroupList).on('click', 'a[data-id]', e => {
+          let id = Number(e.target.dataset.id);
+          if (Number.isNaN(id)) {
+            CATMAID.warn("Could not parse ID: " + e.target.dataset.id);
+            return;
+          }
+          CATMAID.TracingTool.goToNearestInNeuronOrSkeleton('skeleton', id);
+        });
+
+        let matchingPairsHeader = content.appendChild(document.createElement('h1'));
+        matchingPairsHeader.style.clear = 'both';
+        matchingPairsHeader.appendChild(document.createTextNode('Matching pairs'));
       }
     },
     'pair-statistics': {

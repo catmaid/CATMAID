catmaid/CATMAID

commit ed549fd1caee2b07b593f3767f9bc3ea9aa12992

Init: allow URLs with only a project ID and skeleton ID
Without stacks, the URL loading will now get all stacks for the project
and uses the one with the lowest ID. Before, this would result in an
error.

django/applications/catmaid/static/js/init.js
@@ -457,8 +457,21 @@ var project;
                 project.moveTo(zp, yp, xp, sgs);
               }
             });
-        } else  {
+        } else if (sids.length > 0) {
           load = loadStacksFromURL(singleStackViewer);
+        } else {
+          // Get first available stack for project
+          load = CATMAID.fetch(`${pid}/stacks`)
+            .then(stacks => {
+              if (stacks.length === 0) {
+                throw new CATMAID.ValueError("No stacks found for project " + pid);
+              }
+              // Push stack with lowest ID using zoom level 0
+              stacks.sort((a,b) => a.id - b.id);
+              sids.push(stacks[0].id);
+              ss.push(0);
+              return loadStacksFromURL(singleStackViewer);
+            });
         }
 
         // After stacks or stack groups have been loaded, init selected tool.

commit 7a9cb2a7ed8639b4a472347ae0d4c6e9eeb5a0a6

Init: fix loading without URL parameters
This broke by accident in ed549fd1caee2b07.

django/applications/catmaid/static/js/init.js
@@ -457,9 +457,7 @@ var project;
                 project.moveTo(zp, yp, xp, sgs);
               }
             });
-        } else if (sids.length > 0) {
-          load = loadStacksFromURL(singleStackViewer);
-        } else {
+        } else if (pid && sids.length === 0) {
           // Get first available stack for project
           load = CATMAID.fetch(`${pid}/stacks`)
             .then(stacks => {
@@ -472,6 +470,8 @@ var project;
               ss.push(0);
               return loadStacksFromURL(singleStackViewer);
             });
+        } else {
+          load = loadStacksFromURL(singleStackViewer);
         }
 
         // After stacks or stack groups have been loaded, init selected tool.
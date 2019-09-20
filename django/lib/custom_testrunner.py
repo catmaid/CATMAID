e998aefe7e80148cec19e392a371ecf269f441d9
Tom Kazimiers <tom@voodoo-arts.net>
2019-09-20 10:02:08 -0400

    Tests: don't pack static files through pipeline
    Packing and cache busting makes the test setup only more complicated.

--- django/lib/custom_testrunner.py
+++ django/lib/custom_testrunner.py
@@ -2,8 +2,16 @@
 
 from django.conf import settings
 from django.test.runner import DiscoverRunner
+from pipeline.conf import settings as pipeline_settings
 
 class TestSuiteRunner(DiscoverRunner):
+
     def __init__(self, *args, **kwargs):
         settings.TESTING_ENVIRONMENT = True
         super(TestSuiteRunner, self).__init__(*args, **kwargs)
+
+    def setup_test_environment(self, **kwargs):
+        '''Override STATICFILES_STORAGE and pipeline DEBUG.'''
+        super().setup_test_environment(**kwargs)
+        settings.STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
+        pipeline_settings.DEBUG = True

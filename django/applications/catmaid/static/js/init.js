edbab11e560e85f513dd6dd4528064a36da1d42b
Tom Kazimiers <tom@voodoo-arts.net>
2019-09-23 18:07:03 -0400

    Fix user list update for logged in users

--- django/applications/catmaid/static/js/init.js
+++ django/applications/catmaid/static/js/init.js
@@ -962,7 +962,7 @@ var project;
     CATMAID.setupCsrfProtection();
 
     var load = CATMAID.updatePermissions();
-    if (e.id || (e.permissions && -1 !== e.permissions.indexOf('catmaid.can_browse'))) {
+    if (e.is_authenticated || (e.permissions && -1 !== e.permissions.indexOf('catmaid.can_browse'))) {
       // Asynchronously, try to get a full list of users if a user is logged in
       // or the anonymous user has can_browse permissions.
       load = load.then(CATMAID.User.getUsers.bind(CATMAID.User));

56b9daf2f1627ec72dc4b5318ee85aa472970c7e
Chris Barnes <barnesc@janelia.hhmi.org>
2019-11-01 17:00:19 +0000

    Volume tests: lint fixes

--- django/applications/catmaid/tests/apis/test_volume.py
+++ django/applications/catmaid/tests/apis/test_volume.py
@@ -171,76 +171,96 @@ class VolumeTests(CatmaidApiTestCase):
         assign_perm('can_import', self.test_user, self.test_project)
 
         # Make sure a basic mesh can be added (a single triangle).
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Test volume",
                 "mesh": json.dumps([
                     [[0,0,0], [1,0,0], [0,1,0]],
                     [[0,1,2]],
                 ]),
-            })
+            },
+        )
         self.assertStatus(response, code=200)
 
         # No faces
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Malformed volume",
                 "mesh": [
                     [[[0,0,0], [1,0,0], [0,1,0]]],
                 ],
-            })
+            },
+        )
         self.assertStatus(response, code=400)
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Malformed volume",
                 "mesh": [
                     [[[0,0,0], [1,0,0], [0,1,0]]],
                     [],
                 ],
-            })
+            },
+        )
         self.assertStatus(response, code=400)
 
         # No points
         self.assertStatus(response, code=400)
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Test volume",
                 "mesh": json.dumps([
                     [],
                     [[0,1,2]],
                 ]),
-            })
+            },
+        )
         self.assertStatus(response, code=400)
 
         # To few points
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Test volume",
                 "mesh": json.dumps([
                     [[1,0,0], [0,1,0]],
                     [[0,1,2]],
                 ]),
-            })
+            },
+        )
         self.assertStatus(response, code=400)
 
         # Too many point dimensions
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add',
+            {
                 "type": "trimesh",
                 "title": "Test volume",
                 "mesh": json.dumps([
                     [[0,0,0,0], [1,0,0], [0,1,0]],
                     [[0,1,2]],
                 ]),
-            })
+            },
+        )
         self.assertStatus(response, code=400)
 
         # Too many face dimensions
-        response = self.client.post(f'/{self.test_project_id}/volumes/add', {
+        response = self.client.post(
+            f'/{self.test_project_id}/volumes/add', {
                 "type": "trimesh",
                 "title": "Test volume",
                 "mesh": json.dumps([
                     [[0,0,0], [1,0,0], [0,1,0]],
                     [[0,1,2,0]],
                 ]),
-            })
+            },
+        )
         self.assertStatus(response, code=400)

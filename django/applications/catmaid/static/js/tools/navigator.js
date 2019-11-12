6822f4ffed364c8f4061ea8aee56f4943d1f2e3e
Andrew Champion <andrew.champion@gmail.com>
2019-11-12 14:19:21 +0000

    Navigator: copy stack locations from focused stack
    Also indicate which stack was used in the alert message.

--- django/applications/catmaid/static/js/tools/navigator.js
+++ django/applications/catmaid/static/js/tools/navigator.js
@@ -440,10 +440,11 @@
 
     var handleCopyPosRequest = function(e) {
       if (e.altKey) {
-        let stack = project.getStackViewers()[0];
+        let stack = project.focusedStackViewer;
+        let stackTitle = stack.primaryStack.title;
         let stackLocation = `${stack.x.toFixed(2)}, ${stack.y.toFixed(2)}, ${stack.z.toFixed(2)}`;
         CATMAID.tools.copyToClipBoard(stackLocation);
-        CATMAID.msg('Success', `Copied stack space location (px):<br />${stackLocation}`);
+        CATMAID.msg('Success', `Copied stack space location for stack "${stackTitle}" (px):<br />${stackLocation}`);
         // Necessary to handle event to prevent default download link behavior.
         e.preventDefault();
       } else {

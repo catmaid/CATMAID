627ef245440d36843c19fb1b0751e4b70670db8d
Tom Kazimiers <tom@voodoo-arts.net>
2019-11-18 11:51:41 -0500

    Use database defaults for import_cable_length column in stats table
    I could have sworn I made this exact change already yesterday.

--- django/applications/catmaid/migrations/0094_add_imported_cable_length_stats_summary_column.py
+++ django/applications/catmaid/migrations/0094_add_imported_cable_length_stats_summary_column.py
@@ -1,6 +1,17 @@
 from django.db import migrations, models
 
 
+forward = """
+    ALTER TABLE catmaid_stats_summary
+    ADD COLUMN import_cable_length double precision DEFAULT 0 NOT NULL;
+"""
+
+backward = """
+    ALTER TABLE catmaid_stats_summary
+    DROP COLUMN import_cable_length;
+"""
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -8,9 +19,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='statssummary',
-            name='import_cable_length',
-            field=models.FloatField(default=0),
-        ),
+        migrations.RunSQL(forward, backward, [
+            migrations.AddField(
+                model_name='statssummary',
+                name='import_cable_length',
+                field=models.FloatField(default=0),
+            ),
+        ]),
     ]

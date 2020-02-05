catmaid/CATMAID

commit ea9017b0e729dc0ce848511e480c6aa6336bf37a

Add skeleton history graph widget
This widget can show how skeleton membership of nodes of a set of origin
skeletons changes over time. A set of source skeleton IDs is provided
and all the earliest occurences of each past and present member node of
these skeletons is looked up. Each of these nodes is then tracked
through history until its most recent entry. This results in a path of
skeleton IDs for each node, containing each skeleton ID this node has
been member of. The widget collects these paths and maps the start
skeleton ID of that path to the end node of the path. Multiple path will
typically fall into every start/end skeleton group. The directed edge
from start to edge is the summed weight of all paths having the same
start and end node/skeleton.

Note that also past skeleton IDs can be entered, which will reveal where
the treenodes of those former skeletons belong to now.

django/applications/catmaid/static/js/widgets/compartment_graph_widget.js
@@ -841,7 +841,7 @@
             })
           .selector("edge")
             .css({
-              "content": "data(label)",
+              "content": "data(weight)",
               "width": "data(width)", //mapData(weight, 0, 100, 10, 50)",
               "target-arrow-shape": "data(arrow)",
               "target-arrow-color": "data(color)",

django/applications/catmaid/control/skeleton.py
@@ -3385,3 +3385,163 @@ def skeletons_in_bounding_box(request:HttpRequest, project_id) -> JsonResponse:
 
     skeleton_ids = get_skeletons_in_bb(params)
     return JsonResponse(skeleton_ids, safe=False)
+
+
+@api_view(['GET'])
+@requires_user_role([UserRole.Browse])
+def change_history(request:HttpRequest, project_id=None) -> JsonResponse:
+    """Return the history of all skeletons ID changes in project over time.
+    Optionally, this can be constrained by a user ID and a time window.
+    ---
+    parameters:
+        - name: project_id
+          description: Project to operate in
+          required: true
+          type: integer
+          paramType: path
+        - name: initial_user_id
+          description: User who caused the first change in all returned skeleton.
+          required: false
+          type: integer
+          paramType: form
+        - name: changes_after
+          description: |
+            Date of format YYYY-MM-DDTHH:mm:ss, only the date part is required.
+            Limits returns history to skeleton changes after this date.
+          required: false
+          type: string
+          paramType: form
+        - name: changes_before
+          description: |
+            Date of format YYYY-MM-DDTHH:mm:ss, only the date part is required.
+            Limits returns history to skeleton changes before this date.
+          required: false
+          type: string
+          paramType: form
+        - name: skeleton_ids
+          description: Skeleton IDs of the initial set of treenodes.
+          required: false
+          type: array
+          paramType: form
+    type:
+        - type: array
+          items:
+          type: string
+          description: |
+            array of arrays, each representing a unique skeleton path in
+            historic order, newest last.
+          required: true
+    """
+    initial_user_id = request.GET.get('initial_user_id')
+    changes_after = request.GET.get('changes_after')
+    changes_before = request.GET.get('changes_before')
+    skeleton_ids = get_request_list(request.GET, 'skeleton_ids', map_fn=int)
+
+    init_constraints = ['project_id = %(project_id)s']
+    constraints = ['project_id = %(project_id)s']
+
+    if initial_user_id is not None:
+        init_constraints.append("cti.user_id = %(initial_user_id)s")
+
+    if changes_after:
+        init_constraints.append("edition_time > %(changes_after)s")
+        constraints.append("execution_time > %(changes_after)s")
+
+    if changes_before:
+        init_constraints.append("execution_time > %(changes_before)s")
+        constraints.append("execution_time > %(changes_before)s")
+
+    if skeleton_ids:
+        init_constraints.append('skeleton_id = ANY(ARRAY[%(skeleton_ids)s])')
+
+    if not init_constraints:
+        raise ValueError("Please provide at least one constraint")
+
+    # 1. Get all relevant initial transactions
+    # 2. Find treenode IDs modified by those transactions
+    # 3. Get all history and live table entries for those treenodes, ordered by
+    #    transaction execution time, oldest last. History entries come first, live
+    #    entries are last.
+    # 4. Collect all referenced skeleton IDs from ordered treenodes. This results in
+    #    a skeleton ID path for each treenode. To reduce this to distinct paths, a
+    #    textual representation is done for each (id:id:id…) and only distinct values
+    #    are selected. This should allow then to get fragment skeleton ID changes
+    #    through merges and splits.
+    cursor = connection.cursor()
+    cursor.execute("""
+        WITH skeleton_class AS (
+                SELECT id as class_id
+                FROM class
+                WHERE project_id = %(project_id)s
+                    AND class_name = 'skeleton'
+        ),
+        changed_treenodes AS (
+                SELECT t.id, t.skeleton_id, MIN(txid) as txid, MIN(edition_time) as edition_time
+                FROM (
+                        /* Deleted skeletons from history */
+                        select th.id as id, th.skeleton_id as skeleton_id, MIN(th.txid) as txid, MIN(th.edition_time) as edition_time from treenode__history th
+                        /* where th.exec_transaction_id = txs.transaction_id */
+                        {init_constraints}
+                        group by th.id, th.skeleton_id
+                        union all
+                        /* Current skeletons */
+                        select t.id as id, t.skeleton_id as skeleton_id, MIN(t.txid) as txid, MIN(t.edition_time) as edition_time from treenode t
+                        /* where t.txid = txs.transaction_id */
+                        {init_constraints}
+                        GROUP BY t.id, t.skeleton_id
+                ) t
+                GROUP BY id, t.skeleton_id
+        ),
+        all_changed_skeletons AS (
+                SELECT ct.id, ct.skeleton_id, -1 as pos, ct.edition_time, ct.txid
+                FROM changed_treenodes ct
+                UNION
+                SELECT th.id as treenode_id, th.skeleton_id, 0 as pos, th.edition_time, th.txid as txid
+                FROM changed_treenodes ct
+                JOIN treenode__history th
+                        ON th.id = ct.id
+                WHERE th.txid > ct.txid
+                UNION
+                SELECT t.id, t.skeleton_id, 1 as pos, t.edition_time, t.txid
+                FROM changed_treenodes ct
+                JOIN treenode t
+                        ON t.id = ct.id
+                WHERE t.txid > ct.txid
+        ),
+        agg_skeletons AS (
+                SELECT string_agg(skeleton_id::text, ':' ORDER BY pos ASC, txid ASC) as key,
+                    array_agg(skeleton_id ORDER BY pos ASC, txid ASC) AS skeleton_ids,
+                    array_agg(txid ORDER BY pos ASC, txid ASC) AS txids,
+                    max(pos) as present
+                    /*array_agg(edition_time ORDER BY pos ASC, txid ASC) AS times*/
+                FROM all_changed_skeletons
+                GROUP BY id
+        )
+        /*
+        ,agg_treenodes AS (
+                SELECT key, skeleton_ids[1]::text || '-' || skeleton_ids[array_length(skeleton_ids, 1)]::text as begin_end, count(*) as c, skeleton_ids, max(present) as present
+                FROM agg_skeletons
+                GROUP BY key, skeleton_ids
+                ORDER BY key
+        )
+        */
+        SELECT skeleton_ids, count(*), max(present) FROM agg_skeletons
+        GROUP BY key, skeleton_ids
+        ORDER BY skeleton_ids[0], count(*) DESC;
+        /*
+        SELECT begin_end, SUM(c), max(present) from agg_treenodes
+        GROUP BY begin_end, skeleton_ids[1], skeleton_ids[array_length(skeleton_ids, 1)]
+        ORDER by skeleton_ids[1], sum(c) desc;
+        */
+    """.format(**{
+        'init_constraints': ('WHERE ' if init_constraints else '') + ' AND '.join(init_constraints),
+        'constraints': ('WHERE ' if constraints else '') + ' AND '.join(constraints),
+    }), {
+        'project_id': project_id,
+        'initial_user_id': initial_user_id,
+        'changes_after': changes_after,
+        'changes_before': changes_before,
+        'skeleton_ids': skeleton_ids,
+    })
+
+    return JsonResponse(cursor.fetchall(), safe=False)

django/applications/catmaid/static/libs/catmaid/models/skeleton.js
@@ -408,7 +408,32 @@
           }, []);
           return skeletonIds;
         });
-      },
+    },
+
+    /**
+     * Get the skeleton change history for all skeletons in a project,
+     * optionally constrained by user, and after as well as before dates.
+     * Skeletons are modified by splits and merges.
+     */
+    skeletonHistory: function(projectId, skeletonIds = null, userId = null,
+        changesAfter = null, changesBefore = null, api = null) {
+
+      if ([skeletonIds, userId].every(e => e === null)) {
+        throw new CATMAID.ValueError("Please provide either a set of skeleton IDs or a user ID");
+      }
+      return CATMAID.fetch({
+        url: projectId + '/skeletons/change-history',
+        method: 'GET',
+        data: {
+          initial_user_id: userId,
+          changes_after: changesAfter,
+          changes_before: changesBefore,
+          skeleton_ids: skeletonIds ?
+            skeletonIds.split(',').map(s => parseInt(s.trim(), 10)) : [],
+        },
+        api: api,
+      });
+    },
 
   };
 

django/applications/catmaid/urls.py
@@ -304,6 +304,7 @@ urlpatterns += [
     url(r'^(?P<project_id>\d+)/skeleton/annotationlist$', skeleton.annotation_list),
     url(r'^(?P<project_id>\d+)/skeletons/within-spatial-distance$', skeleton.within_spatial_distance),
     url(r'^(?P<project_id>\d+)/skeletons/node-labels$', skeleton.skeletons_by_node_labels),
+    url(r'^(?P<project_id>\d+)/skeletons/change-history$', skeleton.change_history),
     url(r'^(?P<project_id>\d+)/skeletongroup/adjacency_matrix$', skeleton.adjacency_matrix),
     url(r'^(?P<project_id>\d+)/skeletongroup/skeletonlist_subgraph', skeleton.skeletonlist_subgraph),
     url(r'^(?P<project_id>\d+)/skeletongroup/all_shared_connectors', skeleton.all_shared_connectors),

commit 64b2c69b5c1561bf7a61141754dcbec7381110de

Graph widget: fix non-default edge labeling strategies
Through the properties dialog users can select alternative edge labeling
strategies: inbound and outbound fractions of the connections. Updating
this setting correctly recomputed the fractions, but failed to display
them. The problem was a wrong style definition for edges that seems to
have been introduced by accident in commit ea9017b0e729dc0. This is
fixed now and both nodes and labels now use the more modern "label"
style field rather than the older "content".

django/applications/catmaid/static/js/widgets/compartment_graph_widget.js
@@ -844,7 +844,7 @@
       style: cytoscape.stylesheet()
         .selector("node")
             .css({
-              "content": "data(label)",
+              "label": "data(label)",
               "shape": "data(shape)",
               "border-width": 1,
               "background-color": "data(color)",
@@ -856,7 +856,7 @@
             })
           .selector("edge")
             .css({
-              "content": "data(weight)",
+              "label": "data(label)",
               "width": "data(width)", //mapData(weight, 0, 100, 10, 50)",
               "target-arrow-shape": "data(arrow)",
               "target-arrow-color": "data(color)",
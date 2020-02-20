b88612f4adb65a1c363e8127de0aa289a18bf3ae
Chris Barnes <barnesc@janelia.hhmi.org>
2020-02-20 21:10:33 +0000

    Vagrant setup
    Makes it very easy to set up a dev environment in a virtual machine.

---
+++ scripts/vagrant/root.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+PG_URL="http://apt.postgresql.org/pub/repos/apt/"
+APT_LINE="deb ${PG_URL} $(lsb_release -cs)-pgdg main"
+echo "${APT_LINE}" | sudo tee "/etc/apt/sources.list.d/pgdg.list"
+apt-get install wget ca-certificates
+PG_KEY_URL="https://www.postgresql.org/media/keys/ACCC4CF8.asc"
+wget --quiet -O - ${PG_KEY_URL} | sudo apt-key add -
+
+add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository ppa:ubuntugis/ppa
+# necessary for git 2.8's user.useConfigOnly option
+add-apt-repository ppa:git-core/ppa
+
+# node PPA
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+# R PPA
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
+apt-key update
+apt-get update
+
+cd /CATMAID
+sudo xargs apt-get install -y < packagelist-ubuntu-16.04-apt.txt
+apt-get install -y nodejs python3-pip python3.6-venv python3-wheel git r-base
+

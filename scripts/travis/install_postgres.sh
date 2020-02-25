99ad645e0608448f30de2bb2700f7fff2e742c25
Tom Kazimiers <tom@voodoo-arts.net>
2020-02-25 11:39:10 -0500

    CI: make sure Postgres runs on port 5432 and ramdisk

--- scripts/travis/install_postgres.sh
+++ scripts/travis/install_postgres.sh
@@ -8,6 +8,13 @@ sudo apt-get remove -q 'postgresql-*'
 sudo apt-get update -q
 sudo apt-get install -q postgresql-11 postgresql-client-11 postgresql-11-postgis-2.5 postgresql-11-postgis-2.5-scripts
 
+# Drop existin database and crate a new one to make sure we run in a ramdisk and
+# on port 5432.
+sudo pg_dropcluster 11 main
+sudo mkdir -p /var/ramfs/postgresql/11/main
+sudo chown postgres:postgres /var/ramfs/postgresql/11/main
+sudo pg_createcluster -d /var/ramfs/postgresql/11/main -p 5432 11 main
+
 # In case future Postgres version are not available on Travis and have to
 # installed manually, copying the existing hba file can save some time.
 sudo cp /etc/postgresql/{10,11}/main/pg_hba.conf

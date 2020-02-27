90b53915648822a4859e31b18ae5580102f2cda9
Chris Barnes <barnesc@janelia.hhmi.org>
2020-02-27 17:09:49 +0000

    Vagrant: make root setup version-agnostic

--- scripts/vagrant/root.sh
+++ scripts/vagrant/root.sh
@@ -4,8 +4,10 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
+CODENAME="$(lsb_release -cs)"
+
 PG_URL="http://apt.postgresql.org/pub/repos/apt/"
-APT_LINE="deb ${PG_URL} $(lsb_release -cs)-pgdg main"
+APT_LINE="deb ${PG_URL} ${CODENAME}-pgdg main"
 echo "${APT_LINE}" | sudo tee "/etc/apt/sources.list.d/pgdg.list"
 apt-get install wget ca-certificates
 PG_KEY_URL="https://www.postgresql.org/media/keys/ACCC4CF8.asc"
@@ -20,13 +22,14 @@ add-apt-repository ppa:git-core/ppa
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 # R PPA
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list
+echo "deb https://cloud.r-project.org/bin/linux/ubuntu ${CODENAME}/" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
 apt-key update
 apt-get update
 
+VERSION="$(lsb_release -rs)"
 cd /CATMAID
-sudo xargs apt-get install -y < packagelist-ubuntu-16.04-apt.txt
+sudo xargs apt-get install -y < packagelist-ubuntu-${VERSION}-apt.txt
 apt-get install -y nodejs python3-pip python3.6-venv python3-wheel git r-base
 

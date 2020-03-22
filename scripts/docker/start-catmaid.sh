8c52880224e67e8b4a6a6495db2de615bdb24c86
Tom Kazimiers <tom@voodoo-arts.net>
2020-03-22 10:36:11 -0400

    Docker: use exec() in start-catmaid script
    This replaces the Bash script process with the uwsgi process. This makes
    it possible for Supervisor to control the correct process.

--- scripts/docker/start-catmaid.sh
+++ scripts/docker/start-catmaid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Launch uWSGI
-/opt/virtualenvs/catmaid/bin/uwsgi --ini /home/scripts/docker/uwsgi-catmaid.ini
+exec /opt/virtualenvs/catmaid/bin/uwsgi --ini /home/scripts/docker/uwsgi-catmaid.ini

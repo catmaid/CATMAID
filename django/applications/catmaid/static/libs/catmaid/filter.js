b4bfa0697bb5d3ee5d5e4320991b70176a3f4cd0
Tom Kazimiers <tom@voodoo-arts.net>
2020-03-26 15:59:06 -0400

    Node filters: fix 'Pruned arbor' filter
    The logic of node filtering was consistent and two approaches were
    implemented to 50% each. This is fixed now.

--- django/applications/catmaid/static/libs/catmaid/filter.js
+++ django/applications/catmaid/static/libs/catmaid/filter.js
@@ -902,7 +902,7 @@
         var tags = skeleton.tags;
         var cuts = tags[options.tag];
         var nodes = arbor.nodes();
-        let uparborNodes = cuts.forEach(function(cut) {
+        cuts.forEach(function(cut) {
           arbor.subArbor(cut).nodesArray().forEach(function(node) {
             delete nodes[node];
           });
@@ -910,9 +910,9 @@
 
 
         if (!invert) {
-          return uparborNodes;
+          return nodes;
         } else {
-          return Object.keys(uparborNodes).reduce(removeFromObject, arbor.nodes());
+          return Object.keys(nodes).reduce(removeFromObject, arbor.nodes());
         }
       }
     },

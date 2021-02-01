a10f79d5b733b0533d7c6e36df6f576eeb93cdc4
Tom Kazimiers <tom@voodoo-arts.net>
2021-02-01 14:37:17 +0100

    Fix wrong target table deep link model trigger

---
+++ django/applications/catmaid/migrations/0108_fix_deep_link_model_trigger.py
@@ -0,0 +1,31 @@
+import catmaid.fields
+import datetime
+from django.conf import settings
+import django.contrib.postgres.fields.jsonb
+import django.contrib.postgres.functions
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+forward = """
+    DROP TRIGGER on_edit_deep_link_stack;
+    CREATE TRIGGER on_edit_deep_link_stack BEFORE UPDATE ON catmaid_deep_link_stack FOR EACH ROW EXECUTE PROCEDURE on_edit();
+"""
+
+backward = """
+    DROP TRIGGER on_edit_deep_link_stack;
+    CREATE TRIGGER on_edit_deep_link_stack BEFORE UPDATE ON catmaid_deep_link FOR EACH ROW EXECUTE PROCEDURE on_edit();
+"""
+
+
+class Migration(migrations.Migration):
+    """Fixes a wrong trigger target table of the recently added trigger.
+    """
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('catmaid', '0107_update_deep_link_model_state'),
+    ]
+
+    operations = [
+        migrations.RunSQL(forward, backward),
+    ]

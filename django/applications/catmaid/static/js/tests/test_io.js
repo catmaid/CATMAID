02b097d997048eede37a5da3b77350bc4f67ef73
Chris L. Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2021-10-13 17:29:50 +0100

    io tests: fix function path

--- django/applications/catmaid/static/js/tests/test_io.js
+++ django/applications/catmaid/static/js/tests/test_io.js
@@ -1,19 +1,17 @@
 QUnit.test('Utilities test', function (assert) {
 
-  const io = CATMAID.io;
-
   // Test createTSVString
   (function () {
     const simple = [[1, 2], [3, 4]];
-    assert.ok(io.createTSVString(simple) == "1\t2\n3\t4", "createTSVString handles simple cases");
-    assert.ok(io.createTSVString(simple, null, ",", "|") == "1,2|3,4", "createTSVString can use custom separators");
+    assert.ok(CATMAID.createTSVString(simple) == "1\t2\n3\t4", "createTSVString handles simple cases");
+    assert.ok(CATMAID.createTSVString(simple, null, ",", "|") == "1,2|3,4", "createTSVString can use custom separators");
     const fn = (unit, rowIdx, colIdx) => [unit, rowIdx, colIdx].join("");
-    assert.ok(io.createTSVString(simple, fn) == "100\t201\n310\t411", "createTSVString can use a transformer function");
+    assert.ok(CATMAID.createTSVString(simple, fn) == "100\t201\n310\t411", "createTSVString can use a transformer function");
     const bad = [["12\t34", "56\n78"], ["9101", "1121"]];
     assert.throws(function () {
-      io.createTSVString(bad);
+      CATMAID.createTSVString(bad);
     }, "createTSVString errors when separators are present in the units");
-    assert.ok(io.createTSVString(bad, null, null, true), "createTSVString can skip separator-in-unit checks");
+    assert.ok(CATMAID.createTSVString(bad, null, null, true), "createTSVString can skip separator-in-unit checks");
   })();
 
 });

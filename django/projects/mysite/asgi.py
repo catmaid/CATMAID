d87d64bd03fd751cf4c282484e5a0e9c881c8cc1
Tom Kazimiers <tom@voodoo-arts.net>
2021-12-28 23:15:57 +0100

    ASGI: Remove duplicate import

--- django/projects/mysite/asgi.py
+++ django/projects/mysite/asgi.py
@@ -4,7 +4,6 @@ defined in the ASGI_APPLICATION setting.
 """
 import os
 
-from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application

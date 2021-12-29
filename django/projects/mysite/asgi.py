f18919fad60fa05d719d1ac09cec6b784145798a
Tom Kazimiers <tom@voodoo-arts.net>
2021-12-29 21:22:27 +0100

    ASGI: make catmaid module imports possible in entry-point script

--- django/projects/mysite/asgi.py
+++ django/projects/mysite/asgi.py
@@ -8,12 +8,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import catmaid.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry is
+# populated before importing code that may import ORM models or needs imports
+# from the catmaid module.
+django_asgi_app = get_asgi_application()
+
+import catmaid.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             catmaid.routing.websocket_urlpatterns

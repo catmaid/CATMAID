e8352dc9817be7d104db41e9186ac6b04a240fd7
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-03-22 18:07:05 +0000

    nblast: WIP widget with worker wrapper

---
+++ django/applications/catmaid/static/js/widgets/nblast.js
@@ -0,0 +1,47 @@
+
+(function (CATMAID) {
+
+  "use strict";
+
+  class Nblaster {
+    constructor(distThresholds, dotThresholds, cells, k) {
+      this.distThresholds = distThresholds;
+      this.dotThresholds = dotThresholds;
+      this.cells = cells;
+      this.k = k;
+
+      this.worker = null;
+    }
+
+    async init() {
+      this.worker = new CATMAID.PromiseWorker(
+        new Worker(CATMAID.makeStaticURL('libs/nblast-wasm/nblast_wasm_worker.js'))
+      );
+      return this.worker.postMessage(["new", {
+        distThresholds: this.distThresholds,
+        dotThresholds: this.dotThresholds,
+        cells: this.cells,
+        k: this.k,
+      }]);
+    }
+
+    async addNeuron(points, tangents, alphas) {
+      return this.worker.postMessage(["addNeuron", { points, tangents, alphas }]);
+    }
+
+    async queryTarget(queryIdx, targetIdx, normalize, symmetry, useAlpha) {
+      return this.worker.postMessage(["queryTarget", { queryIdx, targetIdx, normalize, symmetry, useAlpha }]);
+    }
+
+    async queriesTargets(queryIdxs, targetIdxs, normalize, symmetry, useAlpha, maxCentroidDist) {
+      return this.worker.postMessage(["queriesTargets", {
+        queryIdxs, targetIdxs, normalize, symmetry, useAlpha, maxCentroidDist
+      }])
+    }
+
+    async allVAll(normalize, symmetry, useAlpha, maxCentroidDist) {
+      normalize, symmetry, useAlpha, maxCentroidDist
+    }
+  }
+
+})(CATMAID);

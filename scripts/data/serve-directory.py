d3b8a362041cccffa963e24fa45485d51a2ee149
Mohinta2892 <samiamohinta2892@gmail.com>
2025-06-18 19:04:09 +0100

    add back key-path args

--- scripts/data/serve-directory.py
+++ scripts/data/serve-directory.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 
 # Simple local Python 3 HTTP(S) server with CORS support. The first argument is
-# the port to listen on. The Server will try to use HTTPS if the path to a
-# certificate is provided as second argument. The third argument can now be a path
-# to the n5 you want to serve (slight modification added for flexibility as the script now
-# no longer needs to reside in the same folder as the n5).
+# the port to listen on. To enable HTTPS, provide the path to a certificate
+# file and a key file. A fourth argument can be a path to the directory
+# you want to serve.
 #
-# To creae a local self-signed certificate (localhost.pem) and a key file
+# To create a local self-signed certificate (localhost.pem) and a key file
 # (localhost-key.pem), run the following:
 # openssl req -new -x509 -keyout localhost-key.pem -out localhost.pem -days 24855 -nodes
-
 #
-# With these files in place, the server now be started in 3 ways:
-# Option 1:
+# With these files in place, the server can now be started in multiple ways:
+# Option 1 (HTTP):
 # python serve-directory.py 9999
-# Option 2:
-# python serve-directory.py 9999 localhost.pem
-# Option 3:
-# python serve-directory.py 9999 localhost.pem /path/to/n5/
+# Option 2 (HTTPS):
+# python serve-directory.py 9999 localhost.pem localhost-key.pem
+# Option 3 (HTTPS with specific directory):
+# python serve-directory.py 9999 localhost.pem localhost-key.pem /path/to/n5/
 
 import os
 import ssl
@@ -45,14 +43,16 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
 
 
 class Server(HTTPServer):
-    def __init__(self, server_address, handler, cert_path=None):
+    def __init__(self, server_address, handler, cert_path=None, key_path=None):
         super(Server, self).__init__(server_address, handler)
-        if cert_path:
+        if cert_path and key_path:
             if not os.path.isfile(cert_path):
                 raise FileNotFoundError("Certificate file not found: {}".format(cert_path))
+            if not os.path.isfile(key_path):
+                raise FileNotFoundError("Key file not found: {}".format(key_path))
 
             context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-            context.load_cert_chain(certfile=cert_path)
+            context.load_cert_chain(certfile=cert_path, keyfile=key_path)
             self.socket = context.wrap_socket(self.socket, server_side=True)
 
 
@@ -60,25 +60,28 @@ def main():
     n_args = len(sys.argv)
 
     cert_path = None
+    key_path = None
     serve_dir = os.getcwd()
 
     if n_args == 2:
         port = int(sys.argv[1])
         protocol = 'HTTP'
-    elif n_args == 3:
-        port = int(sys.argv[1])
-        cert_path = sys.argv[2]
-        protocol = 'HTTPS'
     elif n_args == 4:
         port = int(sys.argv[1])
         cert_path = sys.argv[2]
-        serve_dir = sys.argv[3]
+        key_path = sys.argv[3]
+        protocol = 'HTTPS'
+    elif n_args == 5:
+        port = int(sys.argv[1])
+        cert_path = sys.argv[2]
+        key_path = sys.argv[3]
+        serve_dir = sys.argv[4]
         protocol = 'HTTPS'
     else:
         print("Usage:")
         print("  {} <port>".format(__file__))
-        print("  {} <port> <cert-file>".format(__file__))
-        print("  {} <port> <cert-file> <directory>".format(__file__))
+        print("  {} <port> <cert-file> <key-file>".format(__file__))
+        print("  {} <port> <cert-file> <key-file> <directory>".format(__file__))
         return
 
     os.chdir(serve_dir)
@@ -114,7 +117,7 @@ def main():
             print("Could not read metadata file 'metadata.txt'")
         print('')
 
-    server = Server(('localhost', port), CORSRequestHandler, cert_path)
+    server = Server(('localhost', port), CORSRequestHandler, cert_path, key_path)
     server.serve_forever()
 
 

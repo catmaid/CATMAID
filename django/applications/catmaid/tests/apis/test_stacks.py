c8d839da584c8931b393dce559747b0cb4c5920b
Tom Kazimiers <tom@voodoo-arts.net>
2026-05-05 13:00:28 +0200

    Tests: fix painting tool test case

--- django/applications/catmaid/tests/apis/test_stacks.py
+++ django/applications/catmaid/tests/apis/test_stacks.py
@@ -6,13 +6,20 @@ import tempfile
 import os
 import numpy as np
 import pyn5
+from pathlib import Path
+
+from django.db import transaction
+from django.test import TransactionTestCase, override_settings
 
 from catmaid.models import WritableStack
+from catmaid.control.stack import get_writable_stack_path
 
 from .common import CatmaidApiTestCase
 
 
+@override_settings(MEDIA_ROOT=Path(tempfile.TemporaryDirectory().name))
 class StacksApiTests(CatmaidApiTestCase):
+
     def test_stack_info(self):
         self.fake_authentication()
         test_stack_id = 3
@@ -39,7 +46,7 @@ class StacksApiTests(CatmaidApiTestCase):
                        'INCF Node</a>.</p>',
             "description": "",
             "metadata": None,
-            "num_zoom_levels": -1,
+            "num_zoom_levels": 0,
             "orientation": 0,
             "mirrors": [{
                 "id": 3,
@@ -71,7 +78,11 @@ class StacksApiTests(CatmaidApiTestCase):
                 "y": 0.0,
                 "z": 0.0
             },
-            "downsample_factors": None,
+            "downsample_factors": [{
+                "x": 1,
+                "y": 1,
+                "z": 1
+            }],
         }
 
         self.assertEqual(expected_result, parsed_response)
@@ -79,49 +90,69 @@ class StacksApiTests(CatmaidApiTestCase):
     def test_writable_n5_stack(self):
         self.fake_authentication()
         test_stack_id = 3
-        test_dtype = 'uint32'
+        test_dtype = 'uint8'
+        test_dataset = 'volumes/main'
         test_time = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        test_block_Size = [3, 3, 3]
+        dataset_size = [3, 3, 3]
 
-        with tempfile.TemporaryDirectory() as tempdir:
-            temp_path = os.path.join(tempdir, 'test.n5')
-            dataset_size = [3, 3, 3]
-
-            writable_stack = WritableStack.objects.create(
-                user_id=self.test_user_id, project_id=self.test_project_id,
-                name="Test writable stack", stack_id=test_stack_id,
-                path=temp_path, metadata={
-                    'dataset': 'volumes/test-dataset',
-                    'block_size': [1, 1, 1],
+        # Create a a writable stack
+        response = self.client.post(
+            f'/{self.test_project_id}/writable-stacks/',
+            {
+                'stack_id': test_stack_id,
+                'name': 'Test writable stack',
+                'filetype': 'n5',
+                'metadata': json.dumps({
+                    'dataset': test_dataset,
+                    'block_size': test_block_Size,
                     'dataset_size': dataset_size,
                     'dtype': test_dtype,
                     'last_update_time': test_time,
-                })
+                }),
+            })
+        self.assertStatus(response)
+        parsed_response = json.loads(response.content.decode('utf-8'))
 
-            test_data_raw = np.random.randint(0, 256, (3, 3, 3), test_dtype)
-            test_data = json.dumps(test_data_raw.tolist())
-            test_bounds = [[0, 0, 0], [3, 3, 3]]
+        writable_stack = WritableStack.objects.get(id=parsed_response['id'])
+        self.assertEqual(test_stack_id, writable_stack.stack_id)
 
-            response = self.client.post(
-                f'/{self.test_project_id}/stack/{test_stack_id}/write-block',
-                {
-                    'data': test_data,
-                    'data_bounds': json.dumps(test_bounds),
-                    'compression': 'GZIP',
-                    'compression_opts': -1,
-                })
-            self.assertStatus(response)
-            parsed_response = json.loads(response.content.decode('utf-8'))
+        # Raw data is assumed to be a a list of list of lists, in the order X, Y
+        # and Z.
+        test_data_raw = np.random.randint(0, 256, (3, 3, 3), test_dtype)
+        test_data = json.dumps(test_data_raw.tolist())[1:-1]
+        test_bounds = [[0, 0, 0], [2, 2, 2]] # These bounds are inclusive
+        block_bounds = [[0, 0, 0], [3, 3, 3]] # Exclusive version for query
 
-            self.assertEqual(True, parsed_response.get('update'))
-            self.assertEqual(test_bounds, parsed_response.get('last_update_bounds'))
-            self.assertNotEqual(test_time, parsed_response.get('last_update_time'))
+        response = self.client.post(
+            f'/{self.test_project_id}/writable-stacks/{writable_stack.id}/write-block',
+            {
+                'data': test_data,
+                # TODO: Improve this
+                'data_bounds[0][0]': test_bounds[0][0],
+                'data_bounds[0][1]': test_bounds[0][1],
+                'data_bounds[0][2]': test_bounds[0][2],
+                'data_bounds[1][0]': test_bounds[1][0],
+                'data_bounds[1][1]': test_bounds[1][1],
+                'data_bounds[1][2]': test_bounds[1][2],
+                'compression': 'raw',
+                'scale_level': 0,
+            })
+        self.assertStatus(response)
+        parsed_response = json.loads(response.content.decode('utf-8'))
 
-            # Open written file directly and compare the data
-            n5 = pyn5.open(writable_stack.path, 'volumes/test-dataset', test_dtype.upper(), True)
+        self.assertEqual(True, parsed_response.get('update'))
+        self.assertEqual(test_bounds, parsed_response.get('last_update_bounds'))
+        self.assertNotEqual(test_time, parsed_response.get('last_update_time'))
 
-            self.assertTrue(
-                np.array_equal(
-                    pyn5.read(n5, (np.array(test_bounds[0]), np.array(test_bounds[1])), test_dtype),
-                    test_data_raw,
-                )
+        # Open written file directly and compare the data
+        base_path = get_writable_stack_path(writable_stack.path)
+        dataset_path = os.path.join(test_dataset, 's0')
+        n5 = pyn5.open(base_path, dataset_path, test_dtype.lower(), False)
+
+        self.assertTrue(
+            np.array_equal(
+                test_data_raw.transpose([2, 1, 0]),
+                pyn5.read(n5, (np.array(block_bounds[0]), np.array(block_bounds[1])), test_dtype),
             )
+        )
